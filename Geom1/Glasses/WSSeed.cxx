92b3b4a25af97619d8b485d9ea5b74c679e8167f
Matevž Tadel <osschar@users.noreply.github.com>
2003-07-22 10:44:17 +0000

    Follow-up on TubeTvor changes.

--- Geom1/Glasses/WSSeed.cxx
+++ Geom1/Glasses/WSSeed.cxx
@@ -39,7 +39,7 @@ void WSSeed::Triangulate()
     return;
   }
 
-  pTuber->Init(false, 0, mTLevel*(size - 1) + 1, mPLevel);
+  pTuber->Init(0, mTLevel*(size - 1) + 1, mPLevel);
 
   list<WSPoint*>::iterator a, b = points.begin();
   bool first = true;
@@ -72,12 +72,12 @@ void WSSeed::Ring(WSPoint* f, Real_t t)
   Real_t t3 = t2*t;
   for(UCIndex_t i=1; i<=3; i++) {
     hPnt(i) = f->mCoffs(i,0u) + f->mCoffs(i,1u)*t +
-              f->mCoffs(i,2u)*t2 + f->mCoffs(i,3u)*t3;
+      f->mCoffs(i,2u)*t2 + f->mCoffs(i,3u)*t3;
     hAxe(i) = f->mCoffs(i,1u) + 2*f->mCoffs(i,2u)*t +
-              3*f->mCoffs(i,3u)*t2;
+      3*f->mCoffs(i,3u)*t2;
   }
   Real_t w = f->mCoffs(4u,0u) + f->mCoffs(4u,1u)*t +
-             f->mCoffs(4u,2u)*t2 + f->mCoffs(4u,3u)*t3;
+    f->mCoffs(4u,2u)*t2 + f->mCoffs(4u,3u)*t3;
   Real_t dwdt = TMath::ATan(f->mCoffs(4u,1u) + 2*f->mCoffs(4u,2u)*t +
 			    3*f->mCoffs(4u,3u)*t2);
   Real_t dwc = TMath::Cos(dwdt);
@@ -98,6 +98,17 @@ void WSSeed::Ring(WSPoint* f, Real_t t)
     // perhaps should invert normals for w<0
     pTuber->NewVert(R, N, 0);
   }
+  { // last one
+    phi = -2*TMath::Pi();
+    Real_t cp = TMath::Cos(phi), sp = TMath::Sin(phi);
+    for(UCIndex_t i=1; i<=3; ++i) {
+      R[i-1] = hPnt(i) + cp*w*hUp(i) + sp*w*hAw(i);
+      N[i-1] = -dws*oneAxe(i) + dwc*(cp*hUp(i) + sp*hAw(i));
+    }
+    // perhaps should invert normals for w<0
+    pTuber->NewVert(R, N, 0);
+  }
+
 }
 
 #include "WSSeed.c7"
