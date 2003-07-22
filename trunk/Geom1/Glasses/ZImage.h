404b5a38119449c39798bffa4a0be9dbb3ec384e
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2003-07-22 10:44:20 +0000

    Addition of glTexEnv and GL_TEXTURE_MAG/MIN_FILTER parameters.

--- trunk/Geom1/Glasses/ZImage.h
+++ trunk/Geom1/Glasses/ZImage.h
@@ -8,6 +8,7 @@
 #define Geom1_ZImage_H
 
 #include <Glasses/ZGlass.h>
+#include <Stones/ZColor.h>
 
 #ifndef __CINT__
 #include <IL/il.h>
@@ -15,6 +16,8 @@
 #else
 typedef unsigned int ILuint;
 #endif
+
+#include <GL/gl.h>
 
 class ZImage : public ZGlass {
   // 7777 RnrCtrl("false, 3, RnrBits(1,2,3,0, 0,0,0,0)")
@@ -25,24 +28,42 @@ private:
 
 protected:
   TimeStamp_t	mStampReqTexture; //! TimeStamp of last change of image
-  ILuint	mIL_Name;	//! X{gp}
+  ILuint	mIL_Name;	  //! X{gp}
 
-  TString	mFile;		//  X{GS} 7 Filor()
-  Int_t		mW;		//  X{G}  7 ValOut(-join=>1)
-  Int_t		mH;		//  X{G}  7 ValOut()
+  TString	mFile;		//  X{GS}  7 Filor()
+  Int_t		mW;		//  X{G}   7 ValOut(-join=>1)
+  Int_t		mH;		//  X{G}   7 ValOut()
 
-  Bool_t	bLoaded;	//  X{G}  7 BoolOut(-join=>1)
-  Bool_t	bLoadAdEnlight; //  X{GS} 7 Bool()
+  Int_t		mMagFilter;	//  X{GE}  7 PhonyEnum(-vals=>[GL_NEAREST,Nearest, GL_LINEAR,Linear], -width=>8, -join=>1)
+  Int_t		mMinFilter;	//  X{GE}  7 PhonyEnum(-vals=>[GL_NEAREST,Nearest, GL_LINEAR,Linear], -width=>8)
+
+  Int_t		mEnvMode;	//  X{GS}  7 PhonyEnum(-vals=>[GL_DECAL,Decal, GL_REPLACE,Replace, GL_MODULATE,Modulate, GL_BLEND,Blend], -width=>8, -join=>1)
+  ZColor	mEnvColor;	//  X{PGS} 7 ColorButt()
+
+  Bool_t	bLoaded;	//  X{G}   7 BoolOut(-join=>1)
+  Bool_t	bLoadAdEnlight; //  X{GS}  7 Bool()
 
 public:
   ZImage(const Text_t* n="ZImage", const Text_t* t=0) : ZGlass(n,t) { _init(); }
   virtual ~ZImage();
 
+  virtual void AdEnlightenment();
+
   virtual void SetStamps(TimeStamp_t s)
   { ZGlass::SetStamps(s); mStampReqTexture = s; }
 
-  void Load(); // X{E} 7 MButt(-join=>1)
-  void Save(); // X{E} 7 MButt()
+  void SetMagFilter(Int_t magfilter) {
+    mExecMutex.Lock(); mMagFilter = magfilter;
+    mStampReqTexture = Stamp(LibID(), ClassID()); mExecMutex.Unlock();
+  }
+  void SetMinFilter(Int_t minfilter) {
+    mExecMutex.Lock(); mMinFilter = minfilter; 
+    mStampReqTexture = Stamp(LibID(), ClassID()); mExecMutex.Unlock();
+  }
+
+  void Load();   // X{E} 7 MButt(-join=>1)
+  void Unload(); // X{E} 7 MButt(-join=>1)
+  void Save();   // X{E} 7 MButt()
 
   // Image operations ... locking is user's responsibility
   void  bind();
