c2d90fe4c32bb01c862357c06589fa475c6ebc11
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2003-11-17 17:47:11 +0000

    Minor improvement.

--- trunk/GledCore/Glasses/ZMirEmittingEntity.cxx
+++ trunk/GledCore/Glasses/ZMirEmittingEntity.cxx
@@ -32,8 +32,8 @@ void ZMirEmittingEntity::AdEnlightenment()
   if(mActiveIdentities == 0) {
     mActiveIdentities = new ZHashList("ActiveIdentities",
 	                        GForm("ActiveIdentities of %s", GetName()));
+    mActiveIdentities->SetElementFID(ZIdentity::FID());
     mQueen->CheckIn(mActiveIdentities);
-    // !!!! set to Fid of ZIdentity
   }
 }
 
