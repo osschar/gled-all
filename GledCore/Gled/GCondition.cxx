0609edc7458421e8daf012b61d677780a9e9aa5c
Matevž Tadel <osschar@users.noreply.github.com>
2004-06-03 12:23:44 +0000

    Member TimedWait() renamed to TimedWaitMS(), new member TimedWaitMus().

--- GledCore/Gled/GCondition.cxx
+++ GledCore/Gled/GCondition.cxx
@@ -1,6 +1,6 @@
 // $Header$
 
-// Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
@@ -18,6 +18,10 @@
 #include <sys/time.h>
 #include <unistd.h>
 
+ClassImp(GCondition)
+
+/**************************************************************************/
+
 GCondition::GCondition(GMutex::Init_e e) : GMutex(e) {
   pthread_cond_init(&mCond, 0);
 }
@@ -34,16 +38,38 @@ Int_t GCondition::Wait()
   return ret;
 }
 
-Int_t GCondition::TimedWait(UInt_t wait_ms)
+Int_t GCondition::TimedWaitMS(UInt_t wait_ms)
 {
   // Performs timedwait ... mutex should be locked upon calling this method.
   // Time given in mili-seconds.
+  // Returns 1 for time-out, 0 for other cases.
 
   struct timeval now;
   struct timespec timeout;
 
   gettimeofday(&now, 0);
   int mus_add = now.tv_usec + 1000*wait_ms;
+  timeout.tv_sec = now.tv_sec + mus_add/1000000;
+  timeout.tv_nsec = (mus_add%1000000) * 1000;
+  int retcode = pthread_cond_timedwait(&mCond, &mMut, &timeout);
+  if(retcode == ETIMEDOUT) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+Int_t GCondition::TimedWaitMuS(UInt_t wait_mus)
+{
+  // Performs timedwait ... mutex should be locked upon calling this method.
+  // Time given in micro-seconds.
+  // Returns 1 for time-out, 0 for other cases.
+
+  struct timeval now;
+  struct timespec timeout;
+
+  gettimeofday(&now, 0);
+  int mus_add = now.tv_usec + wait_mus;
   timeout.tv_sec = now.tv_sec + mus_add/1000000;
   timeout.tv_nsec = (mus_add%1000000) * 1000;
   int retcode = pthread_cond_timedwait(&mCond, &mMut, &timeout);
