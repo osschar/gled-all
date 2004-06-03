7b84694dd679b4877956c50bc5b0fd250c28ea2d
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-06-03 12:23:42 +0000

    New locking, transition to ROOT vec/mat classes.

--- trunk/GledCore/Glasses/ZNode.cxx
+++ trunk/GledCore/Glasses/ZNode.cxx
@@ -1,6 +1,6 @@
 // $Header$
 
-// Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
@@ -92,47 +92,47 @@ Int_t ZNode::Level()
 // Wrappers for ZTrans
 /**************************************************************************/
 
-Int_t ZNode::MoveLF(int vi, Real_t amount)
+Int_t ZNode::MoveLF(int vi, Float_t amount)
 {
   if(vi<0 || vi>3) return 0;
-  mTrans.MoveLF((UCIndex_t)vi, amount);
+  mTrans.MoveLF((Int_t)vi, amount);
   mStampReqTrans = Stamp(LibID(), ClassID());
   return 1;
 }
 
-Int_t ZNode::RotateLF(Int_t i1, Int_t i2, Real_t amount)
+Int_t ZNode::RotateLF(Int_t i1, Int_t i2, Float_t amount)
 {
   if(i1<0 || i1>3 || i2<0 || i2>3) return 0;
-  mTrans.RotateLF((UCIndex_t)i1, (UCIndex_t)i2, amount);
+  mTrans.RotateLF((Int_t)i1, (Int_t)i2, amount);
   mStampReqTrans = Stamp(LibID(), ClassID());
   return 1;
 }
 
-Int_t ZNode::Move(ZNode* ref, Int_t vi, Real_t amount)
+Int_t ZNode::Move(ZNode* ref, Int_t vi, Float_t amount)
 {
   if(ref==0) return MoveLF(vi, amount);
   if(vi<0 || vi>3) return 0;
   if(ref == this) {
-    mTrans.MoveLF((UCIndex_t)vi, amount);
+    mTrans.MoveLF((Int_t)vi, amount);
   } else {
     ZTrans* a = BtoA(mParent, ref);
-    mTrans.Move(a, (UCIndex_t)vi, amount);
+    mTrans.Move(a, (Int_t)vi, amount);
     delete a;
   }
   mStampReqTrans = Stamp(LibID(), ClassID());
   return 1;
 }
 
-Int_t ZNode::Rotate(ZNode* ref, Int_t ii1, Int_t ii2, Real_t amount)
+Int_t ZNode::Rotate(ZNode* ref, Int_t ii1, Int_t ii2, Float_t amount)
 {
   if(ref==0) return RotateLF(ii1, ii2, amount);
   if(ii1<0 || ii1>3 || ii2<0 || ii1>3) return 0;
   if(ref == this) {
     // Get explosion from m^-1 * m
-    mTrans.RotateLF((UCIndex_t)ii1, (UCIndex_t)ii2, amount);
+    mTrans.RotateLF((Int_t)ii1, (Int_t)ii2, amount);
   } else {
     ZTrans* a = BtoA(mParent, ref);
-    mTrans.Rotate(a, (UCIndex_t)ii1, (UCIndex_t)ii2, amount);
+    mTrans.Rotate(a, (Int_t)ii1, (Int_t)ii2, amount);
     delete a;
   }
 
@@ -156,49 +156,49 @@ Int_t ZNode::MultBy(ZTrans& t)
 
 /**************************************************************************/
 
-Int_t ZNode::Set3Pos(Real_t x, Real_t y, Real_t z)
+Int_t ZNode::Set3Pos(Float_t x, Float_t y, Float_t z)
 {
   mTrans.Set3Pos(x,y,z); mStampReqTrans = Stamp(LibID(), ClassID()); return 1;
 }
 
-Int_t ZNode::SetRotByAngles(Real_t a1, Real_t a2, Real_t a3)
+Int_t ZNode::SetRotByAngles(Float_t a1, Float_t a2, Float_t a3)
 {
   mTrans.SetRotByAngles(a1,a2,a3);
   mStampReqTrans = Stamp(LibID(), ClassID());
   return 1;
 }
 
-Int_t ZNode::SetRotByDegrees(Real_t a1, Real_t a2, Real_t a3)
+Int_t ZNode::SetRotByDegrees(Float_t a1, Float_t a2, Float_t a3)
 {
-  Real_t f = TMath::Pi()/180;
+  Float_t f = TMath::Pi()/180;
   SetRotByAngles(f*a1, f*a2, f*a3);
   return 1;
 }
 
 /**************************************************************************/
 
-void ZNode::SetS(Real_t xx)
+void ZNode::SetS(Float_t xx)
 {
-  mExecMutex.Lock();
+  WriteLock();
   mSx = mSy = mSz = xx;
   mStampReqTrans = Stamp(LibID(), ClassID());
-  mExecMutex.Unlock();
+  WriteUnlock();
 }
 
-void ZNode::SetScales(Real_t x, Real_t y, Real_t z)
+void ZNode::SetScales(Float_t x, Float_t y, Float_t z)
 {
-  mExecMutex.Lock();
+  WriteLock();
   mSx=x; mSy=y; mSz=z;
   mStampReqTrans = Stamp(LibID(), ClassID());
-  mExecMutex.Unlock();
+  WriteUnlock();
 }
 
-void ZNode::MultS(Real_t s)
+void ZNode::MultS(Float_t s)
 {
-  mExecMutex.Lock();
+  WriteLock();
   mSx*=s; mSy*=s; mSz*=s;
   mStampReqTrans = Stamp(LibID(), ClassID());
-  mExecMutex.Unlock();
+  WriteUnlock();
 }
 
 /**************************************************************************/
@@ -213,16 +213,17 @@ ZTrans* ZNode::ToMFR(int depth)
   ZNode *p = mParent;
   ZTrans* x;
   if(p == 0) {
-    mExecMutex.Lock();
+    ReadLock();
     x = new ZTrans(mTrans);
     if(bUseScale) { x->Scale(mSx, mSy, mSz); }
-    mExecMutex.Unlock();
+    ReadUnlock();
   } else {
-    x = p->ToMFR(++depth); if(x == 0) return 0;
-    mExecMutex.Lock();
+    x = p->ToMFR(++depth);
+    if(x == 0) return 0;
+    ReadLock();
     *x *= mTrans;
     if(bUseScale) { x->Scale(mSx, mSy, mSz); }
-    mExecMutex.Unlock();
+    ReadUnlock();
   }
   return x;
 }
@@ -239,17 +240,17 @@ ZTrans* ZNode::ToNode(ZNode* top, int depth)
 
   ZTrans* x;
   if(p == top) {
-    mExecMutex.Lock();
+    ReadLock();
     x = new ZTrans(mTrans);
     if(bUseScale) { x->Scale(mSx, mSy, mSz); }
-    mExecMutex.Unlock();
+    ReadUnlock();
   } else {
     x = p->ToNode(top, ++depth);
     if(x) {
-      mExecMutex.Lock();
-      *x *= mTrans; if(x == 0) return 0;
+      ReadLock();
+      *x *= mTrans;
       if(bUseScale) { x->Scale(mSx, mSy, mSz); }
-      mExecMutex.Unlock();
+      ReadUnlock();
     }
   }
   return x;
@@ -294,9 +295,9 @@ void ZNode::Spit() const
 
 ostream& operator<<(ostream& s, const ZNode& n) {
   const ZTrans& t = n.RefTrans();
-  for(UCIndex_t i=0; i<=3; i++) {
+  for(Int_t i=0; i<=3; i++) {
     s << t(i,4u) << "\t|\t";
-    for(UCIndex_t j=0; j<=3; j++)
+    for(Int_t j=0; j<=3; j++)
       s << t(i,j) << ((j==3) ? "\n" : "\t");
   }
   return s;
