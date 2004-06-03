fecab246e2ac19a307a8c32331ca98019f3db1c6
Matevž Tadel <osschar@users.noreply.github.com>
2004-06-03 12:30:38 +0000

    Improvements and extensions of rendering, creation from TH2, custom color gradient for z-value coloration (see ZRibbon).

--- Geom1/Rnr/GL/RectTerrain_GL_Rnr.cxx
+++ Geom1/Rnr/GL/RectTerrain_GL_Rnr.cxx
@@ -3,10 +3,6 @@
 
 #include <GL/glu.h>
 
-RectTerrain_GL_Rnr::RectTerrain_GL_Rnr(RectTerrain* rt) :
-  ZNode_GL_Rnr(rt), mTerrain(rt)
-{}
-
 /**************************************************************************/
 
 void RectTerrain_GL_Rnr::check(double xf, double yf,
@@ -21,9 +17,23 @@ void RectTerrain_GL_Rnr::check(double xf, double yf,
 	 xf, -yf, ret, x,y,z);
 }
 
+/**************************************************************************/
+
 void RectTerrain_GL_Rnr::Draw(RnrDriver* rd)
 {
-  if(mTerrain->mNx) {
+  if(mTerrain->mNx && mTerrain->mNy) {
+
+    switch(mTerrain->mOriginMode) {
+    case RectTerrain::OM_Border:
+      glTranslatef(mTerrain->mDx, mTerrain->mDy, 0);
+      break;
+    case RectTerrain::OM_Center:
+      glTranslatef(-0.5*mTerrain->mNx*mTerrain->mDx,
+		   -0.5*mTerrain->mNy*mTerrain->mDy, 0);
+      break;
+    default:
+      break;
+    }
 
     if(mTerrain->bStudySize) {
       GLdouble proj_m[16], modv_m[16];
@@ -40,48 +50,91 @@ void RectTerrain_GL_Rnr::Draw(RnrDriver* rd)
       check(0.5,0.5, proj_m, modv_m, viewp);
     }
 
-    glPushMatrix();
-    glScalef(1,1,mTerrain->mZScale);
-    glEnable(GL_NORMALIZE);
-
-    glColor4fv(mTerrain->mMinCol());
-    for(UCIndex_t i=0; i<mTerrain->mNx-1; i++) {
-      glBegin(GL_QUAD_STRIP);
-      for(UCIndex_t j=0; j<mTerrain->mNy; j++) {
-	MkVN(i, j); MkVN(i+1u, j);
-      }
-      glEnd();
+    if(mTerrain->bUseDispList) {
+      ZGlass_GL_Rnr::Draw(rd);
+    } else {
+      Render(rd);
     }
-
-    glPopMatrix();
-    glDisable(GL_NORMALIZE);
 
   }
 }
 
 /**************************************************************************/
 
-void RectTerrain_GL_Rnr::MkVN(UCIndex_t i, UCIndex_t j)
+void RectTerrain_GL_Rnr::Render(RnrDriver* rd)
 {
-  if(mTerrain->mColSep) {
-    Real_t c = (mTerrain->mP(i,j) - mTerrain->mMinZ) * mTerrain->mColSep /
-      (mTerrain->mMaxZ - mTerrain->mMinZ);
-    Real_t c1 = c - (int)c, c2 = 1 - c1;
-    ZColor col( c2*mTerrain->mMinCol + c1*mTerrain->mMaxCol );
-    glColor4fv(col());
-  }
-  Real_t x, y, z;
+  switch(mTerrain->mRnrMode) {
 
-  x = mTerrain->mOx + i*mTerrain->mDx;
-  y = mTerrain->mOy + j*mTerrain->mDy;
+  case RectTerrain::RM_Histo: {
+    glColor4fv(mTerrain->mMinCol());
+    Int_t minX = 1, maxX = mTerrain->mNx, minY = 1, maxY = mTerrain->mNy;
+    if(mTerrain->bBorder) {
+      --minX; ++maxX; --minY; ++maxY;
+    }
+    glBegin(GL_QUADS);
+    for(Int_t i=minX; i<=maxX; ++i) {
+      for(Int_t j=minY; j<=maxY; ++j) {
+	MkBox(i, j);
+      }
+    }
+    glEnd();
+    break;
+  }
+
+  case RectTerrain::RM_SmoothSquare: {
+    Int_t minX = 1, maxX = mTerrain->mNx, minY = 1, maxY = mTerrain->mNy;
+    if(mTerrain->bBorder) {
+      --minX; ++maxX; --minY; ++maxY;
+    }
+    glEnable(GL_NORMALIZE);
+    glColor4fv(mTerrain->mMinCol());
+    for(Int_t i=minX; i<maxX; ++i) {
+      glBegin(GL_QUAD_STRIP);
+      for(Int_t j=minY; j<=maxY; ++j) {
+	MkVN(i, j); MkVN(i+1, j);
+      }
+      glEnd();
+    }
+    glDisable(GL_NORMALIZE);
+    break;
+  }
+
+  } // end switch Render Mode
+}
+
+/**************************************************************************/
+
+void RectTerrain_GL_Rnr::MkBox(Int_t i, Int_t j)
+{
+  Float_t x, y, z;
+
+  x = (i-1)*mTerrain->mDx;
+  y = (j-1)*mTerrain->mDy;
   z = mTerrain->mP(i,j);
 
-  UCIndex_t il=i,ih=i,jh=j,jl=j;
-  if(i>0) il--; if(i<mTerrain->mNx-1) ih++;
-  if(j>0) jl--; if(j<mTerrain->mNy-1) jh++;
-  Float_t nx[] = { mTerrain->mDx*(ih-il), 0, mTerrain->mP(ih,j)-mTerrain->mP(il,j) };
-  Float_t ny[] = { 0, mTerrain->mDy*(jh-jl), mTerrain->mP(i,jh)-mTerrain->mP(i,jl) };
+  MkCol(z);
+  glNormal3f(0, 0, 1);
+  glVertex3f(x, y, z);
+  glVertex3f(x + mTerrain->mDx, y, z);
+  glVertex3f(x + mTerrain->mDx, y + mTerrain->mDy, z);
+  glVertex3f(x, y + mTerrain->mDy, z);
+}
 
+void RectTerrain_GL_Rnr::MkVN(Int_t i, Int_t j)
+{
+  Float_t x, y, z;
+
+  x = (i-1)*mTerrain->mDx;
+  y = (j-1)*mTerrain->mDy;
+  z = mTerrain->mP(i,j);
+
+  Int_t il=i,ih=i,jh=j,jl=j;
+  if(i>0) il--; if(i<=mTerrain->mNx) ih++;
+  if(j>0) jl--; if(j<=mTerrain->mNy) jh++;
+  Float_t nx[] = { (ih-il)*mTerrain->mDx, 0, mTerrain->mP(ih,j) - mTerrain->mP(il,j) };
+  Float_t ny[] = { 0, (jh-jl)*mTerrain->mDy, mTerrain->mP(i,jh) - mTerrain->mP(i,jl) };
+
+  MkCol(z);
   glNormal3f(-ny[1]*nx[2], -nx[0]*ny[2], nx[0]*ny[1]);
   glVertex3f(x, y, z);
 }
