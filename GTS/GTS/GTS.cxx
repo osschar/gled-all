219ed4ff42d5e1663e24c3d6d57b475674b46de5
Matevž Tadel <osschar@users.noreply.github.com>
2004-06-03 12:33:39 +0000

    Added method GTS::Surface::Replace().

--- GTS/GTS/GTS.cxx
+++ GTS/GTS/GTS.cxx
@@ -1,6 +1,6 @@
 // $Header$
 
-// Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
@@ -82,6 +82,18 @@ GTS::Surface* GTS::Surface::Load(const TString& gtl)
   ms->surface = s;
   return ms;
 }
+
+/**************************************************************************/
+
+void GTS::Surface::Replace(GtsSurface* new_surf)
+{
+  if(surface) {
+    gts_object_destroy (GTS_OBJECT (surface));
+  }
+  surface = new_surf;
+}
+
+/**************************************************************************/
 
 void GTS::Surface::Invert()
 {
