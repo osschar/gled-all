f10dfaa3c3df34764a65f7b0d5692550c010c44a
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-06-03 14:10:22 +0000

    Extension of copyright to 2004.

--- trunk/gled-build/lib-exec/mk_dict_dep.pl
+++ trunk/gled-build/lib-exec/mk_dict_dep.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # $Header$
 
-# Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
+# Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
 # This file is part of GLED, released under GNU General Public License version 2.
 # For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
