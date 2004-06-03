d7c090ad307475a2826984d08989b278a3b04c21
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-06-03 12:23:56 +0000

    Transition to pure ROOT vector/matrix classes.

--- trunk/GledCore/Stones/Z3Point.cxx
+++ trunk/GledCore/Stones/Z3Point.cxx
@@ -1,6 +1,6 @@
 // $Header$
 
-// Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
@@ -10,14 +10,14 @@
 
 ClassImp(Z3Point)
 
-static const ZMatrix Z3PointMatrixProto(3,3);
+static const TMatrixF Z3PointMatrixProto(3,3);
 
 /**************************************************************************/
 
 void Z3Point::_init(const Z3Mark& m, bool Ss_are_sigmas)
 {
   // if Ss_are_sigmas builds inverse of covariance matrix (\1/sigma_x^2 ... \)
-  for(UCIndex_t i=0; i<=2; i++) {
+  for(Int_t i=0; i<=2; i++) {
     mR(i) = m.R(i);
     if(Ss_are_sigmas)
       // watchit !!!! 0 means infinite error !!!!
@@ -48,16 +48,16 @@ void Z3Point::Transform(const ZTrans& t)
   // do tWt^-1, tR w/ 3x3 subset of t
   TMatrix tmp(TMatrix::kZero, Z3PointMatrixProto);
   // W t^-1 ... t^-1 is transpose of t (as ZTrans should be O(4))
-  for(UCIndex_t i=0; i<3; i++)
-    for(UCIndex_t j=0; j<3; j++)
-      for(UCIndex_t k=0; k<3; k++)
+  for(Int_t i=0; i<3; i++)
+    for(Int_t j=0; j<3; j++)
+      for(Int_t k=0; k<3; k++)
 	tmp(i,j) += mW(i,k) * t(j+1u,k+1u);
-  ZVector vmp(mR);
-  for(UCIndex_t i=0; i<3; i++) {
+  TVectorF vmp(mR);
+  for(Int_t i=0; i<3; i++) {
     mR(i) = t(i+1,4u); // Init pos w/ parental move
-    for(UCIndex_t j=0; j<3; j++) {
+    for(Int_t j=0; j<3; j++) {
       mW(i,j) = 0;
-      for(UCIndex_t k=0; k<3; k++)
+      for(Int_t k=0; k<3; k++)
 	mW(i,j) += t(i+1u,k+1u) * tmp(k,j);
       mR(i) += t(i+1u,j+1u) * vmp(j);
     }
@@ -67,11 +67,13 @@ void Z3Point::Transform(const ZTrans& t)
 /**************************************************************************/
 
 #include <iomanip>
+using namespace std;
+
 ostream& operator<<(ostream& s, const Z3Point& p) {
   s.setf(ios::fixed, ios::floatfield); s.precision(3);
-  for(UCIndex_t i=0; i<3; i++) {
+  for(Int_t i=0; i<3; i++) {
     s << p.R(i) << "\t|\t";
-    for(UCIndex_t j=0; j<3; j++)
+    for(Int_t j=0; j<3; j++)
       s << p.W(i,j) << ((j==2) ? "\n" : "\t");
   }
   return s;
--- trunk/GledCore/Stones/ZMark.cxx
+++ trunk/GledCore/Stones/ZMark.cxx
@@ -1,20 +1,22 @@
 // $Header$
 
-// Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
 #include "ZMark.h"
 
+#include <iomanip>
+
 ClassImp(ZMark)
 
-#include <iomanip>
+using namespace std;
 
 ostream& operator<<(ostream& s, ZMark& m) {
   s.setf(ios::fixed, ios::floatfield); s.precision(3);
   s << "X=(";
-  for(UCIndex_t i=0; i<5; i++) s << m.X(i) << ((i==4) ? ")" : ", ");
+  for(Int_t i=0; i<5; i++) s << m.X(i) << ((i==4) ? ")" : ", ");
   s << "\nS=(";
-  for(UCIndex_t i=0; i<5; i++) s << m.S(i) << ((i==4) ? ")" : ", ");
+  for(Int_t i=0; i<5; i++) s << m.S(i) << ((i==4) ? ")" : ", ");
   return s;
 }
