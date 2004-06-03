70a9282eb032f249d22fa36e65d63c64f781b7f5
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-06-03 12:30:38 +0000

    Improvements and extensions of rendering, creation from TH2, custom color gradient for z-value coloration (see ZRibbon).

--- trunk/Geom1/Glasses/RectTerrain.cxx
+++ trunk/Geom1/Glasses/RectTerrain.cxx
@@ -1,6 +1,6 @@
 // $Header$
 
-// Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
@@ -15,6 +15,7 @@
 
 #include "RectTerrain.h"
 #include <Glasses/ZImage.h>
+#include "RectTerrain.c7"
 
 #include <IL/il.h>
 
@@ -22,30 +23,90 @@
 
 ClassImp(RectTerrain)
 
+Float_t RectTerrain::sMaxEpsilon = 0.0001;
+
+/**************************************************************************/
+
 void RectTerrain::_init()
 {
   mNx = mNy = 0;
-  mOx = mOy = 0;
   mDx = mDy = 1;
-  mZScale = 1; mMinZ = 0; mMaxZ = 0;
+
+  mMinZ = 0; mMaxZ = 0;
   mMinCol.gray(0.2); mMaxCol.gray(1);
   mColSep = 1;
-  bStudySize = false;
+  mRibbon = 0;
+
+  mRnrMode    = RM_Histo;
+  mBorderCond = BC_Zero;
+  mOriginMode = OM_Edge;
+  mBValue = 0;
+  bBorder = false;
+
+  mSmoothFac = 0.2;
+
+  bUseDispList = true;
+  bStudySize   = false;
 }
 
 /**************************************************************************/
 
-void RectTerrain::ToStd()
+void RectTerrain::ApplyBorderCondition()
 {
-  mOx = mOy = 0;
-  Stamp(LibID(), ClassID());
-}
+  const Int_t MX = mNx + 1;
+  const Int_t MY = mNy + 1;
+  const Int_t mx = mNx - 1;
+  const Int_t my = mNy - 1;
 
-void RectTerrain::ToCenter()
-{
-  mOx = -0.5 * mDx * mNx;
-  mOy = -0.5 * mDy * mNy;
-  Stamp(LibID(), ClassID());
+  switch (mBorderCond) {
+
+  case BC_Zero:
+  case BC_Const: {
+    const Float_t C = (mBorderCond == BC_Zero) ? 0 : mBValue;
+    mP(0,0) = mP(MX,MY) = C;
+    for(Int_t i=0; i<=MX; ++i) { mP(i, 0) = mP(i, MY) = C; }
+    for(Int_t i=1; i< MY; ++i) { mP(0, i) = mP(MX, i) = C; }
+    break;
+  }
+  case BC_External: {
+    break;
+  }
+  case BC_Equal: {
+    mP(0 ,0) = mP(1,1);   mP( 0,MY) = mP(1,mNy);
+    mP(MX,0) = mP(mNx,1); mP(MX,MY) = mP(mNx,mNy);
+    for(Int_t i=1; i<MX; ++i) { mP(i, 0) = mP(i,1); mP(i, MY) = mP(i,mNy); }
+    for(Int_t i=1; i<MY; ++i) { mP(0, i) = mP(1,i); mP(MX, i) = mP(mNx,i); }
+    break;
+  }
+  case BC_Sym: {
+    mP(0 ,0) = mP(2,2);  mP( 0,MY) = mP(2,my);
+    mP(MX,0) = mP(mx,2); mP(MX,MY) = mP(mx,my);
+    for(Int_t i=1; i<MX; ++i) { mP(i, 0) = mP(i,2); mP(i, MY) = mP(i,my); }
+    for(Int_t i=1; i<MY; ++i) { mP(0, i) = mP(2,i); mP(MX, i) = mP(mx,i); }
+    break;
+  }
+  case BC_ASym: {
+    mP(0 ,0) = 2*mP(1,1)   - mP(2,2);  mP( 0,MY) = 2*mP(1,  mNy) - mP(2,my);
+    mP(MX,0) = 2*mP(mNx,1) - mP(mx,2); mP(MX,MY) = 2*mP(mNx,mNy) - mP(mx,my);
+    for(Int_t i=1; i<MX; ++i) {
+      mP(i, 0) = 2*mP(i,1) - mP(i,2); mP(i, MY) = 2*mP(i,mNy) - mP(i,my);
+    }
+    for(Int_t i=1; i<MY; ++i) {
+      mP(0, i) = 2*mP(1,i) - mP(2,i); mP(MX, i) = 2*mP(mNx,i) - mP(mx,i);
+    }
+    break;
+  }
+  case BC_Wrap: {
+    mP(0, 0) = mP(mNx,mNy); mP(0, MY) = mP(1,mNy);
+    mP(MX,0) = mP(1,  mNy); mP(MX,MY) = mP(1,1);
+    for(Int_t i=1; i<MX; ++i) { mP(i, 0) = mP(i,mNy); mP(i, MY) = mP(i,1); }
+    for(Int_t i=1; i<MY; ++i) { mP(0, i) = mP(mNx,i); mP(MX, i) = mP(1,i); }
+    break;
+  }
+
+  } // end switch mBCond
+
+  mStampReqTring = Stamp(LibID(), ClassID());
 }
 
 /**************************************************************************/
@@ -60,12 +121,13 @@ void RectTerrain::SetFromImage(ZImage* image)
   ZImage::sILMutex.Unlock();
 
   mNx = w; mNy = h;
-  mP.ResizeTo(mNx, mNy);
+  mP.Clear();
+  mP.ResizeTo(mNx + 2, mNy + 2);
   mMinZ = 1e9; mMaxZ = -1e9;
 
-  for(UInt_t y=0; y<mNy; ++y) {
-    ILushort* bar = &(data[(mNy-y-1)*w]);
-    for(UInt_t x=0; x<mNx; ++x) {
+  for(Int_t y=1; y<=mNy; ++y) {
+    ILushort* bar = &(data[(mNy-y)*w]);
+    for(Int_t x=1; x<=mNx; ++x) {
       const float z = (float)(*(bar++));
       if(z > mMaxZ) mMaxZ = z;
       if(z < mMinZ) mMinZ = z;
@@ -77,9 +139,9 @@ void RectTerrain::SetFromImage(ZImage* image)
   float zoffset = 0; // set z = 0 at 0
   float zfac = 1;    // scale not
 
-  float delta = 1.001*(mMaxZ - mMinZ);
-  for(UInt_t x=0; x<mNx; ++x) {
-    for(UInt_t y=0; y<mNy; ++y) {
+  float delta = (1 + sMaxEpsilon)*(mMaxZ - mMinZ);
+  for(Int_t x=1; x<=mNx; ++x) {
+    for(Int_t y=1; y<=mNy; ++y) {
       mP(x,y) = ( (mP(x,y) - mMinZ)/delta - zoffset ) * zfac;
     }
   }
@@ -90,9 +152,106 @@ void RectTerrain::SetFromImage(ZImage* image)
     mMaxZ = -zoffset * zfac; mMinZ = (1 - zoffset) * zfac;
   }
 
-  Stamp(LibID(), ClassID()); 
+  mStampReqTring = Stamp(LibID(), ClassID());
+}
+
+void RectTerrain::SetFromHisto(TH2* histo)
+{
+  mNx = histo->GetNbinsX(); mNy = histo->GetNbinsY();
+  const Int_t MX = mNx + 1, MY = mNy + 1;
+  mP.Clear();
+  mP.ResizeTo(mNx+2, mNy+2);
+  mMinZ = 1e9; mMaxZ = -1e9;
+
+  for(Int_t x=0; x<=MX; ++x) {
+    for(Int_t y=0; y<=MY; ++y) {
+      mP(x,y) = histo->GetBinContent(x,y);
+    }
+  }
+  mMinZ = histo->GetMinimum();
+  mMaxZ = histo->GetMaximum();
+
+  float zoffset = 0; // set z = 0 at 0
+  float zfac = 1;    // scale not
+
+  float delta = 1.001*(mMaxZ - mMinZ);
+  for(Int_t x=0; x<=MX; ++x) {
+    for(Int_t y=0; y<=MY; ++y) {
+      mP(x,y) = ( (mP(x,y) - mMinZ)/delta - zoffset ) * zfac;
+    }
+  }
+  
+  if(zfac > 0) {
+    mMinZ = -zoffset * zfac; mMaxZ = (1 - zoffset) * zfac;
+  } else {
+    mMaxZ = -zoffset * zfac; mMinZ = (1 - zoffset) * zfac;
+  }
+
+  mStampReqTring = Stamp(LibID(), ClassID());
 }
 
 /**************************************************************************/
 
-#include "RectTerrain.c7"
+void RectTerrain::Smooth(Float_t fac)
+{
+  TMatrixF M(mP);
+  Double_t fa = (fac == 0) ? mSmoothFac : fac;
+  Double_t fb = fa/TMath::Sqrt(2.0);
+  Double_t normfac = 1/(1 + 4*fa + 4*fb);
+  for(Int_t i=1; i<=mNx; ++i) {
+    for(Int_t j=1; j<=mNy; ++j) {
+      mP(i,j) = (M(i,j) + fa*(M(i-1,j) + M(i+1,j) + M(i,j-1) + M(i,j+1)) +
+                      fb*(M(i-1,j-1) + M(i+1,j+1) + M(i+1,j-1) + M(i-1,j+1))
+		 ) * normfac;
+    }
+  }
+  mStampReqTring = Stamp(LibID(), ClassID());
+}
+
+void RectTerrain::RecalcMinMax()
+{
+  mMinZ = 1e9; mMaxZ = -1e9;
+  for(Int_t i=1; i<=mNx; ++i) {
+    for(Int_t j=1; j<=mNy; ++j) {
+      if(mP(i,j) > mMaxZ) mMaxZ = mP(i,j);
+      if(mP(i,j) < mMinZ) mMinZ = mP(i,j);
+    }
+  }
+  mMaxZ *= (1 + sMaxEpsilon);
+  mStampReqTring = Stamp(LibID(), ClassID());
+}
+
+/**************************************************************************/
+
+void RectTerrain::ReTring()
+{
+  mStampReqTring = Stamp(LibID(), ClassID());
+}
+
+#include <TRandom.h>
+#include <TVirtualMutex.h>
+
+void RectTerrain::Boobofy()
+{
+  // Must set Ribbon to booby.pov for best effect.
+
+  TH2F* h=new TH2F("Booboo","exampul",128,-5,5,64,-2.5,2.5);
+  TRandom r;
+  for(int i=0;i<1000000;++i) {
+    h->Fill(r.Gaus() - 2, r.Gaus());
+    h->Fill(r.Gaus() + 2, r.Gaus());
+  }
+  for(int i=0;i<6000;++i) {
+    h->Fill(0.1*r.Gaus() - 2, 0.1*r.Gaus());
+    h->Fill(0.1*r.Gaus() + 2, 0.1*r.Gaus());
+  }
+  {
+    R__LOCKGUARD(gCINTMutex);
+    h->Draw("LEGO2");
+  }
+  SetFromHisto(h);
+  for(int i=0; i<5; ++i) Smooth(1);
+  RecalcMinMax();
+}
+
+/**************************************************************************/
--- trunk/Geom1/Rnr/GL/RectTerrain_GL_Rnr.h
+++ trunk/Geom1/Rnr/GL/RectTerrain_GL_Rnr.h
@@ -4,22 +4,44 @@
 
 #include <Glasses/RectTerrain.h>
 #include <Rnr/GL/ZNode_GL_Rnr.h>
-#include <FL/gl.h>
 
 class RectTerrain_GL_Rnr : public ZNode_GL_Rnr {
-private:
-  void check(double,double,GLdouble*,GLdouble*,GLint*);
 
 protected:
   RectTerrain*		mTerrain;
 
-  void MkVN(UCIndex_t i, UCIndex_t j);
+  void MkBox(Int_t i, Int_t j);
+  void MkVN(Int_t i, Int_t j);
+  ZColor MkCol(Float_t z);
+
+  void check(double,double,GLdouble*,GLdouble*,GLint*);
 
 public:
-  RectTerrain_GL_Rnr(RectTerrain* rt);
+  RectTerrain_GL_Rnr(RectTerrain* rt) :
+    ZNode_GL_Rnr(rt), mTerrain(rt) {}
 
   virtual void Draw(RnrDriver* rd);
 
+  virtual void Render(RnrDriver* rd);
+
 }; // endclass RectTerrain_GL_Rnr
+
+/**************************************************************************/
+
+inline ZColor RectTerrain_GL_Rnr::MkCol(Float_t z)
+{
+  if(mTerrain->mColSep) {
+    Float_t c = (z - mTerrain->mMinZ) * mTerrain->mColSep /
+	(mTerrain->mMaxZ - mTerrain->mMinZ);
+    c -= (int)c;
+    if(mTerrain->mRibbon) {
+      glColor4fv(mTerrain->mRibbon->MarkToCol(c)());
+    } else {
+      Float_t c1 = c - (int)c, c2 = 1 - c1;
+      ZColor col( (1 - c)*mTerrain->mMinCol + c*mTerrain->mMaxCol );
+      glColor4fv(col());
+    }
+  }
+}
 
 #endif
