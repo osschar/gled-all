ca21666e80140345d2a9d04a780182d7aa42bc1c
Matevž Tadel <osschar@users.noreply.github.com>
2004-11-30 19:33:05 +0000

    New files; assists dumping of a Pupil to a tga file.

---
+++ GledCore/Glasses/ScreenDumper.cxx
@@ -0,0 +1,36 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// ScreenDumper
+//
+//
+
+#include "ScreenDumper.h"
+
+#include "ScreenDumper.c7"
+
+ClassImp(ScreenDumper)
+
+/**************************************************************************/
+
+void ScreenDumper::_init()
+{
+  mPupil       = 0;
+  mFileNameFmt = "screendumper/img%04d.tga";
+}
+
+/**************************************************************************/
+
+void ScreenDumper::Operate(Operator::Arg* op_arg) throw(Exception)
+{
+  PreOperate(op_arg);
+  if(mPupil)
+    mPupil->EmitDumpImageRay(GForm(mFileNameFmt.Data(), op_arg->fEventID));
+  PostOperate(op_arg);
+}
+
+/**************************************************************************/
