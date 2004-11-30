7dc99327f5a63da03ae8d2db0def382f75676967
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-11-30 19:32:17 +0000

    New files; implementation of MethodCallWidget.

---
+++ trunk/GledCore/Eye/MCW_View.h
@@ -0,0 +1,103 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledCore_MCW_View_H
+#define GledCore_MCW_View_H
+
+#include <Gled/GledTypes.h>
+#include <Gled/GledNS.h>
+#include "OptoStructs.h"
+class FTW_Shell;
+
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Pack.H>
+#include <FL/Fl_SWM.H>
+class Fl_SWM_Manager;
+
+class MCW_View : public Fl_Window, public Fl_SWM_Client
+{
+
+ public:
+
+  class ArgBase : public Fl_Pack
+  {
+  public:
+    string         _typ, _base_typ, _name;
+
+    ArgBase(string& typ, string& base_typ,
+	    string& name, string& defval, int cell_w);
+
+    virtual void StreamData(TBuffer& b) {}
+
+    void insert_box(int w, const char* l);
+  };
+
+  class CtxArg  : public ArgBase
+  {
+  public:
+    FID_t	_carg_fid;
+
+    CtxArg(string& typ, string& base_typ,
+	   string& name, string& defval, int cell_w);
+
+    void SetImage(OptoStructs::ZGlassImg* img);
+    OptoStructs::ZGlassImg* GetImage();
+
+    static void s_Clr_cb(Fl_Widget* w, CtxArg* ud)
+    { ud->SetImage(0); }
+  };
+
+  class VarArg  : public ArgBase
+  {
+  protected:
+    int  _typ_idx;
+
+  public:
+    VarArg(string& typ, string& base_typ,
+	   string& name, string& defval, int cell_w);
+    
+    virtual void StreamData(TBuffer& b);
+
+    void create_input_weed();
+  };
+
+ protected:
+  string		mTitle;
+
+  FTW_Shell*		mShell;   // X{G}
+  GledNS::MethodInfo*	mMInfo;
+
+  Fl_Pack*		mPack;
+  Fl_Pack*		mCtxPack;
+  Fl_Pack*		mVarPack;
+
+  Fl_Button*		bSendOnChange;
+
+  void deduce_max_widths(Fl_Group* g, int *wmaxs);
+  void apply_max_widths(Fl_Group* g, int *wmaxs);
+  void resize_arg_weeds(Fl_Group* g, int dw);
+
+ public:
+  MCW_View(FTW_Shell* shell);
+
+  void ParseMethodInfo(GledNS::MethodInfo* mi) throw(string);
+  void SetABG(ZGlass* alpha, ZGlass* beta=0, ZGlass* gamma=0);
+
+  void Send();
+  void ChangeCallback();
+
+  virtual void resize(int X, int Y, int W, int H);
+  virtual int  handle(int ev);
+
+  static void s_Send_cb(Fl_Widget* w, MCW_View* ud)     { ud->Send(); };
+  static void s_Change_cb(Fl_Widget* w, MCW_View* ud)   { ud->ChangeCallback(); };
+  static void s_Exit_cb(Fl_Widget* w, MCW_View* ud)     { delete ud; };
+  static void s_SendExit_cb(Fl_Widget* w, MCW_View* ud) { ud->Send(); delete ud; };
+
+#include "MCW_View.h7"
+}; // endclass MCW_View
+
+#endif
