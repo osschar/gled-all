4b742693bf2a6d126cee5810ba51d0e7d986545d
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-11-30 19:33:05 +0000

    New files; assists dumping of a Pupil to a tga file.

---
+++ trunk/GledCore/Glasses/ScreenDumper.h
@@ -0,0 +1,35 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledCore_ScreenDumper_H
+#define GledCore_ScreenDumper_H
+
+#include <Glasses/Operator.h>
+#include <Glasses/PupilInfo.h>
+
+class ScreenDumper : public Operator {
+  // 7777 RnrCtrl(0)
+  MAC_RNR_FRIENDS(ScreenDumper);
+
+private:
+  void _init();
+
+protected:
+  PupilInfo*	mPupil; 	// X{GS} L{}
+  TString	mFileNameFmt;	// X{GS} 7 Textor()
+  
+public:
+  ScreenDumper(const Text_t* n="ScreenDumper", const Text_t* t=0) : Operator(n,t) { _init(); }
+
+  virtual void Operate(Operator::Arg* op_arg)     throw(Operator::Exception);
+
+#include "ScreenDumper.h7"
+  ClassDef(ScreenDumper, 1)
+}; // endclass ScreenDumper
+
+GlassIODef(ScreenDumper);
+
+#endif
