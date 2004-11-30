5e79ad97257ebe635459a2ff7e47149e6b700c4c
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-11-30 19:31:57 +0000

    Preparations for new AList implementation, addition of custom buffers and user_rays.

--- trunk/GledCore/Net/Ray.cxx
+++ trunk/GledCore/Net/Ray.cxx
@@ -10,97 +10,146 @@
 // Ray contains stamping information. Rays are sent from Saturn to Eye.
 // RayQN_e (Ray Quantum Number) lists known event types.
 //
+// Current implementation casts ZGlass* to UInt_t upon streaming.
+// !!!! This needs to be fixed for 64-bit compiles with Views.
+// Can easily implement sizeof(void*) streamer ...
+// Could as well make Ray into TMessage in the process ... or sth.
 
 #include "Ray.h"
 #include <Glasses/ZGlass.h>
 #include <TBuffer.h>
 
-void Ray::Streamer(TBuffer &buf)
-{
-  if(buf.IsReading()) {
-    unsigned char uc;
-    UInt_t caller=0;
-    UInt_t a=0, b=0, g=0;
-    buf >> uc;
-    fEvent = (RayQN_e)uc;
-    fStamp = 0;
-    switch(fEvent) {
-    case RQN_change:
-    case RQN_link_change:  GetSLC(buf); buf >> a; break;
-    case RQN_list_add:     GetSLC(buf); buf >> a >> b >> g; break;
-    case RQN_list_remove:  GetSLC(buf); buf >> a >> b; break;
-    case RQN_list_rebuild: GetSLC(buf); buf >> a; break;
-    case RQN_list_clear:   GetSLC(buf); buf >> a; break;
-    case RQN_birth:	   GetSLC(buf); buf >> a; break;
-    case RQN_death:	   GetSLC(buf); buf >> a; break;
-    case RQN_message:
-    case RQN_error:	   buf >> caller >> fMessage; break;
-    case RQN_apocalypse:   break;
-    }
-    fCaller = (ZGlass*)caller;
-    fAlpha = (ZGlass*)a; fBeta = (ZGlass*)b; fGamma = (ZGlass*)g;
-    ISdebug(9, GForm("Ray::Streamer Read  %u %u", fAlpha ? fAlpha->GetSaturnID() : 0, fStamp));
-  } else {
-    buf << ((unsigned char)fEvent);
-    switch(fEvent) {
-    case RQN_change:
-    case RQN_link_change:  PutSLC(buf); buf << UInt_t(fAlpha); break;
-    case RQN_list_add:     PutSLC(buf); buf << UInt_t(fAlpha) << UInt_t(fBeta) << UInt_t(fGamma); break;
-    case RQN_list_remove:  PutSLC(buf); buf << UInt_t(fAlpha) << UInt_t(fBeta); break;
-    case RQN_list_rebuild: PutSLC(buf); buf << UInt_t(fAlpha); break;
-    case RQN_list_clear:   PutSLC(buf); buf << UInt_t(fAlpha); break;
-    case RQN_birth:	   PutSLC(buf); buf << UInt_t(fAlpha); break;
-    case RQN_death:	   PutSLC(buf); buf << UInt_t(fAlpha); break;
-    case RQN_message:
-    case RQN_error:        buf << UInt_t(fCaller) << fMessage; break;
-    case RQN_apocalypse:   break;
-    }
-    ISdebug(9, GForm("Ray::Streamer Wrote %u %u", fAlpha ? fAlpha->GetSaturnID() : 0, fStamp));
+/**************************************************************************/
+
+namespace RayNS {
+
+  void PutPTR(TBuffer& b, ZGlass*& p) {
+    b.WriteFastArray((Char_t*)(&p), sizeof(void*));
+  }
+  void GetPTR(TBuffer& b, ZGlass*& p) {
+    b.ReadFastArray((Char_t*)(&p), sizeof(void*));
   }
 }
 
-void Ray::PutSLC(TBuffer &buf) { buf << fStamp << fLibID << fClassID; }
+/**************************************************************************/
+// Ray
+/**************************************************************************/
 
-void Ray::GetSLC(TBuffer &buf) { buf >> fStamp >> fLibID >> fClassID; }
+Ray::~Ray()
+{
+  if(fRayBits & RB_CustomBuffer)
+    delete fCustomBuffer;
+}
 
+/**************************************************************************/
+// Ray - Writer side
+/**************************************************************************/
+
+void Ray::Write(TBuffer& b)
+{
+  using namespace RayNS;
+  PutPTR(b, fAlpha);
+  b << fRQN << fEyeBits;
+  b << fStamp << fLibID << fClassID;
+  b << fRayBits;
+  if(fRayBits & RB_Beta)       PutPTR(b, fBeta);
+  if(fRayBits & RB_Gamma)      PutPTR(b, fGamma);
+  if(fRayBits & RB_BetaId)     b << fBetaId;
+  if(fRayBits & RB_GammaId)    b << fGammaId;
+  if(fRayBits & RB_BetaLabel)  b << fBetaLabel;
+  if(fRayBits & RB_GammaLabel) b << fGammaLabel;
+  if(fRayBits & RB_CustomBuffer) {
+    b << fCustomLen;
+    b.WriteFastArray(fCustomBuffer->Buffer(), fCustomLen);
+  } 
+}
+
+void Ray::SetCustomBuffer(TBuffer& b)
+{
+  fCustomLen = b.Length();
+  fCustomBuffer = new TBuffer(TBuffer::kWrite, fCustomLen, b.Buffer(), kTRUE);
+  b.DetachBuffer();
+  fCustomBuffer->SetBufferOffset(fCustomLen);
+  fRayBits |= RB_CustomBuffer;
+}
+
+/**************************************************************************/
+// Ray - Reader side
+/**************************************************************************/
+
+void Ray::Read(TBuffer& b)
+{
+  using namespace RayNS;
+  b >> fRQN >> fEyeBits;
+  b >> fStamp >> fLibID >> fClassID;
+  b >> fRayBits;
+  if(fRayBits & RB_Beta)       GetPTR(b, fBeta);  else fBeta  = 0;
+  if(fRayBits & RB_Gamma)      GetPTR(b, fGamma); else fGamma = 0;
+  if(fRayBits & RB_BetaId)     b >> fBetaId;  else  fBetaId = 0;
+  if(fRayBits & RB_GammaId)    b >> fGammaId; else fGammaId = 0;
+  if(fRayBits & RB_BetaLabel)  b >> fBetaLabel;
+  if(fRayBits & RB_GammaLabel) b >> fGammaLabel;
+  if(fRayBits & RB_CustomBuffer) {
+    b >> fCustomLen;
+    fCustomBuffer = new TBuffer(TBuffer::kRead, fCustomLen, b.Buffer() + b.Length(), kFALSE);
+  } else {
+    fCustomLen = 0; fCustomBuffer = 0;
+  }
+}
+
+void Ray::ResetCustomBuffer()
+{
+  fCustomBuffer->SetBufferOffset(0);
+}
+
+/**************************************************************************/
+// Print-out / debug
 /**************************************************************************/
 
 const char* Ray::EventName() const
 {
-  switch(fEvent) {
+  using namespace RayNS;
+
+  switch(fRQN) {
   case RQN_change:	return "change";
   case RQN_link_change: return "link_change";
   case RQN_list_add:    return "list_add";
   case RQN_list_remove: return "list_remove";
   case RQN_list_rebuild:return "list_rebuild";
   case RQN_list_clear:  return "list_clear";
-  case RQN_birth:	return "birth";
   case RQN_death:	return "death";
-  case RQN_message:     return "message";
-  case RQN_error:       return "error";
-  case RQN_apocalypse:  return "apocalypse";
   }
   return "<unknown>";
 }
 
 void Ray::Dump(ostream& s) const {
   s << EventName() <<" "<< fStamp << " ";
-  switch(fEvent) {
-  case RQN_change:
-  case RQN_link_change: s << fAlpha->GetSaturnID() <<","<< fLibID <<","<< fClassID; break;
-  case RQN_list_add:    s << fAlpha->GetSaturnID() <<","<< fBeta->GetSaturnID() <<","<< (fGamma ? fGamma->GetSaturnID() : 0); break;
-  case RQN_list_remove: s << fAlpha->GetSaturnID() <<","<< fBeta->GetSaturnID(); break;
-  case RQN_list_rebuild:s << fAlpha->GetSaturnID(); break;
-  case RQN_list_clear:  s << fAlpha->GetSaturnID(); break;
-  case RQN_birth:	s << fAlpha->GetSaturnID(); break;
-  case RQN_death:	s << fAlpha->GetSaturnID(); break;
-  case RQN_message:
-  case RQN_error:       s << fCaller->GetSaturnID() <<","<< fMessage; break;
-  case RQN_apocalypse:  break;
-  }
+  s << fAlpha->GetSaturnID() <<", ("<< fLibID <<","<< fClassID <<")";
 }
 
 ostream& operator<<(ostream& s, const Ray& r) {
   r.Dump(s);
   return s;
+}
+
+
+/**************************************************************************/
+/**************************************************************************/
+// TextMessage
+/**************************************************************************/
+
+void TextMessage::Streamer(TBuffer& buf)
+{
+  if(buf.IsReading()) {
+    UShort_t t;
+    RayNS::GetPTR(buf, fCaller);
+    buf >> t >> fMessage;
+    fType = (Type_e)t;
+    ISdebug(9, GForm("TextMessage::Streamer Read  %u %d %s", fCaller ? fCaller->GetSaturnID() : 0, fType, fMessage.Data()));
+  } else {
+    RayNS::PutPTR(buf, fCaller);
+    buf << (UShort_t)fType << fMessage;
+
+    ISdebug(9, GForm("TextMessage::Streamer Wrote %u %d %s", fCaller ? fCaller->GetSaturnID() : 0, fType, fMessage.Data()));
+  }
 }
