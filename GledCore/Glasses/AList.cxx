cd7d005ad80fce8d8e80b556d621f63af06de2c9
Matevž Tadel <osschar@users.noreply.github.com>
2004-11-30 19:33:09 +0000

    New files; preparation for new container implementation.

---
+++ GledCore/Glasses/AList.cxx
@@ -0,0 +1,76 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// AList
+//
+//
+
+#include "AList.h"
+
+#include "AList.c7"
+
+ClassImp(AList::ElRep)
+
+ClassImp(AList)
+
+/**************************************************************************/
+
+void AList::_init()
+{
+  // !!!! Set all links to 0 !!!!
+}
+
+/**************************************************************************/
+
+void AList::new_element_check(ZGlass* g)
+{
+  if(g == 0) {
+    throw(string("ZList::new_element_check called with null ZGlass*"));
+  }
+  if(mLid && mCid) {
+    if(!GledNS::IsA(g, FID_t(mLid, mCid))) {
+      throw(string("AList::new_element_check lens of wrong FID_t"));
+    }
+  }
+}
+
+/**************************************************************************/
+
+Int_t AList::RebuildAllRefs(An_ID_Demangler* idd)
+{
+  return RebuildLinkRefs(idd) + RebuildListRefs(idd);
+}
+
+/**************************************************************************/
+
+void AList::Streamer(TBuffer &b)
+{
+  static const string _eh("AList::Streamer ");
+  UInt_t R__s, R__c;
+
+  if(b.IsReading()) {
+
+    Version_t v = b.ReadVersion(&R__s, &R__c);
+    ZGlass::Streamer(b);
+    b >> mSize >> mLid >> mCid;
+    ISdebug(D_STREAM, GForm("%sreading %d elements (%d,%d).",
+			    _eh.c_str(), mSize, mLid, mCid));
+   b.CheckByteCount(R__s, R__c, AList::IsA());
+
+  } else {
+
+    R__c = b.WriteVersion(AList::IsA(), kTRUE);
+    ZGlass::Streamer(b);
+    b << mSize << mLid << mCid;
+    ISdebug(D_STREAM, GForm("%swriting %d elements (%d,%d).",
+			    _eh.c_str(), mSize, mLid, mCid));
+    b.SetByteCount(R__c, kTRUE);
+
+  }
+}
+
+/**************************************************************************/
---
+++ GledCore/Glasses/AList.h
@@ -0,0 +1,129 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledCore_AList_H
+#define GledCore_AList_H
+
+#include <Glasses/ZGlass.h>
+
+
+/**************************************************************************/
+// AList - abstract base glass for collections of lens references         //
+/**************************************************************************/
+
+class AList : public ZGlass {
+  // 7777 RnrCtrl(0)
+  MAC_RNR_FRIENDS(AList);
+
+ public:
+
+  /************************************************************************/
+  // ElRep - list-element representation; needed for list ops and GUI     //
+  /************************************************************************/
+
+  class ElRep {
+  public:
+    ZGlass*	fLens;
+
+    ElRep() : fLens(0) {}
+
+    virtual ZGlass* get_lens()  { return fLens; }
+    virtual Int_t   get_id()    { return 0; }
+    virtual TString get_label() { return 0; }
+
+    ClassDef(ElRep, 1)
+  };
+
+  typedef list<ElRep>		lElRep_t;
+  typedef list<ElRep>::iterator	lElRep_i;
+
+ private:
+  void _init();
+
+ protected:
+  Int_t			mSize;
+  LID_t			mLid;			// X{GS} 7 Value(-join=>1)
+  CID_t			mCid;			// X{GS} 7 Value()
+
+  GMutex	 	mListMutex;	   	//!
+
+  virtual void new_element_check(ZGlass* g);
+
+ public:
+  AList(const Text_t* n="AList", const Text_t* t=0) :
+    ZGlass(n,t), mListMutex(GMutex::recursive) { _init(); }
+
+  virtual void CopyList(lpZGlass_t& dest)     = 0;
+  virtual void CopyListElRefs(lElRep_t& dest) = 0;
+
+
+  virtual Int_t RebuildAllRefs(An_ID_Demangler* idd);
+  virtual Int_t RebuildListRefs(An_ID_Demangler* idd) = 0;
+
+  //----------------------------------------------------------------------
+  // List element handling operations.
+  // Not declared here, as there is no unique interface.
+  // GUI relies on naming conventions!
+  //----------------------------------------------------------------------
+
+  // Deque interface
+  // Pop methods *must* return the poped element if ResultReq is set.
+  // virtual void    PushBack(ZGlass* lens);  // X{E} C{1}
+  // virtual ZGlass* PopBack();               // X{E} C{0}
+  // virtual void    PushFront(ZGlass* lens); // X{E} C{1}
+  // virtual ZGlass* PopFront();              // X{E} C{0}
+
+  // List interface; requires element id.
+  // virtual void AddBefore(ZGlass* lens, Int_t before_id); // X{E} C{1}
+
+  // By-lens interface;
+  // virtual void Add(ZGlass* lens);       // X{E} C{1}
+  // virtual void Remove(ZGlass* lens);    // X{E} C{1}
+
+  // By-id interface; requires element id
+  // virtual void SetElementById(ZGlass* new_el, Int_t target_id);  // X{E} C{1}
+  // virtual void ClearElementById(Int_t target_id);                // X{E} C{0}
+
+  // By-label interface: requires element label
+  // virtual void SetElementByLabel(ZGlass* new_el, TString label); // X{E} C{1}
+  // virtual void ClearElementByLabel(TString label);               // X{E} C{0}
+
+  // Change-element-label: requires editable labels
+  // virtual void ChangeElementLabel(TString old_label, TString new_label); // X{E} C{0}
+
+  //----------------------------------------------------------------------
+  // ElRep properties and supported operations
+  //----------------------------------------------------------------------
+
+  virtual bool elrep_has_id()         { return false; }
+  virtual bool elrep_has_label()      { return false; }
+  virtual bool elrep_can_edit_label() { return false; }
+
+  virtual bool list_deque_ops()       { return false; }
+  virtual bool list_list_ops()        { return false; }
+  virtual bool list_by_lens_ops()     { return false; }
+  virtual bool list_by_id_ops()       { return false; }
+  virtual bool list_by_label_ops()    { return false; }
+
+  //----------------------------------------------------------------------
+  // Stamping methods ... coded here for all sub-classes
+  //----------------------------------------------------------------------
+
+  // just copied from ZList ... think labels and set/clear
+  /*
+  virtual TimeStamp_t	StampListAdd(ZGlass* g, ZGlass* b4);
+  virtual TimeStamp_t	StampListRemove(ZGlass* g);
+  virtual TimeStamp_t	StampListRebuild();
+  virtual TimeStamp_t	StampListClear();
+  */
+
+#include "AList.h7"
+  ClassDef(AList, 1)
+    }; // endclass AList
+
+GlassIODef(AList);
+
+#endif
