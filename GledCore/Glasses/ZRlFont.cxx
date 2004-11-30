d156122b97bd96f5e08f29af0c2ef76d51070b16
Matevž Tadel <osschar@users.noreply.github.com>
2004-11-30 19:33:07 +0000

    New files; preliminary support for multiple GL fonts and management of name-tag rendering.

---
+++ GledCore/Glasses/ZRlFont.cxx
@@ -0,0 +1,29 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// ZRlFont
+//
+//
+
+#include "ZRlFont.h"
+
+#include "ZRlFont.c7"
+
+ClassImp(ZRlFont)
+
+/**************************************************************************/
+
+void ZRlFont::_init()
+{
+  // !!!! Set all links to 0 !!!!
+  mFontName = "default";
+}
+
+/**************************************************************************/
+
+
+/**************************************************************************/
