f63b1b61ee71b764c03c9bd5f7e18073b94a046e
Matevž Tadel <osschar@users.noreply.github.com>
2004-12-01 13:37:25 +0000

    Added function MakeDefaultSurface().

--- GTS/GTS/GTS.cxx
+++ GTS/GTS/GTS.cxx
@@ -4,3 +4,12 @@
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
+#include "GTS.h"
+
+using namespace GTS;
+
+GtsSurface* GTS::MakeDefaultSurface()
+{
+  return gts_surface_new(gts_surface_class (), gts_face_class (),
+			 gts_edge_class (),    gts_vertex_class ());
+}
