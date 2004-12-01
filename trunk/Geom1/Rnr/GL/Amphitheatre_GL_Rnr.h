64df2f38a7d8e180175003bd394050759b535f9c
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-12-01 11:16:36 +0000

    New files: implementation of amphitheatre for placing of ZNode descendants.

---
+++ trunk/Geom1/Rnr/GL/Amphitheatre_GL_Rnr.h
@@ -0,0 +1,33 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef Geom1_Amphitheatre_GL_RNR_H
+#define Geom1_Amphitheatre_GL_RNR_H
+
+#include <Glasses/Amphitheatre.h>
+#include <Rnr/GL/ZNode_GL_Rnr.h>
+
+#include <GL/glu.h>
+
+class Amphitheatre_GL_Rnr : public ZNode_GL_Rnr {
+private:
+  void _init();
+
+protected:
+  Amphitheatre*	 mAmphitheatre;
+  GLUquadricObj* mQuadric;
+
+public:
+  Amphitheatre_GL_Rnr(Amphitheatre* idol) :
+    ZNode_GL_Rnr(idol), mAmphitheatre(idol) { _init(); }
+
+  //virtual void PreDraw(RnrDriver* rd);
+  virtual void Draw(RnrDriver* rd);
+  //virtual void PostDraw(RnrDriver* rd);
+
+}; // endclass Amphitheatre_GL_Rnr
+
+#endif
