9147b058d69d63789ebbd89313ec468d7fea920d
Matevž Tadel <osschar@users.noreply.github.com>
2004-12-08 16:22:11 +0000

    Formatting changes.

--- GledCore/FL/Fl_PhonyEnum.h
+++ GledCore/FL/Fl_PhonyEnum.h
@@ -4,8 +4,8 @@
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
-#ifndef Gled_Fl_PhonyEnum
-#define Gled_Fl_PhonyEnum
+#ifndef GledCore_Fl_PhonyEnum
+#define GledCore_Fl_PhonyEnum
 
 #include <FL/Fl_Choice.H>
 #include <map>
@@ -13,13 +13,18 @@
 class Fl_PhonyEnum : public Fl_Choice {
 protected:
   std::map<int,int>	mVal2Idx;
+  bool			bOwnMap;
   int			mTrueVal; // X{gs}
   int			mHiIdx;
+
 public:
   Fl_PhonyEnum(int x=0, int y=0, int w=200,int h=80, const char* l= 0);
-#include "Fl_PhonyEnum.h7"
+
   void Bruh(int val, const char* label);
   void Update(int val);
+
+#include "Fl_PhonyEnum.h7"
+
 }; // endclass Fl_PhonyEnum
 
 #endif
