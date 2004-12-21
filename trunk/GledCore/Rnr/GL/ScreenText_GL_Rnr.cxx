64cf13bb108961ca3baa4ba023c0820e3b5a674c
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2004-12-21 19:18:04 +0000

    New glass ScreenText.

---
+++ trunk/GledCore/Rnr/GL/ScreenText_GL_Rnr.cxx
@@ -0,0 +1,28 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#include "ScreenText_GL_Rnr.h"
+#include <GledView/GLTextNS.h>
+#include <FL/gl.h>
+
+/**************************************************************************/
+
+void ScreenText_GL_Rnr::_init()
+{}
+
+/**************************************************************************/
+
+//void ScreenText_GL_Rnr::PreDraw(RnrDriver* rd)
+//{}
+
+void ScreenText_GL_Rnr::Draw(RnrDriver* rd)
+{
+  ScreenText& T = *mScreenText;
+  GLTextNS::RnrTextAt(rd, T.GetText(), T.mX, T.mY, T.mZ, T.PtrFgCol(), T.PtrBgCol());
+}
+
+//void ScreenText_GL_Rnr::PostDraw(RnrDriver* rd)
+//{}
