0ad6786f70f42d800b30db6d123b3a480d19d6f7
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-02-01 18:27:40 +0000

    Moved create_smorphs() to rt_functions.cxx which is compiled and loaded as a shared library.

--- trunk/demos/Geom1/remove_test.C
+++ trunk/demos/Geom1/remove_test.C
@@ -9,12 +9,9 @@
 #include <glass_defines.h>
 #include <gl_defines.h>
 
-class RndSMorphCreator;
+#include "rt_functions.h"
 
-ZNode**           subnodes = 0;
-Eventor*          eventor = 0;
-RndSMorphCreator* smorph_creator = 0;
-ZNode*            top_node = 0;
+class RndSMorphCreator;
 
 void remove_test(Int_t NSN = 32)
 {
@@ -22,6 +19,8 @@ void remove_test(Int_t NSN = 32)
     gROOT->Macro("sun.C");
   }
   Gled::theOne->AssertLibSet("Geom1");
+  gSystem->Exec("make rt_functions.so");
+  gSystem->Load("rt_functions.so");
 
   Scene* images  = new Scene("Images");
   scenes->CheckIn(images);
@@ -31,7 +30,6 @@ void remove_test(Int_t NSN = 32)
 
   CREATE_ADD_GLASS(rs_node, ZNode, images, "RndSmorph Node", 0);
   top_node = rs_node;
-
 
   // Eventor and smorph creator.
 
@@ -51,33 +49,4 @@ void remove_test(Int_t NSN = 32)
   // Spawn GUI
   gROOT->LoadMacro("eye.C");
   eye("Eyeeous", "Shellious", 0);
-}
-
-void create_smorphs(Int_t NSN = 32, Int_t NNN = 1024, Int_t SMS = 0)
-{
-  // NSN - number of sub-nodes, each of them receives NNN SMorphs
-  // NNN - number of SMorphs to put into each sub-node
-  // SMS - SleepMiliSeconds (while waiting for eventor to finish)
-
-  if(SMS == 0) SMS = 0.2*NNN;
-  if(subnodes) delete [] subnodes;
-  subnodes = new ZNode* [NSN];
-
-  for(int i=0; i<NSN; ++i) {
-    subnodes[i] = new ZNode(Form("SubNode %d", i+1));
-    top_node->GetQueen()->CheckIn(subnodes[i]);
-    top_node->Add(subnodes[i]);
-  }
-
-  eventor->SetBeatsToDo(NNN);
-  for(int i=0; i<NSN; ++i) {
-    smorph_creator->SetTarget(subnodes[i]);
-    eventor->Reset();
-    GTime t(GTime::I_Now);
-    eventor->Start();
-    gSystem->Sleep(SMS);
-    while(eventor->GetRunning()) gSystem->Sleep(SMS);
-    printf("Done creation of smorphs for subnode %d (%lu).\n", i,
-	   t.TimeUntilNow().ToMiliSec());
-  }
 }
