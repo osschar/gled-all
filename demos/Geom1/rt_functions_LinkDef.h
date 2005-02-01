9f7c3365f6934e59e09b8484969d2d2e702e750e
Matevž Tadel <osschar@users.noreply.github.com>
2005-02-01 18:27:41 +0000

    New files.

---
+++ demos/Geom1/rt_functions_LinkDef.h
@@ -0,0 +1,15 @@
+// $Header$
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ global subnodes;
+#pragma link C++ global eventor;
+#pragma link C++ global smorph_creator;
+#pragma link C++ global top_node;
+
+#pragma link C++ function create_smorphs;
+#endif
