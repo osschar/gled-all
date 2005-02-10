e7933dceb9bd169cb503debe4388ad0e76778735
Matevž Tadel <osschar@users.noreply.github.com>
2005-02-10 12:51:01 +0000

    A_Rnr now subclasses A_View (and can thus absorb rays); RnrScheme creatiion is now steered by a virtual method A_Rnr::CreateRnrScheme.

--- GledCore/RnrBase/A_Rnr.h
+++ GledCore/RnrBase/A_Rnr.h
@@ -4,24 +4,94 @@
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
-#ifndef Gled_A_Rnr_H
-#define Gled_A_Rnr_H
+#ifndef GledCore_A_Rnr_H
+#define GledCore_A_Rnr_H
+
+#include <Eye/OptoStructs.h>
 
 class ZGlass;
 class RnrDriver;
+class RnrScheme;
 
-class A_Rnr {
+class A_Rnr : public OptoStructs::A_View {
+  friend class RnrDriver;
+
+protected:
+  RnrScheme* mRnrScheme;
 
 public:
-  virtual ZGlass* GetGlass() = 0;
+  void crs_links(RnrDriver* rd, RnrScheme* sch);
+  void crs_self(RnrDriver* rd, RnrScheme* sch);
+  void crs_elements(RnrDriver* rd, RnrScheme* sch);
 
-  virtual void PreDraw(RnrDriver*) = 0;
-  virtual void Draw(RnrDriver*) = 0;
-  virtual void PostDraw(RnrDriver*) = 0;
+public:
+  A_Rnr() : OptoStructs::A_View(0), mRnrScheme(0) {}
 
-  virtual void CleanUp(RnrDriver*) {}
+  virtual void AbsorbRay(Ray& ray);
+
+  virtual void CreateRnrScheme(RnrDriver* rd);
+  virtual void DropRnrScheme();
+
+  virtual void PreDraw(RnrDriver* rd) = 0;
+  virtual void Draw(RnrDriver* rd) = 0;
+  virtual void PostDraw(RnrDriver* rd) = 0;
+
+  virtual void CleanUp(RnrDriver* rd) {}
+
+  static const int sMaxRnrLevel;
 };
 
 typedef void (A_Rnr::*RnrSubDraw_foo)(RnrDriver*); // missing throw (?)
+
+/**************************************************************************/
+// ListChangeObserver
+/**************************************************************************/
+
+struct ListChangeObserver : public OptoStructs::A_View {
+  A_Rnr* mRnr;
+
+  ListChangeObserver(OptoStructs::ZGlassImg* list, A_Rnr* client) :
+    OptoStructs::A_View(list), mRnr(client) {}
+
+  virtual void AbsorbRay(Ray& ray);
+};
+
+/**************************************************************************/
+// RnrElement
+/**************************************************************************/
+
+struct RnrElement {
+  A_Rnr*		    fRnr;
+  RnrSubDraw_foo	    fRnrFoo; // If 0, do full descent.
+
+  RnrElement(A_Rnr* r, RnrSubDraw_foo f=0) : fRnr(r), fRnrFoo(f) {}
+};
+
+typedef list<RnrElement>		lRnrElement_t;
+typedef list<RnrElement>::iterator	lRnrElement_i;
+
+typedef vector<lRnrElement_t>		vlRnrElement_t;
+typedef vector<lRnrElement_t>::iterator	vlRnrElement_i;
+
+/**************************************************************************/
+// RnrScheme
+/**************************************************************************/
+
+// For optimization of updates, the fScheme could be split
+// into fSelfScheme, fListScheme, fLinksScheme.
+// then different rays would invalidate only the pertinent sub-schemes.
+
+class RnrScheme {
+public:
+  list<OptoStructs::A_View*>  fListObservers;
+  vlRnrElement_t              fScheme;
+
+  RnrScheme() : fScheme(A_Rnr::sMaxRnrLevel + 1) {}
+  ~RnrScheme();
+
+  void AddListChangeObserver(OptoStructs::ZGlassImg* list, A_Rnr* client);
+
+  lRnrElement_t& operator[](int i) { return fScheme[i]; }
+}; // endclass RnrScheme
 
 #endif
