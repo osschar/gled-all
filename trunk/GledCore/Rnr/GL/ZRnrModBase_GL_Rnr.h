99f9c0ffa14b14ebded08087b9d93ed26d4f2bb2
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-03-01 15:08:33 +0000

    New file.

---
+++ trunk/GledCore/Rnr/GL/ZRnrModBase_GL_Rnr.h
@@ -0,0 +1,36 @@
+// $Header$
+
+// Copyright (C) 1999-2004, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledCore_ZRnrModBase_GL_RNR_H
+#define GledCore_ZRnrModBase_GL_RNR_H
+
+#include <Glasses/ZRnrModBase.h>
+#include <Rnr/GL/ZGlass_GL_Rnr.h>
+#include <RnrBase/RnrDriver.h>
+
+class ZRnrModBase_GL_Rnr : public ZGlass_GL_Rnr {
+private:
+  void _init();
+
+protected:
+  ZRnrModBase*	     mZRnrModBase;
+
+  RnrDriver::RnrMod* mRnrMod;
+  UInt_t             mRebuildRnrCount;
+
+public:
+  ZRnrModBase_GL_Rnr(ZRnrModBase* idol) :
+    ZGlass_GL_Rnr(idol), mZRnrModBase(idol)
+  { _init(); }
+  virtual ~ZRnrModBase_GL_Rnr();
+
+  virtual void PreDraw(RnrDriver* rd);
+  virtual void Draw(RnrDriver* rd);
+  virtual void PostDraw(RnrDriver* rd);
+
+}; // endclass ZRnrModBase_GL_Rnr
+
+#endif
