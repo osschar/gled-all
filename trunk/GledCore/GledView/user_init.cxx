a31cf60150c7b04be61aa78811bfb30bfa4ff94d
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-03-11 17:46:44 +0000

    Added creation of box-type for rendering menus.

--- trunk/GledCore/GledView/user_init.cxx
+++ trunk/GledCore/GledView/user_init.cxx
@@ -1,8 +1,34 @@
 #include <Gled/Gled.h>
+#include <GledView/GledViewNS.h>
+
+#include <FL/Fl.h>
+#include <FL/fl_draw.h>
+
+namespace {
+
+  void menubar_box_draw(int x, int y, int w, int h, Fl_Color c) {
+    int yp = y + h - 6;
+    fl_color(c);
+    fl_rectf(x, y, w, h - 6);
+    fl_color(FL_DARK3);
+    fl_xyline(x, yp, x+w);
+    fl_xyline(x, yp+3, x+w);
+    fl_color(FL_WHITE);
+    fl_xyline(x, yp+1, x+w);
+    fl_xyline(x, yp+2, x+w);
+    fl_color(FL_BACKGROUND_COLOR);
+    fl_rectf(x, yp+4, w, 2);
+  }
+
+}
+
+/**************************************************************************/
 
 void libGledCore_GLED_user_init_View()
 {
-  // Currently empty.
+  // Box types.
+  GledViewNS::menubar_box = FL_FREE_BOXTYPE;
+  Fl::set_boxtype(FL_FREE_BOXTYPE, menubar_box_draw, 1, 1, 2, 7);
 }
 
 void *GledCore_GLED_user_init_View = (void*)libGledCore_GLED_user_init_View;
