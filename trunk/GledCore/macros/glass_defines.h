cec981b54d9b325bdb84980992c6312ba6ca8827
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-03-15 12:51:46 +0000

    Added define CREATE_ATT_GLASS to support different attachment scenarios.

--- trunk/GledCore/macros/glass_defines.h
+++ trunk/GledCore/macros/glass_defines.h
@@ -13,6 +13,10 @@
   _glass_* _var_ = new _glass_(_name_, _title_); \
   _list_->GetQueen()->CheckIn(_var_); _list_->Add(_var_)
 
+#define CREATE_ATT_GLASS(_var_, _glass_, _tgt_, _att_, _name_, _title_) \
+  _glass_* _var_ = new _glass_(_name_, _title_); \
+  _tgt_->GetQueen()->CheckIn(_var_); _tgt_->_att_(_var_)
+
 #define PATH_FIND_GLASS(_var_, _glass_, _lens_, _path_) \
   _glass_* _var_ = _lens_->FindLensByPath(_path_); \
   if(_var_ && GledNS::IsA(_var_, _glass_::FID())==false) _var_ = 0
