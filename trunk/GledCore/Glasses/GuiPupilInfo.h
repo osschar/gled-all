7ec03ea84f13d13285175a9d5f549625aa4e979a
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-03-23 11:34:19 +0000

    New glass: representation of a more customizable Pupil adorned with gui (so far menu but possibly other things will be added).

---
+++ trunk/GledCore/Glasses/GuiPupilInfo.h
@@ -0,0 +1,34 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledCore_GuiPupilInfo_H
+#define GledCore_GuiPupilInfo_H
+
+#include <Glasses/SubShellInfo.h>
+#include <Glasses/PupilInfo.h>
+
+class GuiPupilInfo : public SubShellInfo {
+  MAC_RNR_FRIENDS(GuiPupilInfo);
+
+private:
+  void _init();
+
+protected:
+  PupilInfo*	mPupil;   // X{gS} L{}
+  ZList*	mCameras; // X{gS} L{}
+
+public:
+  GuiPupilInfo(const Text_t* n="GuiPupilInfo", const Text_t* t=0) :
+    SubShellInfo(n,t) { _init(); }
+
+
+#include "GuiPupilInfo.h7"
+  ClassDef(GuiPupilInfo, 1)
+}; // endclass GuiPupilInfo
+
+GlassIODef(GuiPupilInfo);
+
+#endif
