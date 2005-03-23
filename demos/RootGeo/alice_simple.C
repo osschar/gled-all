d52bab74b5eaff917accc1145090523976d4abd7
Matevž Tadel <osschar@users.noreply.github.com>
2005-03-23 12:24:01 +0000

    New file: scene with a simplified ALICE geometry for event visualization.

---
+++ demos/RootGeo/alice_simple.C
@@ -0,0 +1,61 @@
+// $Header$
+
+// alice_simple: simplified ALICE geometry for event visualization
+//
+// libs: Geom1, RootGeo
+
+#include <glass_defines.h>
+#include <gl_defines.h>
+
+class ZGeoNode;
+
+#include "common_foos.C"
+
+/**************************************************************************/
+// main
+/**************************************************************************/
+
+void alice_simple(const Text_t* geom_file = "simple_geo.root",
+		  const Text_t* det_file  = "EvDisp.root")
+{
+  // usage: aligled <options> -- <gled-options> alice_simple.C
+
+  if(Gled::theOne->GetSaturn() == 0) gROOT->Macro("sun.C");
+
+  Gled::theOne->AssertLibSet("Geom1");
+  Gled::theOne->AssertLibSet("RootGeo");
+
+  //--------------------------------------------------------------
+
+  printf("Importing geometry ...\n");
+  TGeoManager::Import(geom_file);
+  printf("Done importing geometry.\n");
+
+  //--------------------------------------------------------------
+
+  Scene* rscene = create_basic_scene();
+
+  ZGlLightModel* lm = (ZGlLightModel*) rscene->FindLensByPath("Light Model");
+  lm->SetLightModelOp(0);
+  lm->SetFrontMode(GL_LINE);
+  lm->SetBackMode(GL_LINE);
+  lm->SetFaceCullOp(0);
+
+  //--------------------------------------------------------------
+
+  ZGeoNode* znode = new ZGeoNode("MasterVolume");
+  znode->SetTNode(gGeoManager->GetTopNode());
+  znode->SetOM(-2);
+  znode->SetUseOM(true);
+  scenes->CheckIn(znode);
+  rscene->Add(znode);
+  znode->SetRnrSelf(false);
+
+  znode->SetDefFile(det_file);
+  znode->LoadFromFile();
+  znode->Restore();
+
+  //--------------------------------------------------------------
+
+  spawn_default_gui(rscene);
+}
