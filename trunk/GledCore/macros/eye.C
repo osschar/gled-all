fe70c97256a8b037dd5e90288ede11790ebbb8ef
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-04-08 11:20:21 +0000

    Use new nest-layout registration mechanism.

--- trunk/GledCore/macros/eye.C
+++ trunk/GledCore/macros/eye.C
@@ -9,13 +9,14 @@ ShellInfo*     shell        = 0;
 SubShellInfo*  def_subshell = 0;
 PupilInfo*     pupil        = 0;
 
-void eye(const Text_t* eye_name="Eye of Ra",
-	 const Text_t* shell_name="Example Shell",
-	 const Text_t* pupil_name="Example Pupil",
+void eye(const Text_t* eye_name  ="Eye of Ra",
+	 const Text_t* shell_name="Shell",
+	 const Text_t* pupil_name="Default Pupil",
 	 const Text_t* scene_name=0)
 {
   if(Gled::theOne->HasGUILibs() == false) {
     printf("eye.C::eye skipping Eye and ShellInfo instantiation (no GUI libraries).\n");
+    return;
   }
 
   register_GledCore_layouts();
@@ -55,23 +56,27 @@ NestInfo* new_nest(const Text_t* name="Nest X", const Text_t* layout = 0)
     ni->SetLeafLayout(NestInfo::LL_Custom);
   }
 
-  shell->GetNests()->Add(ni);
+  shell->AddSubShell(ni);
   return ni;
 }
 
 /**************************************************************************/
 /**************************************************************************/
 
-void register_GledCore_layouts()
+ZList* register_GledCore_layouts(ZList* top=0)
 {
-  Gled& g = *Gled::theOne;
+  if(top == 0) top = fire_queen;
+  top = top->AssertPath(NestInfo::sLayoutPath, "ZNameMap");
+  ZList* l = top->AssertPath("GledCore", "ZList");
 
-  g.AddMTWLayout("GledCore/ZGlass",         "ZGlass(Name[20],Title[20],RefCount[6])");
-  g.AddMTWLayout("GledCore/ZNode",          "ZNode(Pos[18],Rot[18],UseOM[4],OM[5],UseScale[4],Sx[5],Sy[5],Sz[5],KeepParent[4])");
-  g.AddMTWLayout("GledCore/ZQueen Basic state",         
-                                            "ZQueen(State,MinID,MaxID,IDSpan,IDsUsed,IDsPurged,IDsFree,AvgPurgLen,SgmPurgLen,PurgedMS,DeletedMS,ZeroRCPolicy)");
-  g.AddMTWLayout("GledCore/ZQueen Authorization",         
-                                            "ZQueen(AuthMode,Alignment,MapNoneTo)");
-  g.AddMTWLayout("GledCore/Eventor",        "Eventor(Running[4],Performing[4],Start[4],Stop[4])");
-  g.AddMTWLayout("GledCore/Saturn monitor", "SaturnInfo(LAvg1,LAvg5,LAvg15,Memory,MFree,Swap,SFree,CU_Total[6],CU_User[6])");
+  l->Swallow(new ZGlass("ZGlass",         "ZGlass(Name[20],Title[20],RefCount[6])"));
+  l->Swallow(new ZGlass("ZNode",          "ZNode(Pos[18],Rot[18],UseOM[4],OM[5],UseScale[4],Sx[5],Sy[5],Sz[5],KeepParent[4])"));
+  l->Swallow(new ZGlass("ZQueen Basic state",         
+		       "ZQueen(State,MinID,MaxID,IDSpan,IDsUsed,IDsPurged,IDsFree,AvgPurgLen,SgmPurgLen,PurgedMS,DeletedMS,ZeroRCPolicy)"));
+  l->Swallow(new ZGlass("ZQueen Authorization",         
+		       "ZQueen(AuthMode,Alignment,MapNoneTo)"));
+  l->Swallow(new ZGlass("Eventor",        "Eventor(Running[4],Performing[4],Start[4],Stop[4])"));
+  l->Swallow(new ZGlass("Saturn monitor", "SaturnInfo(LAvg1,LAvg5,LAvg15,Memory,MFree,Swap,SFree,CU_Total[6],CU_User[6])"));
+
+  return top;
 }
