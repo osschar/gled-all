add0a990ac1fdd28957d9e453acc0c94cd8d4ef1
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-04-12 16:28:07 +0000

    Unknown.

--- trunk/demos/RootGeo/alice_simple.C
+++ trunk/demos/RootGeo/alice_simple.C
@@ -15,8 +15,8 @@ class ZGeoNode;
 // main
 /**************************************************************************/
 
-void alice_simple(const Text_t* geom_file = "simple_geo.root",
-		  const Text_t* det_file  = "EvDisp.root")
+Scene* alice_simple(const Text_t* geom_file = "simple_geo.root",
+		    const Text_t* det_file  = "EvDisp.root")
 {
   // usage: aligled <options> -- <gled-options> alice_simple.C
 
@@ -27,6 +27,20 @@ void alice_simple(const Text_t* geom_file = "simple_geo.root",
 
   //--------------------------------------------------------------
 
+  Scene* rscene = alice_simple_init(geom_file, det_file);
+
+  //--------------------------------------------------------------
+
+  spawn_default_gui(rscene);
+
+  return rscene;
+}
+
+/**************************************************************************/
+
+Scene* alice_simple_init(const Text_t* geom_file = "simple_geo.root",
+			 const Text_t* det_file  = "EvDisp.root")
+{
   printf("Importing geometry ...\n");
   TGeoManager::Import(geom_file);
   printf("Done importing geometry.\n");
@@ -35,7 +49,7 @@ void alice_simple(const Text_t* geom_file = "simple_geo.root",
 
   Scene* rscene = create_basic_scene();
 
-  ZGlLightModel* lm = (ZGlLightModel*) rscene->FindLensByPath("Light Model");
+  ZGlLightModel* lm = (ZGlLightModel*) rscene->FindLensByPath("Var/Light Model");
   lm->SetLightModelOp(0);
   lm->SetFrontMode(GL_LINE);
   lm->SetBackMode(GL_LINE);
@@ -55,7 +69,5 @@ void alice_simple(const Text_t* geom_file = "simple_geo.root",
   znode->LoadFromFile();
   znode->Restore();
 
-  //--------------------------------------------------------------
-
-  spawn_default_gui(rscene);
+  return rscene;
 }
