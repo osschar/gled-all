18d7bb05abafa1d34cbdd4b127925ba2ca2acbe5
Matevž Tadel <osschar@users.noreply.github.com>
2005-04-18 09:24:54 +0000

    Added members 'Int_t mDefW, mDefH' specifying size of the sub-shell-window on startup.

--- GledCore/Glasses/SubShellInfo.h
+++ GledCore/Glasses/SubShellInfo.h
@@ -19,6 +19,9 @@ protected:
   TString	mCtorLibset;	// X{GS} 7 Textor()
   TString	mCtorName;	// X{GS} 7 Textor()
 
+  Int_t		mDefW;          // X{GS} 7 Value(-range=>[1,256,1], -join=>1)
+  Int_t		mDefH;          // X{GS} 7 Value(-range=>[1,256,1])
+
 public:
   SubShellInfo(const Text_t* n="SubShellInfo", const Text_t* t=0) :
     ZList(n,t) { _init(); }
