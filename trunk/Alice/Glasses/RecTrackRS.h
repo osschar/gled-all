a06eec43adb8e3e4c9c659f6db5fc4e4f507a7c8
alja <alja@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-05-18 16:34:43 +0000

    New render style class for RecTrack.

---
+++ trunk/Alice/Glasses/RecTrackRS.h
@@ -0,0 +1,34 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef Alice_RecTrackRS_H
+#define Alice_RecTrackRS_H
+#include <Glasses/PRSBase.h>
+#include <Stones/ZColor.h>
+
+class RecTrackRS : public PRSBase  {
+  MAC_RNR_FRIENDS(RecTrackRS);
+
+ private:
+  void _init();
+
+ protected:
+
+ public:
+  Bool_t		     bFitKinks;	   // X{GST}  7 Bool( -join=>1)
+  Bool_t		     bFitV0;  // X{GST}  7 Bool()
+
+  RecTrackRS(const Text_t* n="RecTrackRS", const Text_t* t=0) :
+    PRSBase(n,t) { _init(); }
+
+
+#include "RecTrackRS.h7"
+  ClassDef(RecTrackRS, 1)
+    }; // endclass RecTrackRS
+
+GlassIODef(RecTrackRS);
+
+#endif
