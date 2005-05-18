e0f5b4adfbdade32b868a4b6764c8ca11934c5b5
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2005-05-18 16:34:50 +0000

    New Rnr class for RecTrackRS.

---
+++ Alice/Rnr/GL/RecTrackRS_GL_Rnr.cxx
@@ -0,0 +1,33 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#include "RecTrackRS_GL_Rnr.h"
+#include <RnrBase/RnrDriver.h>
+#include <FL/gl.h>
+
+/**************************************************************************/
+
+void RecTrackRS_GL_Rnr::_init()
+{}
+
+/**************************************************************************/
+
+void RecTrackRS_GL_Rnr::PreDraw(RnrDriver* rd)
+{
+  ConsiderRebuildDL(rd);
+  rd->PushRnrMod(RecTrackRS::FID(),  mRnrMod);
+}
+
+void RecTrackRS_GL_Rnr::Draw(RnrDriver* rd)
+{
+  ConsiderRebuildDL(rd);
+  rd->SetDefRnrMod(RecTrackRS::FID(), mRnrMod);
+}
+
+void RecTrackRS_GL_Rnr::PostDraw(RnrDriver* rd)
+{
+  rd->PopRnrMod(RecTrackRS::FID());
+}
