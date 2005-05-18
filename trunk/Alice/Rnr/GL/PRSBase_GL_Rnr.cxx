07d87c5fc8f1e66930b60b1be9737f62ba162e4f
alja <alja@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-05-18 16:34:49 +0000

    New Rnr class for PRSBase.

---
+++ trunk/Alice/Rnr/GL/PRSBase_GL_Rnr.cxx
@@ -0,0 +1,33 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#include "PRSBase_GL_Rnr.h"
+#include <RnrBase/RnrDriver.h>
+#include <FL/gl.h>
+
+/**************************************************************************/
+
+void PRSBase_GL_Rnr::_init()
+{}
+
+/**************************************************************************/
+
+void PRSBase_GL_Rnr::PreDraw(RnrDriver* rd)
+{
+  ConsiderRebuildDL(rd);
+  rd->PushRnrMod(PRSBase::FID(),  mRnrMod);
+}
+
+void PRSBase_GL_Rnr::Draw(RnrDriver* rd)
+{
+  ConsiderRebuildDL(rd);
+  rd->SetDefRnrMod(PRSBase::FID(), mRnrMod);
+}
+
+void PRSBase_GL_Rnr::PostDraw(RnrDriver* rd)
+{
+  rd->PopRnrMod(PRSBase::FID());
+}
