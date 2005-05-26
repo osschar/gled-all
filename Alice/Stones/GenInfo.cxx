280db925ce24165d9e80339001c240eff01e88cf
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2005-05-26 13:20:37 +0000

    Different class structure.

---
+++ Alice/Stones/GenInfo.cxx
@@ -0,0 +1,30 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// GenInfo
+//
+//
+
+#include "GenInfo.h"
+
+ClassImp(GenInfo)
+
+/**************************************************************************/
+
+  void GenInfo::_init()
+{
+  // P = 0;
+  // R = 0;
+  bR = false;
+  Nh = Nc = 0;
+}
+
+/**************************************************************************/
+
+
+
+/**************************************************************************/
