26247bbdf10dc03ba36067fa8c69aec10a51593f
Matevž Tadel <osschar@users.noreply.github.com>
2005-05-30 13:15:13 +0000

    Listener orientation now set; handles mLocationType values Camera/Absoulte.

--- Audio1/Rnr/GL/AlListener_GL_Rnr.cxx
+++ Audio1/Rnr/GL/AlListener_GL_Rnr.cxx
@@ -22,9 +22,15 @@ void AlListener_GL_Rnr::_init()
 
 void AlListener_GL_Rnr::Draw(RnrDriver* rd)
 {
-  ZTrans& t = *rd->GetAbsCamera();
-  alListener3f(AL_POSITION, t(1,4), t(2,4), t(3,4));
-  printf("Listener: %f %f %f\n", t(1,4), t(2,4), t(3,4));
+  ZTrans* tp = 0;
+  switch (mAlListener->mLocationType) {
+  case AlListener::LT_Camera:   tp = rd->GetAbsCamera(); break;
+  case AlListener::LT_Absolute: tp = &rd->ToGCS();       break;
+  }
+  ZTrans& t = *tp;
+  Float_t orient[6] = { t(1,1), t(2,1), t(3,1),  t(1,3), t(2,3), t(3,3) };
+  alListener3f(AL_POSITION,    t(1,4), t(2,4), t(3,4));
+  alListenerfv(AL_ORIENTATION, orient);
 }
 
 //void AlListener_GL_Rnr::PostDraw(RnrDriver* rd)
