8873afd283ca8715aee2895dd774ff3c6d3c8f2d
Matevž Tadel <osschar@users.noreply.github.com>
2005-05-30 13:15:11 +0000

    Added gain handling and enum LocationType to specify if listener position is on-camera or it should be calculated from position-matrix stack.

--- Audio1/Glasses/AlListener.h
+++ Audio1/Glasses/AlListener.h
@@ -12,15 +12,22 @@
 class AlListener : public ZNode {
   MAC_RNR_FRIENDS(AlListener);
 
+public:
+  enum LocationType_e { LT_Camera, LT_Absolute };
+
 private:
   void _init();
 
 protected:
+  LocationType_e mLocationType; // X{GS} 7 PhonyEnum()
+
+  Float_t        mGain; // X{GS} Ray{Source} 7 Value(-range=>[0,100,1,1000])
 
 public:
   AlListener(const Text_t* n="AlListener", const Text_t* t=0) :
     ZNode(n,t) { _init(); }
 
+  void EmitSourceRay();
 
 #include "AlListener.h7"
   ClassDef(AlListener, 1)
