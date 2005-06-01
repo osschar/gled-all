12c8ef58c10e70c250db645b82789b331c73cdf0
alja <alja@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-06-01 17:34:47 +0000

    Add utility fuctions R(), P(), Pt(), Theta() and Eta().

--- trunk/Alice/Stones/Hit.h
+++ trunk/Alice/Stones/Hit.h
@@ -8,6 +8,7 @@
 #define Alice_Hit_H
 
 #include <TObject.h>
+#include <TMath.h>
 
 class Hit : public TObject {
  public:
@@ -20,7 +21,19 @@ class Hit : public TObject {
   Hit(UChar_t detector, Int_t particle, Int_t eva,
       Float_t x, Float_t y, Float_t z );
 
+  /**************************************************************************/
+  // methods needed for combined tree selection
+  Double_t  R()            const { return TMath::Sqrt(x*x+y*y);}
+
+  Double_t  P()            const { return TMath::Sqrt(x*x+y*y+z*z);}
+  Double_t  Pt()           const { return TMath::Sqrt(x*x+y*y);}
+  Double_t  Eta()          const { 
+    if (TMath::Abs(R() != z)) return 0.5*TMath::Log((R()+z)/(R()-z));
+    else  return 1.e30;}
+
+  Double_t  Theta()        const { return (z==0)?TMath::PiOver2():TMath::ACos(z/R());}
   
+  /**************************************************************************/
   void     Dump();
 #include "Hit.h7"
   ClassDef(Hit, 1)
