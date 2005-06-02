fd65cff2c54cb8866fcadca47a9315583802604f
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-06-02 17:01:52 +0000

    Removed all init actions.

--- trunk/Alice/Alice/user_init.cxx
+++ trunk/Alice/Alice/user_init.cxx
@@ -4,20 +4,7 @@
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
-#include <Gled/GledTypes.h>
-
-#include <TSystem.h>
-#include <TROOT.h>
-#include <TInterpreter.h>
-
 void libAlice_GLED_user_init()
-{
-  const Text_t* alishare = gSystem->Getenv("ALIGLEDSHARE");
-  if(alishare) {
-    gROOT->SetMacroPath(GForm("%s:%s/macros", gROOT->GetMacroPath(),
-			      alishare));
-    gInterpreter->AddIncludePath(GForm("%s/macros", alishare));
-  }
-}
+{}
 
 void *Alice_GLED_user_init = (void*)libAlice_GLED_user_init;
