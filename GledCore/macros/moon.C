dd4f8de26007b858d886d58ce7da29cc4099365a
Matevž Tadel <osschar@users.noreply.github.com>
2005-06-08 11:21:37 +0000

    Cleaned-up startup steering.

--- GledCore/macros/moon.C
+++ GledCore/macros/moon.C
@@ -2,31 +2,21 @@
 
 // Top-level script that starts a Moon (client) connected to the Sun
 // specified with -master argument of gled.
-// Also spawns an eye.
-// Preferably run as: gled -m <master> -r moon.C
-
-Saturn*      moon      = 0;
-Saturn*     saturn     = 0;
-
-ZSunQueen*  sun_queen  = 0;
-ZFireQueen* fire_queen = 0;
-ZQueen*     scenes     = 0;
+// Also spawns an eye with a pupil on the first scene of the queen "Scenes".
+// Supposedly run as: gled -m <master> moon.C
 
 void moon()
 {
-  // a moon MUST be started with -r flag
-  moon = Gled::theOne->GetSaturn();
-  if(moon == 0) {
-    cerr <<
-     "Saturn is not spawned ... use the -r command-line option.\n";
-    return;
+  if(Gled::theOne->GetSaturn() == 0) {
+    Gled::theOne->SpawnSaturn();
   }
-  saturn = moon;
 
-  sun_queen  = saturn->GetSunQueen();
-  fire_queen = saturn->GetFireQueen();
+  Gled::AssertMacro("gled_globals.C");
 
-  scenes = (ZQueen*) saturn->GetSunKing()->GetElementByName("Scenes");
+  g_queen = (ZQueen*) g_sun_king->GetElementByName("Scenes");
+  if(g_queen)
+    g_scene = dynamic_cast<Scene*>(g_queen->First());
 
-  gROOT->Macro("eye.C");	// spawn an eye with a pupil on first scene
+  Gled::Macro("eye.C"); // spawn an eye with a pupil on first scene
+  setup_pupil_up_reference();
 }
