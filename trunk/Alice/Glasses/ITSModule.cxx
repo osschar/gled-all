db5cbc0a2f3e1a9f57a268e1d30ece7d766b6877
alja <alja@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-06-20 13:04:56 +0000

    New files.

---
+++ trunk/Alice/Glasses/ITSModule.cxx
@@ -0,0 +1,94 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// ITSModule
+//
+//
+
+#include "ITSModule.h"
+#include "ITSModule.c7"
+
+ClassImp(ITSModule)
+
+/**************************************************************************/
+
+void ITSModule::_init()
+{
+  // *** Set all links to 0 ***
+  bSetTrans = true;
+  mID   = -1;
+  mInfo = 0;
+  mFrameCol.rgba(1,1,1,0.25);
+  mDigCol.rgba(1,1,1,0.4);
+  mSSDPCol.rgba(1,0,0,0.4);
+  mSSDNCol.rgba(0,1,0,0.4);
+}
+
+/**************************************************************************/
+ITSModule::ITSModule(Int_t id, ITSDigitsInfo* info):ZNode() 
+{
+  _init();
+  mInfo = info;
+  SetID(id);
+}
+
+/**************************************************************************/
+void ITSModule::SetID(Int_t id) {
+  static const string _eh("ITSModule::SetID ");
+
+  if (id < mInfo->mGeom->GetStartSPD() || id > mInfo->mGeom->GetLastSSD())
+    throw(_eh + GForm("%d is not valid. ID range from %d to %d",
+		      id,mInfo->mGeom->GetStartSPD(), mInfo->mGeom->GetLastSSD()));
+
+  mID = id;
+  init_module();
+  mStampReqTring = mStampReqTrans = Stamp(FID());
+}
+
+void ITSModule::init_module()
+{
+  mInfo->mGeom->GetModuleId(mID,mLayer,mLadder,mDet);
+
+  if(mID < mInfo->mGeom->GetLastSPD()){
+    mDetID = 0;
+    mDx = mInfo->mSegSPD->Dx()*0.00005;
+    mDz = 3.48; //  mInfo->mSegSPD->Dz()*0.00005;
+    mDy = mInfo->mSegSPD->Dy()*0.00005;
+  } else if (mID < mInfo->mGeom->GetLastSDD() ){
+    mDetID = 1;
+    mDx = mInfo->mSegSDD->Dx()*0.0001;
+    mDz = mInfo->mSegSDD->Dz()*0.00005;
+    mDy = mInfo->mSegSDD->Dy()*0.00005;
+  }
+  else {
+    mDetID=2;
+    mInfo->mSegSSD->SetLayer(mLayer);  
+    mDx = mInfo->mSegSSD->Dx()*0.0001;
+    mDz = mInfo->mSegSSD->Dz()*0.00005;
+    mDy = mInfo->mSegSSD->Dy()*0.00005;
+  }
+
+  if(bSetTrans)
+    set_trans();
+
+  SetName(GForm("%d ITSModule",mID));
+}
+
+/**************************************************************************/
+void ITSModule::set_trans()
+{
+  Double_t pos[3];
+  Double_t rot[9];
+  mInfo->mGeom->GetTrans(mID,pos);
+  mInfo->mGeom->GetRotMatrix(mID,rot);
+  mTrans.SetBaseV(1, rot[0], rot[1], rot[2]);
+  mTrans.SetBaseV(2, rot[3], rot[4], rot[5]);
+  mTrans.SetBaseV(3, rot[6], rot[7], rot[8]);
+  mTrans.SetBaseV(4, pos[0], pos[1], pos[2]);
+}
+
+  /**************************************************************************/
---
+++ trunk/Alice/Glasses/ITSModule.h
@@ -0,0 +1,57 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef Alice_ITSModule_H
+#define Alice_ITSModule_H
+
+#include <Glasses/ZNode.h>
+#include <Stones/ZColor.h>
+#include <Stones/ITSDigitsInfo.h>
+
+class ITSModule : public ZNode {
+  MAC_RNR_FRIENDS(ITSModule);
+
+ private:
+  void _init();
+  void init_module();
+
+ protected:
+  void set_trans();
+  ITSDigitsInfo*           mInfo;
+
+  Bool_t      bSetTrans;// X{GS}  7 Bool()
+  Int_t       mID;      // X{g}   7 Value(-range=>[0,2200,1], -join=>1)
+  Int_t       mDetID;   // X{G}   7 ValOut()
+
+  Int_t       mLayer;   // X{gS}  7 ValOut(-range=>[0,6,1], -join=>1)
+  Int_t       mLadder;  // X{gS}  7 ValOut(-range=>[0,100,1], -join=>1)
+  Int_t       mDet;     // X{gS}  7 ValOut(-range=>[0,100,1])
+  
+  Float_t     mDx;         // X{G}  7 ValOut(-join=>1)
+  Float_t     mDz;         // X{G}  7 ValOut(-join=>1)
+  Float_t     mDy;         // X{G}  7 ValOut()
+
+  ZColor     mFrameCol;    // X{GSPT} 7 ColorButt(-join=>1)
+  ZColor     mDigCol;      // X{GSPT} 7 ColorButt()
+
+  ZColor     mSSDPCol;    // X{GSPT} 7 ColorButt(-join=>1)
+  ZColor     mSSDNCol;      // X{GSPT} 7 ColorButt()
+ public:
+  ITSModule(const Text_t* n="ITSModule", const Text_t* t=0) :
+    ZNode(n,t) { _init(); }
+
+  ITSModule(Int_t id, ITSDigitsInfo* info);
+  
+
+  virtual void SetID(Int_t id); // X{E}
+
+#include "ITSModule.h7"
+  ClassDef(ITSModule, 1)
+    }; // endclass ITSModule
+
+GlassIODef(ITSModule);
+
+#endif
