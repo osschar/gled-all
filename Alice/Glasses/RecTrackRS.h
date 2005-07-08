be25a7406c479baacff26ad1b3baa6ac7d91a525
Matevž Tadel <osschar@users.noreply.github.com>
2005-07-08 09:39:19 +0000

    Added kink-vertex size and color controls.

--- Alice/Glasses/RecTrackRS.h
+++ Alice/Glasses/RecTrackRS.h
@@ -6,28 +6,30 @@
 
 #ifndef Alice_RecTrackRS_H
 #define Alice_RecTrackRS_H
+
 #include <Glasses/PRSBase.h>
 #include <Stones/ZColor.h>
 
-class RecTrackRS : public PRSBase  {
+class RecTrackRS : public PRSBase
+{
   MAC_RNR_FRIENDS(RecTrackRS);
 
  private:
   void _init();
 
- protected:
+ public:
+  Bool_t      bFitKinks;      // X{GST}  7 Bool( -join=>1)
+  Bool_t      bFitV0;         // X{GST}  7 Bool()
+  Float_t     mKinkVSize;     // X{GST}  7 Value(-range=>[0.1,64, 1,10])
+  ZColor      mKinkVColor;    // X{PGST} 7 ColorButt()
 
  public:
-  Bool_t		     bFitKinks;	   // X{GST}  7 Bool( -join=>1)
-  Bool_t		     bFitV0;  // X{GST}  7 Bool()
-
   RecTrackRS(const Text_t* n="RecTrackRS", const Text_t* t=0) :
     PRSBase(n,t) { _init(); }
 
-
 #include "RecTrackRS.h7"
   ClassDef(RecTrackRS, 1)
-    }; // endclass RecTrackRS
+}; // endclass RecTrackRS
 
 GlassIODef(RecTrackRS);
 
