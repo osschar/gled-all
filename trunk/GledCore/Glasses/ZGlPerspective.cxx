b8968e28ae95e1d6ab558ceedb973db93bcb490c
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-07-13 14:39:57 +0000

    New glass: modifies GL projection and modelview matrix for orthographic viewing.

---
+++ trunk/GledCore/Glasses/ZGlPerspective.cxx
@@ -0,0 +1,62 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// ZGlPerspective
+//
+// Sets-up GL projection and modelview matrices for orthographic
+// viewing in fixed or pixel-based coordinates.
+//
+// TODO:
+// a) handling of negative offsets (need origin mode, or some flags)
+// b) for fixed mode, set w/h in accordance with window size (optional)
+
+#include "ZGlPerspective.h"
+#include "ZGlPerspective.c7"
+
+ClassImp(ZGlPerspective)
+
+/**************************************************************************/
+
+void ZGlPerspective::_init()
+{
+  // *** Set all links to 0 ***
+  mViewMode  = VM_Nop;
+  mOrthoW    = 10; mOrthoH   = 10;
+  mOrthoNear = -1; mOrthoFar = 1;
+  mOx = mOy = mOz = 0;
+}
+
+/**************************************************************************/
+
+void ZGlPerspective::StandardPersp()
+{
+  ZGlPerspective& X = *this;
+  X.SetViewMode(VM_Nop);
+  X.SetOrthoW(10.000000); X.SetOrthoH(10.000000);
+  X.SetOrthoNear(-1.000000); X.SetOrthoFar(1.000000);
+  X.SetOx(0); X.SetOy(0); X.SetOz(0);
+}
+
+void ZGlPerspective::StandardFixed()
+{
+  ZGlPerspective& X = *this;
+  X.SetViewMode(VM_OrthoFixed);
+  X.SetOrthoW(10.000000); X.SetOrthoH(10.000000);
+  X.SetOrthoNear(-1.000000); X.SetOrthoFar(1.000000);
+  X.SetOx(0); X.SetOy(0); X.SetOz(0);
+}
+
+void ZGlPerspective::StandardPixel()
+{
+  ZGlPerspective& X = *this;
+  X.SetViewMode(VM_OrthoPixel);
+  X.SetOrthoW(10.000000); X.SetOrthoH(10.000000);
+  X.SetOrthoNear(-1.000000); X.SetOrthoFar(1.000000);
+  X.SetOx(5.000000); X.SetOy(-50.000000); X.SetOz(0.000000);
+}
+
+/**************************************************************************/
