4de140a53b0488e504d6bfc317835426825ee61c
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-08-26 09:30:38 +0000

    New glass WGlValuator, a GL widget for manipulating glass value data-members.

---
+++ trunk/GledCore/Rnr/GL/WGlValuator_GL_Rnr.cxx
@@ -0,0 +1,109 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#include "WGlValuator_GL_Rnr.h"
+
+#include <RnrBase/Fl_Event_Enums.h>
+#include <Rnr/GL/ZRlFont_GL_Rnr.h>
+#include <Rnr/GL/WGlFrameStyle_GL_Rnr.h>
+
+#include <Stones/ZMIR.h>
+#include <Eye/Eye.h>
+
+#include <TDataMember.h>
+#include <TDataType.h>
+
+#include <GL/gl.h>
+
+/**************************************************************************/
+
+void WGlValuator_GL_Rnr::_init()
+{
+  bBelowMouse = 0;
+}
+
+/**************************************************************************/
+
+Double_t WGlValuator_GL_Rnr::get_value()
+{
+  WGlValuator& V = *mWGlValuator;
+  char* addr = (char*)V.mCbackAlpha + V.mDataMember->GetOffset();
+  return GledNS::peek_value(addr, V.mDataMember->GetDataType()->GetType());
+}
+
+/**************************************************************************/
+
+void WGlValuator_GL_Rnr::Draw(RnrDriver* rd)
+{
+  obtain_rnrmod(rd, mFontRMS);
+  obtain_rnrmod(rd, mFrameRMS);
+  
+  GLTextNS::TexFont    *txf = ((ZRlFont_GL_Rnr*)mFontRMS.rnr())->GetFont();
+  WGlFrameStyle_GL_Rnr &FSR = *(WGlFrameStyle_GL_Rnr*)mFrameRMS.rnr();
+
+  WGlValuator& V = *mWGlValuator;
+
+  string label("<no-set>");
+  if(V.DataOK())
+    label = GForm(V.GetFormat(), get_value());
+  FSR.FullRender(txf, label, V.mDx, V.mDy, bBelowMouse);
+}
+
+/**************************************************************************/
+
+int WGlValuator_GL_Rnr::Handle(RnrDriver* rd, Fl_Event& ev)
+{
+  WGlValuator& V = *mWGlValuator;
+
+  if(ev.fEvent == FL_LEAVE) {
+    bBelowMouse = false;
+    Redraw(rd);
+    return 1;
+  }
+  
+  if(ev.fEvent == FL_ENTER) {
+    bBelowMouse = true;
+    Redraw(rd);
+    return 1;
+  }
+
+  if(ev.fEvent == FL_PUSH && ev.fButton == FL_LEFT_MOUSE) {
+    if(V.DataOK()) {
+      mX = ev.fX; mY = ev.fX;
+      printf("push at %d %d\n", mX, mY);
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  if(ev.fEvent == FL_DRAG) {
+    Int_t dx = (ev.fX - mX) / 4;
+    printf("drag at %d %d; x=%d, d=%d\n", ev.fX, ev.fX, mX, dx);
+    if(dx !=0 && V.DataOK()) {
+      ZMIR mir(V.mCbackAlpha);
+      V.mDataMemberInfo->fSetMethod->ImprintMir(mir);
+      V.mDataMemberInfo->fSetMethod->FixMirBits(mir, fImg->fEye->GetSaturnInfo());
+      Double_t     val  = dx*V.mStepA;
+      if(V.mStepB) val /= V.mStepB;
+      val += get_value();
+      if(val > V.mMax) val = V.mMax;
+      if(val < V.mMin) val = V.mMin;
+      GledNS::stream_value(mir, V.mDataMember->GetDataType()->GetType(), val);
+      fImg->fEye->Send(mir);
+      
+      mX = ev.fX;
+    }
+    return 1;
+  }
+  /*
+    if(ev.fEvent == FL_RELEASE) {
+    bBelowMouse = true;
+    Redraw(rd);
+    return 1;
+    }
+  */
+  return 0;
+}
