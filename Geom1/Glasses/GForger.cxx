085df79f66f6846b78a9d5053f6f872ef24abd4e
Matevž Tadel <osschar@users.noreply.github.com>
2005-10-03 10:53:41 +0000

    New glass.

---
+++ Geom1/Glasses/GForger.cxx
@@ -0,0 +1,63 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// GForger
+//
+//
+
+#include "GForger.h"
+#include "GForger.c7"
+
+#include <TSystem.h>
+
+ClassImp(GForger)
+
+/**************************************************************************/
+
+void GForger::_init()
+{
+  mFileName = "gforge.png";
+
+  mMesh  = 256;
+  mPower = 1.2;
+
+  mDimension = 2.15;
+
+  bCraters       = false;
+  mCraterDensity = 1;
+  mCraterHeight  = 0.5;
+}
+
+/**************************************************************************/
+
+void GForger::Forge()
+{
+  lStr_t parts;
+  parts.push_back
+    (GForm("gforge -name %s -mesh %d -power %f -dimension %f %s",
+	   mFileName.Data(), mMesh, mPower, mDimension, mADimText.Data()
+	   ));
+  if(bCraters)
+    parts.push_back
+      (GForm("-craters %f %f", mCraterDensity, mCraterHeight));
+
+  gSystem->Exec(GledNS::join_strings(" ", parts));
+
+  if(mImage != 0) {
+    {
+      GLensWriteHolder imglock(*mImage);
+      mImage->SetFile(mFileName);
+      mImage->Load();
+    }
+    if(mTerrain != 0) {
+      GLensWriteHolder imglock(*mTerrain);
+      mTerrain->SetFromImage(*mImage);
+    }
+  }
+}
+
+/**************************************************************************/
