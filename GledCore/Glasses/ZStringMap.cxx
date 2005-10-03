4f259704cfd1b6fd38946ce58cc27d1dc5e125c2
Matevž Tadel <osschar@users.noreply.github.com>
2005-10-03 10:26:14 +0000

    New container glasses.

---
+++ GledCore/Glasses/ZStringMap.cxx
@@ -0,0 +1,27 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// ZStringMap
+//
+//
+
+#include "ZStringMap.h"
+#include "ZStringMap.c7"
+
+ClassImp(ZStringMap)
+
+/**************************************************************************/
+
+void ZStringMap::_init()
+{
+
+}
+
+/**************************************************************************/
+
+
+/**************************************************************************/
