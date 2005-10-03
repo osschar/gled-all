e5a35f0097d977ef34fab537b42380ba3cde77cc
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-10-03 10:17:00 +0000

    Moved in from Eye/.

---
+++ trunk/GledCore/GledView/FTW_Branch.cxx
@@ -0,0 +1,321 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#include "FTW_Branch.h"
+#include "FTW_Nest.h"
+#include "FTW_Shell.h"
+
+#include <Eye/Eye.h>
+#include <Glasses/ZList.h>
+
+#include <FL/Fl_Button.H>
+
+namespace OS = OptoStructs;
+using namespace FTW;
+
+typedef list<FTW_Leaf*>::iterator leaf_iterator;
+
+/**************************************************************************/
+
+static void cb_list_collexp(Fl_Button* b, FTW_Branch* l) {
+  l->CollExpList();
+}
+
+/**************************************************************************/
+
+void FTW_Branch::build_leaves(int insert_pos)
+{
+  AList::lElRep_t d;
+  mLeavesTimeStamp = ((AList*)fImg->fLens)->CopyListElReps(d);
+  for(AList::lElRep_i i=d.begin(); i!=d.end(); ++i) {
+    OS::ZGlassImg* img = fImg->fEye->DemanglePtr(i->get_lens());
+    FTW_Leaf*     leaf = FTW_Leaf::Construct(mNest, this, img, true, false);
+    leaf->mListId    = i->get_id();
+    leaf->mListLabel = i->get_label();
+    mLeaves.push_back(leaf);
+    mNest->InsertLeaf(leaf, insert_pos++);
+  }
+}
+
+void FTW_Branch::wipe_leaves()
+{
+  if(bLeavesCreated) {
+    for(leaf_iterator i=mLeaves.begin(); i!=mLeaves.end(); ++i)
+      delete *i;
+    bLeavesCreated = false;
+  }
+  mLeaves.clear();
+}
+
+/**************************************************************************/
+
+void FTW_Branch::insert_leaf(Ray& ray, list<FTW_Leaf*>::iterator pos)
+{
+  int nest_pos = (pos == mLeaves.end()) ?
+    mNest->PackPosAfter(this) : mNest->PackPos(*pos);
+  FTW_Leaf* leaf = FTW_Leaf::Construct(mNest, this, ray.fBetaImg, true, false);
+  leaf->mListId    = ray.fBetaId;
+  leaf->mListLabel = ray.fBetaLabel;
+  mLeaves.insert(pos, leaf);
+  mNest->InsertLeaf(leaf, nest_pos);
+}
+
+void FTW_Branch::remove_leaf(list<FTW_Leaf*>::iterator pos)
+{
+  static const Exc_t _eh("FTW_Branch::remove_leaf ");
+
+  if(pos == mLeaves.end()) {
+    mNest->GetShell()->Message
+      (_eh + "got end() as position argument. Expect trouble.");
+      return;
+  }
+  delete *pos;
+  mLeaves.erase(pos);
+}
+
+/**************************************************************************/
+
+AList::ElRep FTW_Branch::beta_elrep(Ray& ray)
+{
+  return AList::ElRep(ray.fBeta, ray.fBetaId, ray.fBetaLabel);
+}
+
+AList::ElRep FTW_Branch::gamma_elrep(Ray& ray)
+{
+  return AList::ElRep(ray.fGamma, ray.fGammaId, ray.fGammaLabel);
+}
+
+list<FTW_Leaf*>::iterator FTW_Branch::leaf_pos(AList::ElRep elrep)
+{
+  AList::ElType_e type = ((AList*)fImg->fLens)->el_type();
+  switch(type) {
+  case AList::ET_Lens:
+    return find_if(mLeaves.begin(), mLeaves.end(),
+		   FTW_Leaf::leaf_eq_lens(elrep.get_lens()));
+  case AList::ET_Id:
+    return find_if(mLeaves.begin(), mLeaves.end(),
+		   FTW_Leaf::leaf_eq_id(elrep.get_id()));
+  case AList::ET_Label:
+    return find_if(mLeaves.begin(), mLeaves.end(),
+		   FTW_Leaf::leaf_eq_label(elrep.get_label()));
+  default:
+    return mLeaves.end();
+  }
+}
+
+/**************************************************************************/
+// FTW_Branch
+/**************************************************************************/
+
+FTW_Branch::FTW_Branch(FTW_Nest* nest, FTW_Leaf* parent,
+		       OS::ZGlassImg* img,
+		       bool is_list_member, bool is_link_desc) :
+  FTW_Leaf(nest, parent, img, is_list_member, is_link_desc),
+  bLeavesCreated(false), bListExpanded(false)
+{
+  wListExpander->labelcolor(FL_BLACK);
+  wListExpander->callback((Fl_Callback*)cb_list_collexp, this);
+}
+
+FTW_Branch::~FTW_Branch() {
+  wipe_leaves();
+}
+
+/**************************************************************************/
+
+void FTW_Branch::AbsorbRay(Ray& ray)
+{
+  using namespace RayNS;
+
+  if(ray.fRQN > RQN_list_begin && ray.fRQN < RQN_list_end) {
+
+    if(bLeavesCreated && mLeavesTimeStamp < ray.fStamp) {
+
+      switch(ray.fRQN) {
+
+      case RQN_list_push_back:
+	insert_leaf(ray, mLeaves.end());
+	break;
+      case RQN_list_pop_back:
+	// delete mLeaves.back();
+	remove_leaf(--mLeaves.end());
+	break;
+      case RQN_list_push_front:
+	insert_leaf(ray, mLeaves.begin());
+	break;
+      case RQN_list_pop_front:
+	//delete mLeaves.front();
+	remove_leaf(mLeaves.begin());
+	break;
+
+      case RQN_list_insert: 
+	insert_leaf(ray, leaf_pos(gamma_elrep(ray)));
+	break;
+      case RQN_list_remove:
+	remove_leaf(leaf_pos(beta_elrep(ray)));
+	break;
+
+      case RQN_list_element_set:
+      case RQN_list_insert_label:
+      case RQN_list_remove_label:
+	cout << "Branch::AbsorbRay "<< ray << ". Not supported yet.\n";
+	break;
+	//case RQN_list_rebuild:
+	//      case RQN_list_clear:
+
+	/*
+      case RQN_list_add: {
+	lLoI_i i = ray.fGammaImg ?
+	  find_if(mLeoim.begin(), mLeoim.end(), Leoim_img_eq(ray.fGammaImg)) :
+	  mLeoim.end();
+
+	FTW_Leaf* leaf = FTW_Leaf::Construct(mNest, this, ray.fBetaImg, true, false);
+	if(!bListExpanded) leaf->hide();
+	if(i==mLeoim.end()) mNest->InsertLeaf(leaf, mNest->PackPosAfter(this));
+	else                mNest->InsertLeaf(leaf, i->leaf);
+
+	mLeoim.insert(i, Leaf_o_Img(ray.fBetaImg, leaf));
+
+	label_namebox();
+	return;
+      }
+
+      case RQN_list_remove: {
+	lLoI_i i = find_if(mLeoim.begin(), mLeoim.end(), Leoim_img_eq(ray.fBetaImg));
+	//cout <<"FTW_Branch::AbsorbRay RQN_list_remove for "<< eximg->fLens->GetName() <<
+	//", leaf="<< i->leaf <<endl;
+	if(i == mLeoim.end()) {
+	  cout <<"FTW_Branch::AbsorbRay RQN_list_remove didn't find view to remove ...\n";
+	  return;
+	}
+	// mNest->RemoveLeaf(i->leaf); // Called in Leaf dtor.
+	delete i->leaf;
+
+	mLeoim.erase(i);
+	label_namebox();
+	return;
+      }
+	*/
+
+      case RQN_list_rebuild:
+
+      case RQN_list_clear: {
+	bool was_expanded = bListExpanded;
+	if(bListExpanded) CollapseList();
+	wipe_leaves();
+	if(was_expanded)  ExpandList();
+	label_namebox();
+	return;
+      }
+
+      } // end switch ray.fRQN
+
+    } else { // leaves not created
+
+      label_namebox();
+
+    }
+
+  } // enf if list-ray
+
+  FTW_Leaf::AbsorbRay(ray);
+}
+
+/**************************************************************************/
+
+void FTW_Branch::CollExpList()
+{
+  if(bListExpanded) CollapseList(); else ExpandList();
+}
+
+void FTW_Branch::ExpandList() {
+  static const Exc_t _eh("FTW_Branch::ExpandList ");
+
+  if(bListExpanded) return; // ants can call this for no good reason
+  if(!bLeavesCreated) {
+    if(((AList*)fImg->fLens)->Size() > 128000) {
+      mNest->GetShell()->Message
+	(_eh + "refusing to expand list longer than 128000 elements.", Eye::MT_wrn);
+      return;
+    }
+    build_leaves( mNest->PackPosAfter(this) );
+    bLeavesCreated = true;
+  } else {
+    for(leaf_iterator i=mLeaves.begin(); i!=mLeaves.end(); ++i)
+      (*i)->ShowRecursively();
+  }
+  bListExpanded = true;
+  label_weeds();
+  mNest->redraw();
+}
+
+void FTW_Branch::CollapseList()
+{
+  if(!bListExpanded) return;
+  for(leaf_iterator i=mLeaves.begin(); i!=mLeaves.end(); ++i)
+    (*i)->HideRecursively();
+  bListExpanded = false;
+  label_weeds();
+  mNest->redraw();
+}
+
+void FTW_Branch::CopyListLeaves(list<FTW_Leaf*>& list)
+{
+  if(!bLeavesCreated) return;
+  copy(mLeaves.begin(), mLeaves.end(), back_inserter(list));
+}
+
+/**************************************************************************/
+
+void FTW_Branch::ShowListMembers()
+{
+  if(bListExpanded)
+    for(leaf_iterator i=mLeaves.begin(); i!=mLeaves.end(); ++i)
+      (*i)->ShowRecursively();
+}
+
+void FTW_Branch::HideListMembers()
+{
+  if(bListExpanded)
+    for(leaf_iterator i=mLeaves.begin(); i!=mLeaves.end(); ++i)
+      (*i)->HideRecursively();
+}
+
+/**************************************************************************/
+
+float FTW_Branch::LeafPosition(FTW_Leaf* leaf)
+{
+  if(!bLeavesCreated) return 0;
+  int j = 0;
+  for(leaf_iterator i=mLeaves.begin(); i!=mLeaves.end(); ++i)
+    if(*i == leaf) return float(j)/mLeaves.size();
+  return 1;
+}
+
+/**************************************************************************/
+/**************************************************************************/
+// protected
+/**************************************************************************/
+/**************************************************************************/
+
+void FTW_Branch::label_namebox() {
+  int s = bLeavesCreated ? mLeaves.size() : ((AList*)fImg->fLens)->Size();
+  if(s) {
+    wName->set_label(GForm("%s [%d]", fImg->fLens->GetName(), s));
+  } else {
+    wName->set_label(GForm("%s []", fImg->fLens->GetName()));
+  }
+}
+
+void FTW_Branch::label_weeds() {
+  if(bListExpanded) {
+    wListExpander->label("@#-2>");
+  } else {
+    wListExpander->label("@#-2>|");
+  }
+  wListExpander->redraw_label();
+  FTW_Leaf::label_weeds();
+}
---
+++ trunk/GledCore/GledView/MTW_View.cxx
@@ -0,0 +1,202 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+
+#include "MTW_View.h"
+#include "MTW_SubView.h"
+#include "MTW_Layout.h"
+#include "FTW_Shell.h"
+#include <Glasses/ZGlass.h>
+#include <Glasses/ShellInfo.h>
+#include <Stones/ZMIR.h>
+#include <Eye/Eye.h>
+#include <Gled/GledNS.h>
+
+#include <GledView/GledViewNS.h>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl.H>
+#include <FL/fl_draw.H>
+
+namespace GNS  = GledNS;
+namespace GVNS = GledViewNS;
+namespace OS   = OptoStructs;
+namespace FGS  = FltkGledStuff;
+
+
+/**************************************************************************/
+// MTW_View
+/**************************************************************************/
+
+void MTW_View::_init()
+{
+  mFltkRep = 0; // Fltk representation.
+  bShown   = false;
+  mSelfRep = 0;
+}
+
+void MTW_View::auto_label()
+{
+  if(mWindow && mWindow->parent() == 0) {
+    m_window_label = mGlass ? 
+      GForm("%s[%s]", mGlass->GetName(), mGlass->VGlassInfo()->fName.Data()) :
+      "<no-glass>";
+    mWindow->label(m_window_label.Data());
+  }
+  if(mSelfRep) {
+    mSelfRep->copy_label(GForm("%s [ID=%d, RC=%d] '%s'",
+			       mGlass->GetName(),
+			       mGlass->GetSaturnID(), mGlass->GetRefCount(),
+			       mGlass->GetTitle()));
+  }
+}
+
+/**************************************************************************/
+
+MTW_View::MTW_View(OS::ZGlassImg* img, FTW_Shell* shell) :
+  OS::A_View(img),
+  FTW_SubShell(shell),
+  Fl_SWM_Client(shell)
+{
+  mGlass = fImg->fLens;
+  _init();
+}
+
+MTW_View::MTW_View(ZGlass* glass, Fl_SWM_Manager* swm_mgr) :
+  OS::A_View(0),
+  FTW_SubShell(0),
+  Fl_SWM_Client(swm_mgr)
+{
+  mGlass = glass;
+  _init();
+}
+
+MTW_View::~MTW_View()
+{}
+
+/**************************************************************************/
+
+void MTW_View::Labelofy()
+{
+  // Sets labels to sub-views ... resizes self.
+
+  int dww = 0;
+  for(lpMTW_SubView_i sv=mSubViews.begin(); sv!=mSubViews.end(); ++sv) {
+    int dx = 0;
+    for(lMTW_Weed_i w=(*sv)->RefWeeds().begin(); w!=(*sv)->RefWeeds().end(); ++w) {
+      Fl_Widget*       o = w->fWeed;
+      GVNS::WeedInfo& wi = *w->fWeedInfo;
+      o->label(w->fWeedInfo->fName.Data());
+      if(wi.bLabel && !wi.bLabelInside) {
+	o->align(FL_ALIGN_LEFT);
+	dx += FGS::swm_label_width(w->fWeedInfo->fName, swm_manager->cell_w());
+	o->position(o->x()+dx, o->y());
+      }
+    }
+    (*sv)->size((*sv)->w() + dx, (*sv)->y());
+    dww += dx;
+  }
+  mFltkRep->size(mFltkRep->w() + dww, mFltkRep->h());
+}
+
+/**************************************************************************/
+
+void MTW_View::AbsorbRay(Ray& ray)
+{
+  if(ray.IsBasicChange()) { auto_label(); }
+
+  if(bShown) {
+    if(ray.fRQN == RayNS::RQN_change) {
+      UpdateDataWeeds(ray.fFID);
+    }
+    else if(ray.fRQN == RayNS::RQN_link_change) {
+      UpdateLinkWeeds(ray.fFID);
+    }
+  }
+}
+
+/**************************************************************************/
+
+void MTW_View::UpdateDataWeeds(FID_t fid)
+{
+  bool update_all = fid.is_null();
+  for(lpMTW_SubView_i sv=mSubViews.begin(); sv!=mSubViews.end(); ++sv) {
+    if(update_all || (*sv)->GetClassInfo()->fFid == fid)
+      (*sv)->UpdateDataWeeds();
+  }
+}
+
+void MTW_View::UpdateLinkWeeds(FID_t fid)
+{
+  bool update_all = fid.is_null();
+  for(lpMTW_SubView_i sv=mSubViews.begin(); sv!=mSubViews.end(); ++sv) {
+    if(update_all || (*sv)->GetClassInfo()->fFid == fid)
+      (*sv)->UpdateLinkWeeds();
+  }
+}
+
+/**************************************************************************/
+// SelfRep
+/**************************************************************************/
+
+MTW_View::SelfRep::SelfRep(MTW_View* v, int x, int y, int w, int h) :
+      Fl_Box(x,y,w,h), fView(v)
+{
+  box(FL_EMBOSSED_BOX);
+  if(labelfont() < FL_BOLD)	labelfont(labelfont() + FL_BOLD);
+  align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+  color(fl_rgb_color(220,200,200));
+}
+
+int MTW_View::SelfRep::handle(int ev) {
+  FTW_Shell* shell = fView->GetShell();
+  if(shell != 0) {
+    switch (ev) {
+
+    case FL_PUSH: {
+      if(Fl::event_button() == 2) {
+	Fl::paste(*this);
+      }
+      if(Fl::event_button() == 3) {
+	shell->FullMenu(fView->fImg, Fl::event_x_root(), Fl::event_y_root());
+      }
+      return 1;
+    }
+
+    case FL_DRAG: {
+      if(Fl::event_state(FL_BUTTON1) && ! Fl::event_inside(this)) {
+	ID_t id          = fView->fImg->fLens->GetSaturnID();
+	const char* text = GForm("%u", id);
+	shell->X_SetSource(fView->fImg);
+	Fl::copy(text, strlen(text), 0);
+	Fl::dnd();
+	return 1;
+      }
+    }
+
+    case FL_DND_ENTER:
+      return (fView->fImg->fIsList) ? 1 : 0;
+
+    case FL_DND_LEAVE:
+      return 1;
+
+    case FL_DND_RELEASE:
+      return (Fl::belowmouse() == this) ? 1 : 0;
+
+    case FL_PASTE: {
+      if(fView->fImg->fIsList && shell->GetSource()->has_contents()) {
+	AList* l = (AList*) fView->fImg->fLens;
+	auto_ptr<ZMIR> mir ( l->MkMir_Add(0) );
+	shell->GetSource()->fix_MIR_beta(mir);
+	fView->fImg->fEye->Send(*mir);
+      }
+      return 1;
+    }
+    } // switch (ev)
+  } 
+
+  return Fl_Box::handle(ev);
+}
