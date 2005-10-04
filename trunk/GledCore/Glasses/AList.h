2614c9bd2200842f6b284594225ca31a9b5d2626
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-10-04 09:56:59 +0000

    Added an inline keyword to make gcc-3.3 happy.

--- trunk/GledCore/Glasses/AList.h
+++ trunk/GledCore/Glasses/AList.h
@@ -304,7 +304,7 @@ class AList : public ZGlass
 
 
 #ifndef __CINT__
-bool AList::Stepper<ZGlass>::is_ok() { return true; }
+inline bool AList::Stepper<ZGlass>::is_ok() { return true; }
 #endif
 
 #endif
