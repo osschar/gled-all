b1feadebf425326c4aec83555225d86a9b2919b1
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-10-11 09:59:45 +0000

    Added member 'TimeStamp_t mStampPointCalcReq' and appropriate stamp pragmas.

--- trunk/Alice/Glasses/PRSBase.h
+++ trunk/Alice/Glasses/PRSBase.h
@@ -12,7 +12,8 @@
 #include <Stones/ZColor.h>
 #include <Glasses/ZImage.h>
 
-class PRSBase : public ZRnrModBase {
+class PRSBase : public ZRnrModBase
+{
   // 7777 RnrCtrl(RnrBits(0,4,0,0))
   MAC_RNR_FRIENDS(PRSBase);
 
@@ -20,9 +21,10 @@ class PRSBase : public ZRnrModBase {
   void _init();
 
  protected:
+  TimeStamp_t              mStampPointCalcReq; //!
 
  public:
-  Float_t                  mMagField;      // X{GST}  7 Value(-range=>[-10,10, 1,1000])
+  Float_t                  mMagField;      // X{GST} Stamp{PointCalcReq} 7 Value(-range=>[-10,10, 1,1000])
   //
   ZColor                   mVertexColor;   // X{GSPT} 7 ColorButt(-join=>1)
   Float_t                  mVertexSize;    // X{GST}  7 Value(-range=>[0.1,64, 1,10])
@@ -42,20 +44,24 @@ class PRSBase : public ZRnrModBase {
   Float_t                  mThetaOff;      // X{GST}  7 Value(-range=>[0,180, 1,100])
   Float_t                  mPhi;           // X{GST}  7 Value(-range=>[0,360, 1,100], -join=>1)
   Float_t                  mPhiOff;        // X{GST}  7 Value(-range=>[0,360, 1,100])
-  Float_t                  mMaxR;          // X{GST}  7 Value(-range=>[0,100000,1], -join=>1)
-  Float_t                  mMaxZ;          // X{GST}  7 Value(-range=>[0,100000,1])
+  Float_t                  mMaxR;          // X{GST} Stamp{PointCalcReq}  7 Value(-range=>[0,100000,1], -join=>1)
+  Float_t                  mMaxZ;          // X{GST} Stamp{PointCalcReq}  7 Value(-range=>[0,100000,1])
   // helix limits 
-  Float_t                  mMaxOrbs;       // X{GST}  7 Value(-range=>[0,20,1,100])
-  Float_t                  mMinAng;        // X{GST}  7 Value(-range=>[1,180 ,1], -join=>1)
-  Float_t                  mDelta;         // X{GST}  7 Value(-range=>[1e-3,100, 1,1000])
+  Float_t                  mMaxOrbs;       // X{GST} Stamp{PointCalcReq}  7 Value(-range=>[0,20,1,100])
+  Float_t                  mMinAng;        // X{GST} Stamp{PointCalcReq}  7 Value(-range=>[1,180 ,1], -join=>1)
+  Float_t                  mDelta;         // X{GST} Stamp{PointCalcReq}  7 Value(-range=>[1e-3,100, 1,1000])
   // track textures
-  ZLink<ZImage>                  mTexture;      // X{gST} L{} RnrBits{4,0,5,0, 0,0,0,0}
+  ZLink<ZImage>            mTexture;      // X{gST} L{} RnrBits{4,0,5,0, 0,0,0,0}
   Float_t                  mTexFactor;    // X{GST}  7 Value(-range=>[0,1000, 1,100])
   Float_t                  mTexUCoor;     // X{GST}  7 Value(-range=>[0,1, 1,1000], -join=>1)
   Float_t                  mTexVCoor;     // X{GST}  7 Value(-range=>[0,1, 1,1000])
 
   PRSBase(const Text_t* n="PRSBase", const Text_t* t=0) :
     ZRnrModBase(n,t) { _init(); }
+
+  virtual void SetStamps(TimeStamp_t s)
+  { ZRnrModBase::SetStamps(s); mStampPointCalcReq = s; }
+
 
   static Float_t sDefaultMagField;
 
