76985f76f536b300a4f8408507b05b35b95611b1
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-10-14 14:01:55 +0000

    New glass TimeScreenText: a TimeMakerClient that displays current time (subclass of ScreenText).

---
+++ trunk/Geom1/Glasses/TimeScreenText.cxx
@@ -0,0 +1,29 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// TimeScreenText
+//
+//
+
+#include "TimeScreenText.h"
+#include "TimeScreenText.c7"
+
+ClassImp(TimeScreenText);
+
+/**************************************************************************/
+
+void TimeScreenText::_init()
+{}
+
+/**************************************************************************/
+
+void TimeScreenText::TimeTick(Double_t t, Double_t dt)
+{
+  SetText(GForm(mFormat, t));
+}
+
+/**************************************************************************/
