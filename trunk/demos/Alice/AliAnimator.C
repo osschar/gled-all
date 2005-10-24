ecfa2f6446d7ff4dbb6db7b1022b7a7eed1da4df
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-10-24 14:07:53 +0000

    Added camera movement along a spiral and a convenience function prepare_for_capture().

--- trunk/demos/Alice/AliAnimator.C
+++ trunk/demos/Alice/AliAnimator.C
@@ -17,6 +17,8 @@ ZGeoNode*     volt      = 0;
 Scene*        gscene    = 0;
 ZAliLoad*     aliload   = 0;
 
+PupilInfo*    movie_pupil = 0;
+
 // default event display layout
 const Text_t* particle_layout =
   "ZNode(RnrSelf[4],RnrElements[4],RnrMod[8]):"
@@ -35,6 +37,14 @@ const Text_t* particle_layout =
 
 class MultiBoard;
 class ZImage;
+
+class WSSeed;
+void setup_camera_path(WSSeed* s, Int_t N,
+		       Double_t x0, Double_t y0, Double_t z0,
+		       Double_t dphi, Double_t dr0,
+		       Double_t t_start_dr, Double_t max_r);
+
+void rnr_off(ZGlass* top, const Text_t* path);
 
 /**************************************************************************/
 
@@ -87,6 +97,7 @@ void AliAnimator(const Text_t* dirname = 0,
   image2->SetLoadAdEnlight(true);
 
   CREATE_ADD_GLASS(mcrst, MCTrackRnrStyle, var, "MC Track RnrStyle", 0);
+  mcrst->SetTrackWidth(2);
   mcrst->SetTexture(image2);
   CREATE_ADD_GLASS(recrst, RecTrackRS, var, "Rec Track RnrStyle", 0);
 
@@ -97,13 +108,9 @@ void AliAnimator(const Text_t* dirname = 0,
   CREATE_ADD_GLASS(giis, GIImportStyle, var, "ImportMode", 0);
 
 
-  ZAliLoad* al = new ZAliLoad();
-  aliload = al;
-  al->SetOM(-2);
-  al->SetUseOM(true);
-  g_queen->CheckIn(al);
-  g_scene->Add(al);
-
+  ASSIGN_ADD_GLASS(aliload, ZAliLoad, g_scene, "ZAliLoad", 0);
+  aliload->SetOM(-2);
+  aliload->SetUseOM(true);
 
   /**************************************************************************/
   // Anim
@@ -116,6 +123,7 @@ void AliAnimator(const Text_t* dirname = 0,
   stxt->SetX(-1);
   stxt->SetFormat("time: %4.1f ns");
   tclist->Add(stxt);
+
   /**************************************************************************/
 
   CREATE_ADD_GLASS(etor, Eventor, g_queen, "Animator", 0);
@@ -129,17 +137,22 @@ void AliAnimator(const Text_t* dirname = 0,
   tmaker->SetMaxT(1000);
   tmaker->SetClients(tclist);
 
-
   etor->Add(g_scene->FindLensByPath("Lamp Origin/Dynamo/Lamp Origin Rotator"));
 
   CREATE_ADD_GLASS(sdumper, ScreenDumper, etor, "ScreenDumper", 0);
-  sdumper->SetFileNameFmt("");
+  sdumper->SetFileNameFmt("/foo/kabl/mXXX/img-%04d");
   sdumper->SetWaitDump(true);
 
   /**************************************************************************/
 
+  Double_t fade_time = 30;
+  Double_t max_time  = 35;
+  
+
   CREATE_ADD_GLASS(mpupil, PupilInfo, g_queen, "MoviePupil", 0);
+  movie_pupil = mpupil; // assign to global
   mpupil->SetAutoRedraw(false);
+  mpupil->SetZFov(65);
   mpupil->SetBlend(true);
   mpupil->SetShowView(false);
   mpupil->Add(g_scene);
@@ -147,13 +160,46 @@ void AliAnimator(const Text_t* dirname = 0,
   mpupil->SetUpRefAxis(2);
 
   sdumper->SetPupil(mpupil);
+
   mcrst->SetCheckT(true);
   mcrst->SetMaxT(0.1);
-  mcrst->SetFadeT(30);
-  mcrst->SetKillT(35);
+  mcrst->SetFadeT(fade_time);
+  mcrst->SetKillT(max_time);
   mcrst->SetAnimDeltaT(10);
   mcrst->SetTScale(-9);
   mcrst->SetTrackWidth(1.6);
+
+  // Camera movement
+  //--------------------------------
+
+  CREATE_ATT_GLASS(wker, WSWalker, tmaker, AddClient, "WSWalker", 0);
+
+  CREATE_ADD_GLASS(cam_path, WSSeed, g_scene, "Camera Path", 0);
+  Int_t travel_time = fade_time;
+  setup_camera_path(cam_path, travel_time,
+		    -2, 0, 0,
+		    -TMath::TwoPi()/travel_time, 0.4,
+		    2, 10);
+  cam_path->RotateLF(2, 3, TMath::PiOver2());
+  cam_path->MovePF(2, 1);
+  cam_path->SetFat(false);
+
+  CREATE_ADD_GLASS(traveler, SMorph, cam_path, "Travel SMorph", 0);
+  traveler->SetScale(0.25);
+
+  wker->SetSeed(cam_path);
+  wker->SetNode(traveler);
+  wker->SetTime(0);
+
+  mpupil->SetCameraBase(traveler);
+  mpupil->SetLookAt(g_scene);
+
+  // Fix things
+  //--------------------------------
+
+  // Geometry line width.
+  ZGlBlending* zglb = dynamic_cast<ZGlBlending*>(var->FindLensByPath("Blending"));
+  zglb->SetLineWidth(2);
 
   /**************************************************************************/
   // End Anim
@@ -191,24 +237,84 @@ void AliAnimator(const Text_t* dirname = 0,
     l->Swallow(new ZGlass("Particle", particle_layout));
   }
 
-  // NestInfo* geo_nest = new_nest("Geometry's nest", geometry_mini_layout);
-  // geo_nest->Add(g_simple_geometry);
-  // geo_nest->SetDefW(66);
-  
   spawn_default_gui();
 
   Gled::LoadMacro("zaliload_metagui.C");
   MetaViewInfo* mvi = make_zaliload_metagui();
   mvi->SetExpertP(true);
-  //g_shell->SpawnMetaGui(al, mvi);
+  g_shell->SpawnMetaGui(aliload, mvi);
 
   // Movie!!
   g_shell->AddSubShell(mpupil);
+  mpupil->SetWidth(800);
+  mpupil->SetHeight(600);
+  g_nest->ImportKings();
 
   if(dirname != 0) {
-    al->SetDataDir(dirname);
+    aliload->SetDataDir(dirname);
     // Shoot a MIR to have functional GUI during processing: 	 
-    ZMIR* setup_mir = al->S_LoadVSD(); 	 
+    ZMIR* setup_mir = aliload->S_LoadVSD(); 	 
     g_saturn->ShootMIR(setup_mir);
   }
 }
+
+void prepare_for_capture()
+{
+  rnr_off(g_scene, "Camera Path");
+  rnr_off(g_scene, "Lamp Origin");
+  rnr_off(g_scene, "Markers");
+
+  printf("Importing particles ...\n");
+  aliload->SelectParticles(0, true);
+  printf("Turning off display lists ...\n");
+  aliload->SetUseDLRec(false);
+  printf("Done.\n");
+}
+
+/**************************************************************************/
+/**************************************************************************/
+
+void setup_camera_path(WSSeed* s, Int_t N,
+		       Double_t x0, Double_t y0, Double_t z0,
+		       Double_t dphi, Double_t dr0,
+		       Double_t t_start_dr, Double_t max_r)
+{
+  // !!! So far ignoring t_start_dr and max_r !!!
+
+  Double_t t   = 0;
+  Double_t r   = TMath::Sqrt(x0*x0 + y0*y0);
+  Double_t phi = TMath::ATan2(y0, x0);
+
+  for(Int_t i=0; i<=N; ++i) {
+    WSPoint* p = new WSPoint(GForm("Point %d", i));
+    double ss = TMath::Sin(phi);
+    double cc = TMath::Cos(phi);
+    p->SetPos(r*cc, r*ss, z0);
+
+    Double_t dr = 0;
+    if(t >= t_start_dr) dr = dr0;
+    if(r + dr > max_r)  dr = max_r - r;
+
+    Double_t der_phi  = TMath::ATan2(dr*ss + r*dphi*cc, dr*cc - r*dphi*ss);
+    p->RotateLF(1, 2, der_phi);
+
+    g_queen->CheckIn(p);
+    s->Add(p);
+
+    phi += dphi;
+    r   += dr;
+    t   += 1;
+  }
+}
+
+void rnr_off(ZGlass* top, const Text_t* path)
+{
+  ZNode* n = dynamic_cast<ZNode*>(top->FindLensByPath(path));
+  if(n) {
+    n->SetRnrSelf(false);
+    n->SetRnrElements(false);
+  } else {
+    printf("WRN: rnr_off '%s' not found under %s.\n", path, top->Identify().Data());
+  }
+}
+
