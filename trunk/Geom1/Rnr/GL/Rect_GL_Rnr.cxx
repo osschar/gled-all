e281b49cd687b155a18add4ee92295d3722289fd
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2005-12-25 19:38:01 +0000

    Removed blending change (should be done elsewhere when needed); clean-up.

--- trunk/Geom1/Rnr/GL/Rect_GL_Rnr.cxx
+++ trunk/Geom1/Rnr/GL/Rect_GL_Rnr.cxx
@@ -6,39 +6,30 @@ void Rect_GL_Rnr::Draw(RnrDriver* rd)
 {
   if(mRect->mWidth <= 0) return;
 
-  glPushAttrib(GL_LINE_BIT     | GL_COLOR_BUFFER_BIT |
-	       GL_LIGHTING_BIT | GL_DEPTH_BUFFER_BIT  );
+  glPushAttrib(GL_LINE_BIT | GL_COLOR_BUFFER_BIT);
   glPushMatrix();
-  glDisable(GL_LIGHTING);
-  glScalef(mRect->mULen, mRect->mVLen, 1);
 
   // Strips
-  glLineWidth(mRect->mWidth);
+  if(mRect->mWidth)
+    glLineWidth(mRect->mWidth);
   glColor4fv(mRect->mColor());
-  if(mRect->mColor.a() < 1) {
-    glEnable(GL_BLEND);
-    glBlendEquation(GL_FUNC_ADD);
-    glBlendFunc(GL_SRC_ALPHA, GL_ZERO);
-    glDepthMask(GL_FALSE);
-  }
+  glBegin(GL_LINES);
+  const Float_t mX = 0.5*mRect->mULen, mY = 0.5*mRect->mVLen;
   if(mRect->mUStrips) {
-    Float_t step=1.0/mRect->mUStrips, u=-0.5;
-    glBegin(GL_LINES);
-    for(unsigned short i=0; i<=mRect->mUStrips; ++i) {
-      glVertex3f(u,-0.5,0); glVertex3f(u,0.5,0);
+    Float_t step=mRect->mULen/mRect->mUStrips, u=-mX;
+    for(Short_t i=0; i<=mRect->mUStrips; ++i) {
+      glVertex3f(u, -mY, 0); glVertex3f(u, mY, 0);
       u += step;
     }
-    glEnd();
   }
   if(mRect->mVStrips) {
-    Float_t step=1.0/mRect->mVStrips, v=-0.5;
-    glBegin(GL_LINES);
-    for(unsigned short i=0; i<=mRect->mVStrips; ++i) {
-      glVertex3f(-0.5,v,0); glVertex3f(0.5,v,0);
+    Float_t step=mRect->mVLen/mRect->mVStrips, v=-mY;
+    for(Short_t i=0; i<=mRect->mVStrips; ++i) {
+      glVertex3f(-mX, v, 0); glVertex3f(mX, v, 0);
       v += step;
     }
-    glEnd();
   }
+  glEnd();
 
   glPopMatrix();
   glPopAttrib();
