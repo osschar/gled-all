a7a9d673008123f2ff5f9d1b35e0101506bc8897
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2006-01-06 14:28:54 +0000

    New glass ZGlColorFader; somewhat supports centrally scaled color, point size and line width.

---
+++ trunk/GledCore/Glasses/ZGlColorFader.h
@@ -0,0 +1,37 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledCore_ZGlColorFader_H
+#define GledCore_ZGlColorFader_H
+
+#include <Glasses/ZRnrModBase.h>
+#include <Stones/ZColor.h>
+
+class ZGlColorFader : public ZRnrModBase
+{
+  // 7777 RnrCtrl(RnrBits(0,4,0,0))
+  MAC_RNR_FRIENDS(ZGlColorFader);
+
+private:
+  void _init();
+
+protected:
+  ZColor  mColorFac;     // X{PRGS} 7 ColorButt()
+  Float_t mPointSizeFac; // X{GS}   7 Value(-range=>[0,128, 1,1000])
+  Float_t mLineWidthFac; // X{GS}   7 Value(-range=>[0,128, 1,1000])
+
+public:
+  ZGlColorFader(const Text_t* n="ZGlColorFader", const Text_t* t=0) :
+    ZRnrModBase(n,t) { _init(); }
+
+
+
+#include "ZGlColorFader.h7"
+  ClassDef(ZGlColorFader, 1);
+}; // endclass ZGlColorFader
+
+
+#endif
---
+++ trunk/GledCore/Rnr/GL/ZGlColorFader_GL_Rnr.h
@@ -0,0 +1,30 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledCore_ZGlColorFader_GL_RNR_H
+#define GledCore_ZGlColorFader_GL_RNR_H
+
+#include <Glasses/ZGlColorFader.h>
+#include <Rnr/GL/ZRnrModBase_GL_Rnr.h>
+
+class ZGlColorFader_GL_Rnr : public ZRnrModBase_GL_Rnr {
+private:
+  void _init();
+
+protected:
+  ZGlColorFader* mZGlColorFader;
+
+public:
+  ZGlColorFader_GL_Rnr(ZGlColorFader* idol) :
+    ZRnrModBase_GL_Rnr(idol), mZGlColorFader(idol)
+  { _init(); }
+
+  virtual void PreDraw(RnrDriver* rd);
+  virtual void Draw(RnrDriver* rd);
+  virtual void PostDraw(RnrDriver* rd);
+}; // endclass ZGlColorFader_GL_Rnr
+
+#endif
