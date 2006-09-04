ef9b7a706a9c78007c9eb7d253acdaf7cafeb4d0
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2006-09-04 15:16:44 +0000

    Added ZVector vec to the test.

--- trunk/demos/GledCore/string_map.C
+++ trunk/demos/GledCore/string_map.C
@@ -1,6 +1,7 @@
 #include <glass_defines.h>
 
 ZStringMap *smap=0;
+ZVector *vec =0;
 ZGlass *a=0, *b=0, *c=0;
 
 void string_map()
@@ -18,6 +19,11 @@ void string_map()
   ASSIGN_ADD_GLASS(a, ZGlass, g_scene, "Alpha", "CINT var a");
   ASSIGN_ADD_GLASS(b, ZGlass, g_scene, "Bravo", "CINT var b");
   ASSIGN_ADD_GLASS(c, ZGlass, g_scene, "Charlie", "CINT var c");
+
+  ASSIGN_ADD_GLASS(vec, ZVector, g_scene, "ZVector", "CINT var vec");
+  vec->Add(a);
+  vec->Resize(4);
+  vec->SetElementById(c, 3);
 
   Gled::LoadMacro("eye.C");
   eye(false);
