cae83ed19a5e5d4155045043995e985fb6378bde
Matevž Tadel <osschar@users.noreply.github.com>
2006-10-09 14:37:31 +0000

    New fltk weed to allow editing of TVector3.

--- Geom1/perllib/GLED_Geom1_Widgets.pm
+++ Geom1/perllib/GLED_Geom1_Widgets.pm
@@ -35,8 +35,11 @@ sub make_widget {
 
 sub make_cxx_cb {
   my $S = shift;
-return <<"fnord";
-void ${::CLASSNAME}View::$S->{Methodbase}_Callback($S->{Widget}* o) {
+  my $r = "void ${::CLASSNAME}View::$S->{Methodbase}_Callback($S->{Widget}* o) {\n";
+  if($S->{-const}) {
+    $r .= "  $S->{Methodbase}_Update(o)\n;";
+  } else {
+    $r .= <<"fnord";
   Eye* e = (mView->fImg) ? mView->fImg->fEye : 0;
   if(e) {
     auto_ptr<ZMIR> _m( mIdol->S_Set$S->{Methodbase}(TLorentzVector(o->x(),o->y(),o->z(),o->t())) );
@@ -45,8 +48,9 @@ void ${::CLASSNAME}View::$S->{Methodbase}_Callback($S->{Widget}* o) {
   } else {
     mIdol->Set$S->{Methodbase}(TLorentzVector(o->x(),o->y(),o->z(),o->t()));
   }
-}\n
 fnord
+  }
+  return $r . "}\n\n";
 }
 
 sub make_weed_update {
@@ -54,6 +58,57 @@ sub make_weed_update {
   $S->make_weed_update_A() .
     "  const TLorentzVector& x = mIdol->Ref$S->{Methodbase}();\n" .
     "  w->set(x.X(), x.Y(), x.Z(), x.T());\n" .
+  $S->make_weed_update_B();
+}
+
+########################################################################
+
+package GLED::Widgets::Vector3; @ISA = ('GLED::Widgets');
+
+sub new {
+  my $proto = shift;
+  my $S = $proto->SUPER::new(@_);
+  $S->{Type} = "TVector3";
+  $S->{Widget} = "Fl_Vector3";
+  $S->{Include} = "FL/Fl_Vector3.h";
+  $S->{LabelP}       = "true";
+  $S->{LabelInsideP} = "false";
+  $S->{CanResizeP}   = "true";
+  $S->{-width} = 28 unless exists $S->{-width};
+  $S->{-height} = 1 unless exists $S->{-height};
+  return $S;
+}
+
+sub make_widget {
+  my $S=shift;
+  return $S->make_widget_A() . $S->make_widget_B();
+}
+
+sub make_cxx_cb {
+  my $S = shift;
+  my $r = "void ${::CLASSNAME}View::$S->{Methodbase}_Callback($S->{Widget}* o) {\n";
+  if($S->{-const}) {
+    $r .= "  $S->{Methodbase}_Update(o)\n;";
+  } else {
+    $r .= <<"fnord";
+  Eye* e = (mView->fImg) ? mView->fImg->fEye : 0;
+  if(e) {
+    auto_ptr<ZMIR> _m( mIdol->S_Set$S->{Methodbase}(TVector3(o->x(),o->y(),o->z())) );
+    e->Send(*_m);
+    SetUpdateTimer();
+  } else {
+    mIdol->Set$S->{Methodbase}(TVector3(o->x(),o->y(),o->z()));
+  }
+fnord
+  }
+  return $r . "}\n\n";
+}
+
+sub make_weed_update {
+  my $S = shift;
+  $S->make_weed_update_A() .
+    "  const TVector3& x = mIdol->Ref$S->{Methodbase}();\n" .
+    "  w->set(x.X(), x.Y(), x.Z());\n" .
   $S->make_weed_update_B();
 }
 
