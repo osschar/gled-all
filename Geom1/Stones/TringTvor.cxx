f583df7458dfe8c49395fd8071506b3e0086a3c5
Matevž Tadel <osschar@users.noreply.github.com>
2006-10-16 10:54:52 +0000

    Moved Tring/TubeTvor from Tvor/ to Stones/; sub-classed them from ZRCStone.

--- Geom1/Stones/TringTvor.cxx
+++ Geom1/Stones/TringTvor.cxx
@@ -11,15 +11,68 @@
 #include "TringTvor.h"
 #include <TVector3.h>
 
+ClassImp(TringTvor);
+
+void TringTvor::_init()
+{
+  mVerts  = 0;  mNorms      = 0;  mCols      = 0;  mTexs = 0;
+  mTrings = 0;  mTringNorms = 0;  mTringCols = 0;
+
+  mNStripEls   = 0;  mStripEls    = 0;  mStripTrings = 0;
+  mNStrips     = 0;  mStripBegs   = 0;  mStripLens   = 0;
+}
+
+TringTvor::TringTvor() :
+  bSmoothShade(false), bColP(false), bTexP(false),
+  mNVerts  (0),
+  mNTrings (0)
+{
+  _init();
+}
+
+TringTvor::TringTvor(Int_t nv, Int_t nt) :
+  bSmoothShade(false), bColP(false), bTexP(false),
+  mNVerts  (nv),
+  mNTrings (nt)
+{
+  _init();
+  MakePrimaryArrays();
+}
+
 TringTvor::TringTvor(Int_t nv, Int_t nt, Bool_t smoothp,
 		     Bool_t colp, Bool_t texp) :
-  bSmoothShade(smoothp),
-  bColP(colp), bTexP(texp),
-  mNVerts(nv),
-  mNTrings(nt)
+  bSmoothShade(smoothp), bColP(colp), bTexP(texp),
+  mNVerts  (nv),
+  mNTrings (nt)
 {
-  mVerts  = new Float_t[3*mNVerts];  mNorms = 0; mCols = 0; mTexs = 0;
-  mTrings = new Int_t  [3*mNTrings]; mTringNorms = 0; mTringCols = 0;
+  _init();
+  MakePrimaryArrays();
+  MakeSecondaryArrays();
+}
+
+TringTvor::~TringTvor()
+{
+  DeleteSecondaryArrays();
+  DeleteTriangleStrips();
+  DeletePrimaryArrays();
+}
+
+/**************************************************************************/
+
+void TringTvor::MakePrimaryArrays()
+{
+  mVerts  = new Float_t[3*mNVerts];
+  mTrings = new Int_t  [3*mNTrings];
+}
+
+void TringTvor::DeletePrimaryArrays()
+{
+  delete [] mVerts;  mVerts  = 0;
+  delete [] mTrings; mTrings = 0;
+}
+
+void TringTvor::MakeSecondaryArrays()
+{
   if(bSmoothShade) {
     mNorms = new Float_t[3*mNVerts];
     if(bColP) mCols = new UChar_t[4*mNVerts];
@@ -28,26 +81,24 @@ TringTvor::TringTvor(Int_t nv, Int_t nt, Bool_t smoothp,
     if(bColP) mTringCols = new UChar_t[4*mNTrings];
   }
   if(bTexP) mTexs = new Float_t[2*mNVerts];
-
-  mNStripEls = 0;
-  mStripEls    = 0;
-  mStripTrings = 0;
-  mNStrips   = 0;
-  mStripBegs = 0;
-  mStripLens = 0;
 }
 
-TringTvor::~TringTvor()
+void TringTvor::DeleteSecondaryArrays()
 {
-  delete [] mVerts;    delete [] mNorms; delete [] mCols; delete [] mTexs;
-  delete [] mTrings;   delete [] mTringNorms; delete [] mTringCols;
-  delete [] mStripEls; delete [] mStripBegs; delete [] mStripLens;
+  delete [] mNorms; mNorms = 0;
+  delete [] mCols;  mCols  = 0;
+  delete [] mTexs;  mTexs  = 0;
+
+  delete [] mTringNorms; mTringNorms = 0;
+  delete [] mTringCols;  mTringCols  = 0;
 }
 
 /**************************************************************************/
 
 void TringTvor::GenerateTriangleNormals()
 {
+  if (!mTringNorms) MakeTringNorms();
+
   TVector3 e1, e2, n;
   for(Int_t t=0; t<mNTrings; ++t) {
     Int_t*    T = Triangle(t);
@@ -65,6 +116,9 @@ void TringTvor::GenerateTriangleNormals()
 void TringTvor::GenerateTriangleNormalsAndColors
   (void (*foo)(Float_t*, UChar_t*, void*), void* ud)
 {
+  if (!mTringNorms) MakeTringNorms();
+  if (!mTringCols)  MakeTringCols();
+
   TVector3 e1, e2, n;
   Float_t  cg[3];
 
@@ -84,6 +138,58 @@ void TringTvor::GenerateTriangleNormalsAndColors
     cg[2] = (v0[2] + v1[2] + v2[2]) / 3;
     foo(cg, TriangleColor(t), ud);
   }
+}
+
+/**************************************************************************/
+
+void TringTvor::GenerateVertexNormals()
+{
+  // Generate per-vertex normals by averaging over normals of all triangles
+  // sharing the vertex.
+  // Requires building of intermediate structure.
+  //
+  // ??? Need an argument: weight normal contribution by triangle area
+  // ??? or ... perhaps better ... by vertex angle.
+
+  // Should generate tring-normals if they do not exist?
+  // Depends ...
+
+  vector<Int_t> *trings_per_vert = new vector<Int_t> [mNVerts];
+
+  Int_t* T = mTrings;
+  for (Int_t t=0; t<mNTrings; ++t, T+=3)
+    {
+      trings_per_vert[T[0]].push_back(t);
+      trings_per_vert[T[1]].push_back(t);
+      trings_per_vert[T[2]].push_back(t);
+    }
+
+  if (!mNorms) MakeNorms();
+
+  Float_t *v0, *v1, *v2;
+  Int_t   *t;
+  Float_t e1[3], e2[3], n[3];
+  Float_t* N = mNorms;
+  for (Int_t i=0; i<mNVerts; ++i, N+=3)
+    {
+      vector<Int_t>& v = trings_per_vert[i];
+      Int_t       size = (Int_t) v.size();
+      //printf("V=%3d : Nt = %2d : ", i, size);
+      N[0] = N[1] = N[2] = 0;
+      for (Int_t j=0; j<size; ++j) {
+        //printf("%3d ", v[j]);
+        t  = Triangle(v[j]);
+        v0 = Vertex(t[0]); v1 = Vertex(t[1]); v2 = Vertex(t[2]);
+        e1[0] = v1[0] - v0[0]; e1[1] = v1[1] - v0[1]; e1[2] = v1[2] - v0[2];
+        e2[0] = v2[0] - v0[0]; e2[1] = v2[1] - v0[1]; e2[2] = v2[2] - v0[2];
+        TMath::NormCross(e1, e2, n);
+        N[0] += n[0]; N[1] += n[1]; N[2] += n[2];
+      }
+      TMath::Normalize(N);
+      //printf(" : %f %f %f\n", N[0], N[1], N[2]);
+    }
+
+  delete [] trings_per_vert;
 }
 
 /**************************************************************************/
@@ -157,13 +263,15 @@ void TringTvor::GenerateTriangleStrips(Int_t max_verts)
 
   actcDelete(tc);
 
+  if (mNStripEls || mNStrips) DeleteTriangleStrips();
+
   mNStripEls = cnt;
   mNStrips   = strip_list.size();
 
-  mStripEls    = new Int_t[mNStripEls];
-  mStripTrings = new Int_t[mNStripEls];
-  mStripBegs = new Int_t*[mNStrips];
-  mStripLens = new Int_t[mNStrips];
+  mStripEls    = new Int_t [mNStripEls];
+  mStripTrings = new Int_t [mNStripEls];
+  mStripBegs   = new Int_t*[mNStrips];
+  mStripLens   = new Int_t [mNStrips];
 
   //printf("Now building strip data, num_idx=%d, num_strips=%d.\n",
   // mNStripEls, mNStrips);
@@ -197,4 +305,62 @@ void TringTvor::GenerateTriangleStrips(Int_t max_verts)
     ++strip_idx;
   }
 
+}
+
+void TringTvor::DeleteTriangleStrips()
+{
+  delete [] mStripEls;   mStripEls  = 0;
+  delete [] mStripBegs;  mStripBegs = 0;
+  delete [] mStripLens;  mStripLens = 0;
+}
+
+/**************************************************************************/
+// Export
+/**************************************************************************/
+
+namespace {
+template<typename T> void dump3vec(ostream& o, T v) {
+  o << "< " << v[0] << ", " << v[1] << ", " << v[2] << " >, \n";
+}
+}
+
+void TringTvor::ExportPovMesh(ostream& o, Bool_t smoothp)
+{
+  // Exports triangle-data as POV mesh2 object.
+  // By experimentation I would say that smooth triangles do not work properly
+  // in pov-3.6.1 (15.10.2006).
+
+  o << "mesh2 {\n";
+
+  {
+    o << "  vertex_vectors { " << mNVerts << ",\n";
+    Float_t* V = mVerts;
+    for (Int_t i=0; i<mNVerts; ++i, V+=3) {
+      o << "    "; dump3vec<Float_t*>(o, V);
+    }
+    o << "  }\n";
+  }
+
+  if (smoothp) {
+    Bool_t no_normals = (mNorms == 0);
+    if (no_normals) GenerateVertexNormals();
+    o << "  normal_vectors { " << mNVerts << ",\n";
+    Float_t* N = mNorms;
+    for (Int_t i=0; i<mNVerts; ++i, N+=3) {
+      o << "    "; dump3vec<Float_t*>(o, N);
+    }
+    o << "  }\n";
+    if (no_normals) { delete [] mNorms; mNorms = 0; }
+  }
+
+  {
+    o << "  face_indices { " << mNTrings << ",\n";
+    Int_t* T = mTrings;
+    for (Int_t i=0; i<mNTrings; ++i, T+=3) {
+      o << "    "; dump3vec<Int_t*>(o, T);
+    }
+    o << "  }\n";
+  }  
+
+  o << "}\n";
 }
