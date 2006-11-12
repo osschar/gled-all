ddfd961b082a679ce47d5141bd221b756a20280f
Matevž Tadel <osschar@users.noreply.github.com>
2006-11-12 20:12:23 +0000

    This commit was generated by cvs2svn to compensate for changes in r1577, which included commits to RCS files with non-trunk default branches.

---
+++ Var1/Glasses/TabletReader.cxx
@@ -0,0 +1,25 @@
+// $Header$
+
+// Copyright (C) 1999-2005, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+//__________________________________________________________________________
+// TabletReader
+//
+//
+
+#include "TabletReader.h"
+#include "TabletReader.c7"
+
+ClassImp(TabletReader)
+
+/**************************************************************************/
+
+void TabletReader::_init()
+{}
+
+/**************************************************************************/
+
+
+/**************************************************************************/
