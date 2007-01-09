51f0b239f2af4d5f4c941fd151e762e932c554e6
Matevž Tadel <osschar@users.noreply.github.com>
2007-01-09 18:08:16 +0000

    Added method MultiplyVec3IP().

--- Var1/Stones/HTrans.h
+++ Var1/Stones/HTrans.h
@@ -136,6 +136,7 @@ public:
   void     RotateIP(TVector3& v) const;
   TVector3 Rotate(const TVector3& v) const;
 
+  void     MultiplyVec3IP(TT* in, TT w) const;
   void     MultiplyVec3(const TT* in, TT w, TT* out) const;
   void     RotateVec3(const TT* in, TT* out) const;
 
