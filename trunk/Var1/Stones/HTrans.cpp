5e54bb9e0d6a83c0440b02cc30699fbb54299647
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2007-01-09 18:08:16 +0000

    Added method MultiplyVec3IP().

--- trunk/Var1/Stones/HTrans.cpp
+++ trunk/Var1/Stones/HTrans.cpp
@@ -413,6 +413,16 @@ inline TVector3 HTrans<TT>::Rotate(const TVector3& v) const
 /**************************************************************************/
 
 template<class TT>
+inline void HTrans<TT>::MultiplyVec3IP(TT* in, TT w) const
+{
+  TT out[3];
+  out[0] = M[F00]*in[0] + M[F01]*in[1] + M[F02]*in[2] + M[F03]*w;
+  out[1] = M[F10]*in[0] + M[F11]*in[1] + M[F12]*in[2] + M[F13]*w;
+  out[2] = M[F20]*in[0] + M[F21]*in[1] + M[F22]*in[2] + M[F23]*w;
+  in[0] = out[0]; in[1] = out[1]; in[2] = out[2];
+}
+
+template<class TT>
 inline void HTrans<TT>::MultiplyVec3(const TT* in, TT w, TT* out) const
 {
   out[0] = M[F00]*in[0] + M[F01]*in[1] + M[F02]*in[2] + M[F03]*w;
