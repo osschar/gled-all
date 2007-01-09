a3c1708e80fb69c8435e41deddd68c152f7dd036
Matevž Tadel <osschar@users.noreply.github.com>
2007-01-09 18:08:11 +0000

    Added structures to hold vertex-connectivity data and method to build them.

--- Var1/Glasses/TriMesh.cxx
+++ Var1/Glasses/TriMesh.cxx
@@ -10,6 +10,10 @@
 // Wrapper over TringTvor (triangulation data) and Opcode structures.
 //
 // User is responsible for (re)creation of aabboxes in TringTvor.
+//
+// Structure VConnData must be reduced (possibly use quantization to shorts.
+// Structure VertexData should be extended with per-point values.
+// I guess something like gl uniform varibles (including vector forms).
 
 
 #include "TriMesh.h"
@@ -115,14 +119,9 @@ void TriMesh::ImportRectTerrain(RectTerrain* rt)
   TringTvor* tt = 0;
   { GLensReadHolder _rlck(rt);
     printf("lock terrain\n");
-    tt = rt->GetTTvor();
-    if(tt == 0) {
-      rt->MakeTringTvor();
-      tt = rt->GetTTvor();
-      if(tt == 0)
-        throw(_eh + "tvor null after MakeTringTvor().");
-    }
-    rt->DisownTringTvor();
+    tt = rt->SpawnTringTvor(true, true, true, false);
+    if(tt == 0)
+      throw(_eh + "tvor null after MakeTringTvor().");
     printf("unlock terrain\n");
   }
   GLensWriteHolder _wlck(this);
@@ -184,6 +183,9 @@ void TriMesh::ImportGTSurf(GTSurf* gts)
   gts_surface_foreach_vertex(surf, (GtsFunc) Dumper::vdump, &arg);
   gts_surface_foreach_face  (surf, (GtsFunc) Dumper::fdump, &arg);
 
+  // tt->MakeSecondaryArrays(true,  true, false);
+  // tt->MakeSecondaryArrays(false, true, false);
+  tt->GenerateVertexNormals();
   tt->GenerateTriangleNormals();
 
   GLensWriteHolder _wlck(this);
@@ -321,4 +323,153 @@ void TriMesh::MakeTetrahedron(Float_t l1, Float_t l2, Float_t w, Float_t h)
   C.GenerateTriangleNormals();
 
   Stamp(FID());
+}
+
+/**************************************************************************/
+// Vertex algorithms
+/**************************************************************************/
+
+void TriMesh::BuildVertexConnections()
+{
+  static const Exc_t _eh("TriMesh::BuildVertexConnections ");
+
+  if (!mTTvor)
+    throw(_eh + "mTTvor is null.");
+
+  const Int_t nVerts = mTTvor->mNVerts;
+
+  vector<Int_t> *trings_per_vert = new vector<Int_t> [nVerts];
+  mTTvor->FindTrianglesPerVertex(trings_per_vert);
+
+  vector<VertexData> vertex_data_vec(nVerts);
+  mVDataVec.swap(vertex_data_vec);
+
+  for (Int_t v = 0; v < nVerts; ++v)
+  {
+    VertexData& vdata = mVDataVec[v];
+
+    vector<Int_t>& ts = trings_per_vert[v];
+    Int_t     ts_size = (Int_t) ts.size();
+
+    using namespace Opcode;
+
+    // Vertex with loop index 'v' plays the role of v1.
+    const Point& v1 = *(Point*) mTTvor->Vertex(v);
+
+    // If one has an overall parametrization of the surface shape,
+    // the derivatives with respect to major directions can be calculated.
+    // Ideally, the principal directions can be defined as a function of
+    // vertex coordinates, like u = f(x,y,z), v=g(x,y,z).
+    // TriMesh is the right place to know about these.
+    //
+    // For now we keep them constant, as needed for x-y plane
+    Point u_vec(1, 0, 0);
+    Point v_vec(0, 1, 0);
+
+    for (Int_t t = 0; t < ts_size; ++t)
+    {
+      Int_t vi0, vi2;  
+
+      mTTvor->TriangleOtherVertices(t, v, vi0, vi2);
+
+      const Point &v0 = mTTvor->Vertex(vi0);
+      const Point &v2 = mTTvor->Vertex(vi2);
+
+      Point e1; e1.Sub(v0, v1);
+      Point e2; e2.Sub(v2, v1);
+
+      Int_t ii0, ii2; // inner indices of VConns
+      vdata.FindTwoVConnIdcs(vi0, vi2, ii0, ii2);
+
+      VConnData &vcd0 = vdata.fVConns[ii0];
+      if (vcd0.fDistance == 0)
+      {
+        ++vdata.fNeighbourConns;
+        vcd0.fDistance = e1.Magnitude();
+        vcd0.fdU       = u_vec | e1;
+        vcd0.fdV       = v_vec | e1;
+        vcd0.fAngle    = atan2f(vcd0.fdV, vcd0.fdU);
+        if (vcd0.fAngle < 0)
+          vcd0.fAngle += TMath::TwoPi();
+      }
+
+      VConnData &vcd2 = vdata.fVConns[ii2];
+      if (vcd2.fDistance == 0)
+      {
+        ++vdata.fNeighbourConns;
+        vcd2.fDistance = e2.Magnitude();
+        vcd2.fdU       = u_vec | e2;
+        vcd2.fdV       = v_vec | e2;
+        vcd2.fAngle    = atan2f(vcd2.fdV, vcd2.fdU);
+        if (vcd2.fAngle < 0)
+          vcd2.fAngle += TMath::TwoPi();
+      }
+
+      Float_t surf  = 0.25f * (e1 ^ e2).Magnitude();
+      Float_t sprd  = 0.5f  * acosf((e1 | e2) / (vcd0.fDistance * vcd2.fDistance));
+
+      printf("--- v=%d t=%d v0=%d v2=%d, surf=%5.2f, sprd=%5.2f\n",
+             v, t, vi0, vi2, surf, sprd);
+
+      vcd0.fSpread  += sprd;
+      vcd0.fSurface += surf;
+      vcd2.fSpread  += sprd;
+      vcd2.fSurface += surf;
+
+      // Angle or dU,dV could be calculated given to two reference directions.
+      // This depends on the client, really.
+      // External callback?
+      // This angle would also be used as a sorting criteria.
+    }
+
+    // Sort VConns according to fAngle (default operator<()).
+    sort(vdata.fVConns.begin(), vdata.fVConns.end());
+
+    printf("Vertex %2d (%5.2f, %5.2f, %5.2f) neighb-conns=%d, size=%d\n",
+           v, v1.x, v1.y, v1.z,
+           vdata.fNeighbourConns, vdata.fVConns.size());
+    for (Int_t n = 0; n < vdata.fNeighbourConns; ++n)
+    {
+      VConnData& d = vdata.fVConns[n];
+
+      printf("  %d  nv=%2d  d=%5.2f, sp=%5.2f, su=%5.2f, angl=%5.2f du=%5.2f, dv=%5.2f\n",
+             n, d.fVTarget, d.fDistance, d.fSpread, d.fSurface, d.fAngle, d.fdU, d.fdV);
+    }
+
+  }
+
+  delete [] trings_per_vert;
+}
+
+/**************************************************************************/
+/**************************************************************************/
+/**************************************************************************/
+/**************************************************************************/
+
+TriMesh::VConnData& TriMesh::VertexData::FindVConn(Int_t vtarget)
+{
+  vector<VConnData>::iterator i = fVConns.begin();
+  while (i != fVConns.end())
+  {
+    if (i->fVTarget == vtarget) return *i;
+    ++i;
+  }  
+  fVConns.push_back(VConnData(vtarget));
+  return fVConns.back();
+}
+
+void TriMesh::VertexData::FindTwoVConnIdcs(Int_t  v1,  Int_t  v2,
+                                           Int_t& vi1, Int_t& vi2)
+{
+  Int_t idx = 0;
+  vi1 = vi2 = -1;
+  vector<VConnData>::iterator i = fVConns.begin();
+  while (i != fVConns.end())
+  {
+    if (i->fVTarget == v1) vi1 = idx;
+    if (i->fVTarget == v2) vi2 = idx;
+    ++i; ++idx;
+  }
+  if (vi1 == -1) { fVConns.push_back(VConnData(v1)); vi1 = idx++; }
+  if (vi2 == -1) { fVConns.push_back(VConnData(v2)); vi2 = idx++; }
 }
