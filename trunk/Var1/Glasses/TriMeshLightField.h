371cced26078d22296d66ea4ec6cb533c640758b
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2007-02-25 19:13:02 +0000

    Implemented cacluclation of ligt-map (including shadows, of course) for positional and directional light.

--- trunk/Var1/Glasses/TriMeshLightField.h
+++ trunk/Var1/Glasses/TriMeshLightField.h
@@ -9,6 +9,10 @@
 
 #include <Glasses/TriMeshField.h>
 
+#include <TVector3.h>
+
+class ZNode;
+
 class TriMeshLightField : public TriMeshField
 {
   MAC_RNR_FRIENDS(TriMeshLightField);
@@ -17,11 +21,24 @@ private:
   void _init();
 
 protected:
+  Float_t    mAmbiLit;      // X{GS} 7 Value(-range=>[0,1, 1,1000])
+  Float_t    mAmbiShadowed; // X{GS} 7 Value(-range=>[0,1, 1,1000])
+  Float_t    mAmbiBackFace; // X{GS} 7 Value(-range=>[0,1, 1,1000])
+
+  TVector3   mLampPos;      // X{GSR} 7 Vector3()
+  Bool_t     bDirectional;  // X{GS}  7 Bool()
 
 public:
   TriMeshLightField(const Text_t* n="TriMeshLightField", const Text_t* t=0) :
     TriMeshField(n,t) { _init(); }
 
+  void ModulateTvor(Bool_t regen_tring_cols=true);  // X{E} 7 MCWButt()
+  void PartiallyModulateTvor(set<Int_t> vertices, Bool_t regen_tring_cols=true);
+
+  void CalculateLightField(); // X{E} 7 MButt()
+
+  void SetupLampPos(ZNode* lamp, ZNode* mesh);                // X{E} C{2} 7 MCWButt()
+  void SetupLampDir(ZNode* mesh, Float_t theta, Float_t phi); // X{E} C{1} 7 MCWButt()
 
 #include "TriMeshLightField.h7"
   ClassDef(TriMeshLightField, 1) // TriMeshFiled with support for light-map tracing.
