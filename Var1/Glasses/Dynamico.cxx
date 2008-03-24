88b2970d1ef665d7b9abfa0c1343abe472654c19
Matevž Tadel <osschar@users.noreply.github.com>
2008-03-24 19:32:51 +0000

    Added members 'HTransF mLastTrans' and 'Opcode::AABB mLastAABB'. These will: - allow rendering to be done in parallel with collision detection; - provide last position that will be needed for exact collision time determination. Added member function handle_collision() that performs simplified collision handling for a dynamico.

--- Var1/Glasses/Dynamico.cxx
+++ Var1/Glasses/Dynamico.cxx
@@ -22,11 +22,10 @@ ClassImp(Dynamico);
 
 void Dynamico::_init()
 {
-  mTrans.SetPos(0.5, 0.5, 8.0);
-  Float_t oos2 = 1.0/TMath::Sqrt(2);
-  mTrans.SetBaseVec(1,  oos2, oos2, 0);
-  mTrans.SetBaseVec(2,  0, 0, 1);
-  mTrans.SetBaseVecViaCross(3);
+  mLastTransPtr = &mLastTrans;
+  mLastAABBPtr  = &mLastAABB;
+
+  bParked = false;
 
   mV = mW = 0.0f;
   mLevH   = 0.1f;
@@ -38,5 +37,81 @@ void Dynamico::_init()
 
 /**************************************************************************/
 
+bool Dynamico::handle_collision(Opcode::RayCollider    & ray_collider,
+                                Opcode::Ray            & ray,
+                                Opcode::Point          & com,
+                                CollisionSegments      & segments,
+                                Extendio               * extendio)
+{
+  // Handle collision with a static object extendio.
+  //
+  // If com is inside extendio, let it go, we're too late (or it
+  // was inside to start with).
+  // If collision-vertex is in front of com bounce off with change of
+  // direction, otherwise just move away from collision-vertex.
+  //
+  // Input:
+  //   com      center-of-mass of this dynamico
+  // Return:    false if ray collision fails.
+  //
+  // !!!!! For the moment this is also used for dyno-dyno collision.
+  // When fixed, this should take Statico* and the new method Dyno*.
+
+  static const Exc_t _eh("Dynamico::handle_collision ");
+
+  if (ray_collider.Collide(ray, *extendio->get_opc_model(), extendio->RefTrans()))
+  {
+    using namespace Opcode;
+
+    Opcode::CollisionFaces& CF = * ray_collider.GetDestination();
+
+    if (bVerboseCollide)
+      printf("%sCollide shooting ray outwards at the hit mesh from COM, ni=%d.\n",
+             _eh.Data(), CF.GetNbFaces());
+
+    // Do fixing only when nfaces is even, i.e., dyno's com is outside.
+    if (CF.GetNbFaces() % 2 == 0)
+    {
+      // center-vertex of collision segments.
+      const Point& cv = segments.RefCenter();
+
+      Point com2cv(cv); com2cv -= com;
+
+      Point& fwd = * (Point*) mTrans.PtrBaseVec(1);
+
+      if ((fwd | com2cv) * mV > 0 && segments.size() > 1)
+      {
+        // CV is in front - turn to be parallel with collision plane
+        // or a bit further as plane normal is not projected into xy
+        // plane of dyno.
+        // If segments.size() == 1 => plane is not defined (happens rarely).
+
+        Plane P(segments[0].mP0, segments[0].mP1, cv);
+        // turn to be parallel with collision plane or a bit further
+        // as plane normal is not projected into xy plane of dyno.
+        Float_t dp = fwd | P.n;
+        if (dp < 0) P.n.Neg();
+        Point cross = P.n ^ fwd;
+        Float_t  cp = 0.5f * cross.Magnitude();
+        if ((cross | *(Point*)mTrans.PtrBaseVec(3)) < 0)
+          mTrans.RotateLF(2, 1, TMath::PiOver2() - TMath::ASin(cp));
+        else
+          mTrans.RotateLF(1, 2, TMath::PiOver2() - TMath::ASin(cp));
+      }
+      else
+      {
+        // CV is behind - move away from it.
+        // !!! 0.1 should be replaced with a ray from cv back towards com.
+        com2cv *= -0.1f;
+        mTrans.Move3PF(com2cv.x, com2cv.y, com2cv.z);
+      }
+    }
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
 /**************************************************************************/
--- Var1/Glasses/Dynamico.h
+++ Var1/Glasses/Dynamico.h
@@ -21,9 +21,22 @@ private:
   void _init();
 
 protected:
+  HTransF         mLastTrans;  // Last transform from current master.
+  Opcode::AABB    mLastAABB;   // Last enclosing bbox in parent frame.
+
+  Bool_t          bParked;     // X{GS}  7 Bool()
+
   Float_t         mV;          // X{GS}  7 Value(-range=>[-100,100, 1,1000], -join=>1)
   Float_t         mW;          // X{GS}  7 Value(-range=>[  -5,  5, 1,1000])
   Float_t         mLevH;       // X{GS}  7 Value(-range=>[   0,200, 1,1000])
+
+  // Float_t         mPowPerM;    // X{GS}  7 Value(-range=>[-10,10, 1,1000])
+  // Force per mass, perchance?
+  // No ... force ... mass is eventually variable.
+  // Then need "engine type"; min/max F, max dF/dt
+
+  //HPointF         mVVec;       // X{RS}  7 HPointF(-const=>1)
+  //HPointF         mWVec;       // X{RS}  7 HPointF(-const=>1)
 
   UChar_t         mMoveMode;   // X{GS}  7 PhonyEnum(-type=>MoveMode_e)
 
@@ -33,9 +46,16 @@ public:
   Dynamico(const Text_t* n="Dynamico", const Text_t* t=0) :
     Extendio(n,t) { _init(); }
 
+  virtual void update_last_data() { mLastTrans = mTrans; mLastAABB = mLastAABB; }
+
+  bool handle_collision(Opcode::RayCollider    & ray_collider,
+                        Opcode::Ray            & ray,
+                        Opcode::Point          & com,
+                        CollisionSegments      & segments,
+                        Extendio               * extendio);
+
 #include "Dynamico.h7"
   ClassDef(Dynamico, 1);
 }; // endclass Dynamico
-
 
 #endif
