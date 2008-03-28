5fd2ce4a1dcc0b7f06abb37e8ab09f1074fe2341
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2008-03-28 09:53:06 +0000

    Extended image-rescale-to-pow2 for ATI to GL version 2.1.7412.

--- trunk/Geom1/Rnr/GL/ZImage_GL_Rnr.cxx
+++ trunk/Geom1/Rnr/GL/ZImage_GL_Rnr.cxx
@@ -49,7 +49,7 @@ void  ZImage_GL_Rnr::check_rescale()
       sRescaleToPow2 = 1;
     }
     else if (vendor.BeginsWith("ATI")) {
-      if (major <= 2 && minor <= 1 && patch <= 7276) {
+      if (major <= 2 && minor <= 1 && patch <= 7412) {
         ISmess(mh + "ATI, GL <= 2.1.7276 (very slow rendering).");
         sRescaleToPow2 = 1;
       }
