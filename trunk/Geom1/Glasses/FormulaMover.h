9b5cf16de852b6db94c2c97de5d92f6cc023f77e
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2008-04-21 20:08:14 +0000

    New files - in fact old files with classes I never really got to do properly.

---
+++ trunk/Geom1/Glasses/FormulaMover.h
@@ -0,0 +1,55 @@
+// $Header$
+
+// Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef Geom1_FormulaMover_H
+#define Geom1_FormulaMover_H
+
+#include <Glasses/Operator.h>
+#include <Stones/TimeMakerClient.h>
+
+class TFormula;
+
+class FormulaMover : public Operator, public TimeMakerClient
+{
+public:
+  enum Operation_e { O_Off, O_Set, O_Add };
+
+private:
+  void _init();
+
+protected:
+  Operation_e   mPosMode; // X{GS} Ray{PosChanged} 7 PhonyEnum()
+  TString       mX;       // X{GS} Ray{PosChanged} 7 Textor()
+  TString       mY;       // X{GS} Ray{PosChanged} 7 Textor()
+  TString       mZ;       // X{GS} Ray{PosChanged} 7 Textor()
+  TFormula*     mForX;    // X{GS} Ray{PosChanged}
+  TFormula*     mForY;    // X{GS} Ray{PosChanged}
+  TFormula*     mForZ;    // X{GS} Ray{PosChanged}
+
+  Operation_e   mRotMode; // X{GS} Ray{RotChanged} 7 PhonyEnum()
+  TString       mPhi;     // X{GS} Ray{RotChanged} 7 Textor()
+  TString       mTheta;   // X{GS} Ray{RotChanged} 7 Textor()
+  TString       mEta;     // X{GS} Ray{RotChanged} 7 Textor()
+  TFormula*     mForPhi;  // X{GS} Ray{RotChanged}
+  TFormula*     mForTheta;// X{GS}
+  TFormula*     mForEta;  // X{GS}
+
+public:
+  FormulaMover(const Text_t* n="FormulaMover", const Text_t* t=0) :
+    Operator(n,t) { _init(); }
+
+  void EmitPosChangedRay();
+  void EmitRotChangedRay();
+
+  // TimeMakerClient
+  virtual void TimeTick(Double_t t, Double_t dt);
+
+#include "FormulaMover.h7"
+  ClassDef(FormulaMover, 1);
+}; // endclass FormulaMover
+
+
+#endif
---
+++ trunk/Geom1/Glasses/LocalMover.h
@@ -0,0 +1,46 @@
+// $Header$
+
+// Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef Geom1_LocalMover_H
+#define Geom1_LocalMover_H
+
+#include <Glasses/Operator.h>
+
+class ZTrans;
+
+class LocalMover : public Operator
+{
+  MAC_RNR_FRIENDS(LocalMover);
+
+private:
+  void _init();
+
+protected:
+  Bool_t   bMoveOn;   // X{GS} 7 Bool(-join=>1)
+  Bool_t   bMoveInPF; // X{GS} 7 Bool()
+  Double_t mDx;       // X{GS} 7 Value(-join=>1)
+  Double_t mDy;       // X{GS} 7 Value(-join=>1)
+  Double_t mDz;       // X{GS} 7 Value()
+
+  Bool_t   bRotOn;    // X{GS} 7 Bool(-join=>1)
+  Bool_t   bRotInPF;  // X{GS} 7 Bool()
+  Double_t mPhi;      // X{GS} 7 Value(-join=>1)
+  Double_t mTheta;    // X{GS} 7 Value(-join=>1)
+  Double_t mEta;      // X{GS} 7 Value()
+
+  ZTrans*  mRotMatrix; //!
+
+public:
+  LocalMover(const Text_t* n="LocalMover", const Text_t* t=0) :
+    Operator(n,t) { _init(); }
+
+
+#include "LocalMover.h7"
+  ClassDef(LocalMover, 1);
+}; // endclass LocalMover
+
+
+#endif
