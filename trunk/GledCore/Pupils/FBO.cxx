29763e37c8675ed54f0936253fbb70ee1a31a67e
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2008-07-20 18:33:27 +0000

    Added flag that enforces allocation of framebuffer with power-2 dimensions. Helps on ATI when the buffer is used for texturing.

--- trunk/GledCore/Pupils/FBO.cxx
+++ trunk/GledCore/Pupils/FBO.cxx
@@ -6,6 +6,8 @@
 
 #include "FBO.h"
 
+#include <TMath.h>
+
 #include <GL/gl.h>
 #include <GL/glext.h>
 
@@ -14,6 +16,8 @@
 //
 // Cut'n'pasted from examples in:
 //   http://www.opengl.org/registry/specs/EXT/framebuffer_object.txt
+
+Bool_t FBO::sRescaleToPow2 = true; // For ATI.
 
 FBO::FBO() :
   fFrameBuffer  (0),
@@ -31,6 +35,21 @@ FBO::~FBO()
 void FBO::init(int w, int h)
 {
   static const Exc_t _eh("FBO::init ");
+
+  fIsRescaled = false;
+  if (sRescaleToPow2)
+  {
+    Int_t nw = 1 << TMath::CeilNint(TMath::Log2(w));
+    Int_t nh = 1 << TMath::CeilNint(TMath::Log2(h));
+    if (nw != w || nh != h)
+    {
+      fWScale = ((Float_t)w) / nw;
+      fHScale = ((Float_t)h) / nh;
+      printf("w,h=%d,%d; nw,nh=%d,%d; wscl,hscl=%f,%f\n", w,h,nw,nh,fWScale,fHScale);
+      w = nw; h = nh;
+      fIsRescaled = true;
+    }
+  }
 
   if (fFrameBuffer != 0)
   {
@@ -127,10 +146,21 @@ void FBO::unbind()
 
 void FBO::bind_texture()
 {
+  glPushAttrib(GL_TEXTURE_BIT);
   glBindTexture(GL_TEXTURE_2D, fColorTexture);
+  glEnable(GL_TEXTURE_2D);
+
+  glMatrixMode(GL_TEXTURE);
+  glPushMatrix();
+  glScalef(fWScale, fHScale, 1);
+  glMatrixMode(GL_MODELVIEW);
 }
 
 void FBO::unbind_texture()
 {
-  glBindTexture(GL_TEXTURE_2D, 0);
+  glMatrixMode(GL_TEXTURE);
+  glPopMatrix();
+  glMatrixMode(GL_MODELVIEW);
+
+  glPopAttrib();
 }
