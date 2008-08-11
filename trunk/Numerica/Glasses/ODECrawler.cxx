d6abc9bd100b0c2552a6741fc68c34e91ee72f2c
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2008-08-11 18:01:51 +0000

    Add new class ODEStorage with specializations for float and double. This allows external management of arrays for trajectory storage. Remove inheritance from Eventor. Use detached thread execution instead.

--- trunk/Numerica/Glasses/ODECrawler.cxx
+++ trunk/Numerica/Glasses/ODECrawler.cxx
@@ -1,49 +1,131 @@
 // $Header$
 
-//________________________________________________________________________
-// ODECrawler
-//
-// ODE integrator built upon adaptive step 4th order runge-kutta.
-// Based on odeint from numerical recipes.
-// X corresponds to time (scalar).
-// Y corresponds to state vector at a given time.
-// Works in double precision.
-//
-// No good way for permanent storage or broadcasting of results yet.
-// Trajectory is stored in mXStored, mYstored;
-// Final state can be obtained from mY.
-//
-// Example glass Moonraker. Macro moonraker.C.
-//________________________________________________________________________
-
 #include "ODECrawler.h"
 #include "ODECrawler.c7"
 
 #include <TMath.h>
 
-ClassImp(ODECrawlerMaster)
-ClassImp(ODECrawler)
+
+//==============================================================================
+// ODECrawlerMaster
+//==============================================================================
+
+//______________________________________________________________________________
+//
+// Abstract base class for glass that provides:
+// a) ODE order
+// b) initial conditions
+// c) derivatives
+
+ClassImp(ODECrawlerMaster);
+
+
+//==============================================================================
+// ODECrawler
+//==============================================================================
+
+//______________________________________________________________________________
+//
+// ODE integrator built upon adaptive step 4th order runge-kutta.
+// Based on odeint from numerical recipes.
+// X corresponds to time (scalar).
+// Y corresponds to state vector at a given time.
+// Final state can be obtained from mY.
+// Works in double precision.
+//
+// No good way for permanent storage or broadcasting of results yet.
+//
+// Trajectory is stored in ODEstorage object with at least mStoreDx
+// between consequtive points.
+// Maximum of mStoreMax intermediate points are stored.
+// If mStoreMax = 0 no results are stored.
+// If mStoreMax < 0 all points are stored (default).
+//
+// Example glass Moonraker. Macro moonraker.C.
+//________________________________________________________________________
+
+ClassImp(ODECrawler);
 
 void ODECrawler::_init()
 {
   hTINY = 1e-30; hSAFETY = 0.9; hPGROW = -0.2; hPSHRNK = -0.25; hERRCON = 1.89e-4;
+  hTrueMaster = 0;
+
   mGuessesOK = mGuessesBad = mStored = 0;
-  mMaxSteps = 1000000; mStoreDx = 0.001; mStoreMax = 1000;
-  mXStored = mYStored = 0;
+  mMaxSteps  = 1000000; mStoreDx = 0.001; mStoreMax = -1;
+  mStorage   = new ODEStorageD(0);
+  mXStored   = mYStored = 0;
   mAcc = 1e-8; mH1 = 1e-2; mHmin = 1e-18;
   mX1 = 0; mX2 = 0;
-
-  bContinuous = false; // Single cycle
-  bMultix     = true;  // No reasonable storage / transfer mechanism.
 }
 
 ODECrawler::~ODECrawler()
 {
-  delete mXStored;
-  delete [] mYStored;
+  delete mStorage;
 }
 
 /**************************************************************************/
+
+void ODECrawler::AdvanceXLimits()
+{
+  // Advance x limits forward, so that X1 becomes X2 and integration
+  // interval length is preserved.
+
+  Double_t d = mX2 - mX1;
+  mX1  = mX2;
+  mX2 += d;
+}
+
+void ODECrawler::SetStorage(ODEStorage* s)
+{
+  delete mStorage;
+  mStorage = s;
+}
+
+void ODECrawler::DetachStorage()
+{
+  mStorage = 0;
+}
+
+ODEStorage* ODECrawler::SwapStorage(ODEStorage* s)
+{
+  ODEStorage* old = s;
+  mStorage = s;
+  return old;
+}
+
+/**************************************************************************/
+
+void ODECrawler::init_integration(Bool_t call_ode_start)
+{
+  // Initialize storage arrays, clear integration state.
+  // If call_ode_start is true, call ODEStart() in master to get
+  // initial parameters.
+  // hTrueMaster must be set.
+
+  mN = hTrueMaster->ODEOrder();
+  mY.ResizeTo(mN);
+  if (mStoreMax != 0)
+  {
+    if (mStorage == 0)
+    {
+      mStorage = new ODEStorageD(mN);
+    }
+    else if (mStorage->Order() != mN)
+    {
+      mStorage->ResetOrder(mN);
+    }
+    else
+    {
+      mStorage->Reset();
+    }
+  }
+  
+  mGuessesOK = mGuessesBad = mStored = 0;
+
+  if (call_ode_start)
+    hTrueMaster->ODEStart(mY, mX1, mX2);
+}
 
 Int_t
 ODECrawler::Rkqs(TVectorD& y, TVectorD& dydx, Double_t& x, Double_t htry,
@@ -53,23 +135,27 @@ ODECrawler::Rkqs(TVectorD& y, TVectorD& dydx, Double_t& x, Double_t htry,
 
   TVectorD yerr(mN), ytemp(mN);
   Double_t h=htry;
-  while(1) {
+  while (true)
+  {
     Rkck(y, dydx, x, h, ytemp, yerr);
     errmax = 0.0;
-    for(Int_t i=0; i<mN; i++) errmax = TMath::Max(errmax,TMath::Abs(yerr(i)/yscal(i)));
+    for (Int_t i=0; i<mN; ++i)
+      errmax = TMath::Max(errmax,TMath::Abs(yerr(i)/yscal(i)));
     errmax /= mAcc;
-    if(errmax <= 1.0) break;
+    if (errmax <= 1.0) break;
     htemp = hSAFETY*h*TMath::Power(errmax,hPSHRNK);
     h = (h >= 0.0 ? TMath::Max(htemp,0.1*h) : TMath::Min(htemp,0.1*h));
-    ISdebug(6, GForm("ODECrawler::Rkqs h=%f; errmax=%f", h, errmax));
     xnew = x + h;
-    if(xnew == x) {
+    if (xnew == x)
+    {
       ISerr(GForm("ODECrawler::Rkqs stepsize underflow, errmax=%f", errmax));
       return 1;
     }
   }
-  if(errmax > hERRCON) hnext = hSAFETY*h*TMath::Power(errmax,hPGROW);
-  else hnext=5.0*h;
+  if (errmax > hERRCON)
+    hnext = hSAFETY*h*TMath::Power(errmax,hPGROW);
+  else
+    hnext = 5.0*h;
   x += (hdid=h);
   y = ytemp;
   return 0;
@@ -77,46 +163,44 @@ ODECrawler::Rkqs(TVectorD& y, TVectorD& dydx, Double_t& x, Double_t htry,
 
 /**************************************************************************/
 
-void
-ODECrawler::Rkck(TVectorD& y, TVectorD& dydx, Double_t x, Double_t h,
-		 TVectorD& yout, TVectorD& yerr)
+void ODECrawler::Rkck(TVectorD& y, TVectorD& dydx, Double_t x, Double_t h,
+                      TVectorD& yout, TVectorD& yerr)
 {
-  static Double_t a2=0.2,a3=0.3,a4=0.6,a5=1.0,a6=0.875,b21=0.2,
-    b31=3.0/40.0,b32=9.0/40.0,b41=0.3,b42 = -0.9,b43=1.2,
-    b51 = -11.0/54.0, b52=2.5,b53 = -70.0/27.0,b54=35.0/27.0,
-    b61=1631.0/55296.0,b62=175.0/512.0,b63=575.0/13824.0,
-    b64=44275.0/110592.0,b65=253.0/4096.0,c1=37.0/378.0,
-    c3=250.0/621.0,c4=125.0/594.0,c6=512.0/1771.0,
-    dc5 = -277.00/14336.0;
-  Double_t dc1=c1-2825.0/27648.0,dc3=c3-18575.0/48384.0,
-    dc4=c4-13525.0/55296.0,dc6=c6-0.25;
+  static const Double_t a2=0.2, a3=0.3, a4=0.6, a5=1.0, a6=0.875,
+    b21=0.2, b31=3.0/40.0, b32=9.0/40.0, b41=0.3, b42=-0.9, b43=1.2,
+    b51=-11.0/54.0, b52=2.5,b53=-70.0/27.0, b54=35.0/27.0,
+    b61=1631.0/55296.0, b62=175.0/512.0, b63=575.0/13824.0,
+    b64=44275.0/110592.0, b65=253.0/4096.0, c1=37.0/378.0,
+    c3=250.0/621.0, c4=125.0/594.0, c6=512.0/1771.0, dc5=-277.00/14336.0;
+  static const Double_t dc1=c1-2825.0/27648.0, dc3=c3-18575.0/48384.0,
+    dc4=c4-13525.0/55296.0, dc6=c6-0.25;
 
   TVectorD ak2(mN), ak3(mN), ak4(mN), ak5(mN), ak6(mN), ytemp(mN);
 
-  for(Int_t i=0; i<mN; i++)
+  for (Int_t i=0; i<mN; ++i)
     ytemp(i) = y(i) + b21*h*dydx(i);
   hTrueMaster->ODEDerivatives(x+a2*h,ytemp,ak2);
-  for(Int_t i=0; i<mN; i++)
+  for (Int_t i=0; i<mN; ++i)
     ytemp(i)=y(i)+h*(b31*dydx(i)+b32*ak2(i));
   hTrueMaster->ODEDerivatives(x+a3*h,ytemp,ak3);
-  for(Int_t i=0; i<mN; i++)
+  for (Int_t i=0; i<mN; ++i)
     ytemp(i)=y(i)+h*(b41*dydx(i)+b42*ak2(i)+b43*ak3(i));
   hTrueMaster->ODEDerivatives(x+a4*h,ytemp,ak4);
-  for(Int_t i=0; i<mN; i++)
+  for (Int_t i=0; i<mN; ++i)
     ytemp(i)=y(i)+h*(b51*dydx(i)+b52*ak2(i)+b53*ak3(i)+b54*ak4(i));
   hTrueMaster->ODEDerivatives(x+a5*h,ytemp,ak5);
-  for(Int_t i=0; i<mN; i++)
+  for (Int_t i=0; i<mN; ++i)
     ytemp(i)=y(i)+h*(b61*dydx(i)+b62*ak2(i)+b63*ak3(i)+b64*ak4(i)+b65*ak5(i));
   hTrueMaster->ODEDerivatives(x+a6*h,ytemp,ak6);
-  for(Int_t i=0; i<mN; i++)
+  for (Int_t i=0; i<mN; ++i)
     yout(i)=y(i)+h*(c1*dydx(i)+c3*ak3(i)+c4*ak4(i)+c6*ak6(i));
-  for(Int_t i=0; i<mN; i++)
+  for (Int_t i=0; i<mN; ++i)
     yerr(i)=h*(dc1*dydx(i)+dc3*ak3(i)+dc4*ak4(i)+dc5*ak5(i)+dc6*ak6(i));
 }
 
 /**************************************************************************/
 
-void ODECrawler::Crawl()
+void ODECrawler::Integrate()
 {
   // Integrate ODE with derivatives provided by mODEMaster from X1 to X2.
 
@@ -126,46 +210,46 @@ void ODECrawler::Crawl()
   h = TMath::Sign(mH1, mX2-mX1);
   TVectorD y(mY);
   TVectorD yscal(mN), dydx(mN);
-  if (mStoreMax > 0) xsav = x - 2*mStoreDx;
-  for(Int_t nstp=0; nstp<mMaxSteps; nstp++) {
+  if (mStoreMax != 0) xsav = x - 2*mStoreDx;
+  for (Int_t nstp=0; nstp<mMaxSteps; ++nstp)
+  {
     hTrueMaster->ODEDerivatives(x, y, dydx);
-    ISdebug(6, GForm("ODECrawl Pre Rkqs at x=%f", x));
-    for(Int_t i=0; i<mN; i++) {
-      yscal(i)=TMath::Abs(y(i)) + TMath::Abs(dydx(i)*h) + hTINY;
-      ISdebug(6, GForm(" %6d %f %f", i, y(i), dydx(i)));
+    for (Int_t i=0; i<mN; ++i)
+    {
+      yscal(i) = TMath::Abs(y(i)) + TMath::Abs(dydx(i)*h) + hTINY;
     }
 
-    if(mStoreMax && mStored < mStoreMax-1 && TMath::Abs(x-xsav) > TMath::Abs(mStoreDx)) {
-      (*mXStored)(mStored) = x;
-      mYStored[mStored].ResizeTo(mN);
-      mYStored[mStored] = y;
-      mStored++;
+    if (mStoreMax < 0 ||
+        (mStoreMax > 0 && mStored < mStoreMax - 1 &&
+         TMath::Abs(x - xsav) > TMath::Abs(mStoreDx)))
+    {
+      mStorage->AddEntry(x, y.GetMatrixArray());
+      ++mStored;
       xsav = x;
     }
 
-    if ((x+h-mX2)*(x+h-mX1) > 0.0) h=mX2-x;
+    if ((x + h - mX2)*(x + h - mX1) > 0.0) h = mX2 - x;
 
-    if(Rkqs(y, dydx, x, h, yscal, hdid, hnext)) return;
+    if (Rkqs(y, dydx, x, h, yscal, hdid, hnext)) return;
 
-    ISdebug(6, GForm("ODECrawl Post Rkqs at x=%f hdid=%f hnext=%f",
-		     x, hdid, hnext));
-    for(Int_t i=0; i<mN; i++) {
-      ISdebug(6, GForm("  %6d %f %f", i, y(i), dydx(i)));
-    }
+    if (hdid == h)
+      ++mGuessesOK;
+    else
+      ++mGuessesBad;
 
-    if(hdid == h) mGuessesOK++; else mGuessesBad++;
-    if((x-mX2)*(mX2-mX1) >= 0.0) {
+    if ((x - mX2)*(mX2 - mX1) >= 0.0)
+    {
       mY = y;
-      if(mStoreMax) {
-	(*mXStored)(mStored) = x;
-	mYStored[mStored].ResizeTo(mN);
-	mYStored[mStored] = y;
-	mStored++;
+      if (mStoreMax != 0)
+      {
+        mStorage->AddEntry(x, y.GetMatrixArray());
+	++mStored;
       }
       return;
     }
-    if (TMath::Abs(hnext) <= mHmin) {
-      ISerr(GForm("ODECrawler::Crawl Step size too small, hnext=%g", hnext));
+    if (TMath::Abs(hnext) <= mHmin)
+    {
+      ISerr(GForm("ODECrawler::Integrate Step size too small, hnext=%g", hnext));
       return;
     }
     h = hnext;
@@ -175,36 +259,24 @@ void ODECrawler::Crawl()
 
 /**************************************************************************/
 
-Operator::Arg* ODECrawler::PreDance(Operator::Arg* op_arg)
+void ODECrawler::Crawl(Bool_t call_ode_start)
 {
-  op_arg = Eventor::PreDance(op_arg);
+  // Integrate with given master / parameters.
+  // If called via a MIR it is executed in a broadcasted detached thread.
+  //
+  // The object is locked for the duration of the execution.
 
+  static const Exc_t _eh("ODECrawler::Crawl ");
+
+  assert_odemaster(_eh);
   hTrueMaster = dynamic_cast<ODECrawlerMaster*>(*mODEMaster);
-  if(!hTrueMaster) {
-    ISerr("ODECrawler::PreDance master not of an ODECrawlerMaster");
-    delete op_arg;
-    return 0;
-  }
-  mN = hTrueMaster->ODEOrder();
-  if(mY.GetNoElements() != mN) mY.ResizeTo(mN);
-  if(!mXStored) {
-    mXStored = new TVectorF(mStoreMax);
-    mYStored = new TVectorF[mStoreMax];
-  } else if(mXStored->GetNoElements() != mStoreMax) {
-    mXStored->ResizeTo(mStoreMax);
-    delete [] mYStored; mYStored = new TVectorF[mStoreMax];
-  } else {
-    mXStored->Zero();
-  }
-  mGuessesOK = mGuessesBad = mStored = 0;
-  hTrueMaster->ODEStart(mY, mX1, mX2);
+  if (!hTrueMaster)
+    throw(_eh + "master not an ODECrawlerMaster.");
 
-  return op_arg;
-}
+  GLensReadHolder rdlock(this);
 
-void
-ODECrawler::Operate(Operator::Arg* op_arg) throw(Operator::Exception) {
-  Eventor::PreOperate(op_arg);
-  Crawl();
-  Eventor::PostOperate(op_arg);
+  init_integration(call_ode_start);
+  Integrate();
+
+  Stamp(FID());
 }
