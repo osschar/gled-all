d6a54d5bcd1053687b1911d6002ed24957e7beee
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2008-09-23 19:26:26 +0000

    Add cosmic ball nest layout. Start integrator thread for the solar-system.

--- trunk/demos/Var1/solar_system.C
+++ trunk/demos/Var1/solar_system.C
@@ -14,6 +14,9 @@ Eventor      *eventor  = 0;
 TimeMaker    *tmaker   = 0;
 ScreenDumper *dumper   = 0;
 
+const Text_t* ball_layout = "ZGlass(Name,Title):CosmicBall(M[8]):"
+  "Sphere(Radius, LOD, Color)";
+
 void solar_system()
 {
   gRandom = new TRandom3(0); // Seed 0 means seed with TUUID.
@@ -29,14 +32,16 @@ void solar_system()
   CREATE_ADD_GLASS(lamp, Lamp, g_scene, "Lamp", "");
   lamp->SetPos(0, 0, 0);
   lamp->SetDiffuse(1, 1, 0.6);
-  // lamp->SetDrawLamp(false);
+  lamp->SetDrawLamp(false);
   lamp->SetLampScale(1);
   // This crashes my laptop together with glDrawArrays of tringtvor ... badly.
-  // Must be a weird ATI bug.
+  // Must be a weird ATI bug. 2007
   g_scene->GetGlobLamps()->Add(lamp);
 
   ASSIGN_ADD_GLASS(ss, SolarSystem, g_scene, "SolarSystem", "");
   ss->SetColor(0.8, 0.8, 0.8, 0.8);
+  ss->SetMaxTheta(10);
+  ss->SetMaxEcc(0.00);
   ode = ss->GetODECrawler();
 
   ss->MakeStar();
@@ -55,14 +60,8 @@ void solar_system()
   g_pupil->SetNearClip(1);
   g_pupil->SetFarClip(5000);
   g_pupil->SetMoveOM(-1);
-  g_pupil->SetWidth(1024);
-  g_pupil->SetHeight(768);
-
-  ode->SetStoreDx(10);
-  ode->SetX1(0); ode->SetX2(100);
-  ode->SetAcc(1e-8);
-  ode->SetH1(1); ode->SetHmin(1e-3);
-  ode->Crawl();
+  // g_pupil->SetWidth(1024);
+  // g_pupil->SetHeight(768);
 
   ASSIGN_ADD_GLASS(eventor, Eventor, g_queen, "Eventor", 0);
   eventor->SetInterBeatMS(20);
@@ -76,6 +75,20 @@ void solar_system()
   ASSIGN_ADD_GLASS(dumper, ScreenDumper, eventor, "ScreenDumper", 0);
   dumper->SetWaitSignal(true);
   dumper->SetPupil(g_pupil);
+
+  {
+    ZList* layouts = g_queen->AssertPath("var/layouts", "ZNameMap");
+    fill_GledCore_layouts(layouts);
+    layouts->Swallow("Var1", new ZGlass("CosmicBall", ball_layout));
+
+    g_nest->SetLayoutList(layouts);
+  }
+
+  ode->SetStoreDx(10);
+  ode->SetAcc(1e-8);
+  ode->SetH1(1); ode->SetHmin(1e-8);
+
+  ss->StartIntegratorThread();
 
   eventor->Start();
 }
