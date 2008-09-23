09ee02e8618fef1aa0e8381299ebd95d8e8d4507
Matevž Tadel <osschar@users.noreply.github.com>
2008-09-23 19:24:53 +0000

    Put ODE integration in a dedicated thread with increased nice. Do management of storage chunks with given time interval for the future and past.

--- Var1/Glasses/SolarSystem.h
+++ Var1/Glasses/SolarSystem.h
@@ -23,14 +23,18 @@ class SolarSystem : public ZNode,
   MAC_RNR_FRIENDS(SolarSystem);
 
 private:
+  Bool_t             hWarnTimeOutOfRange; //!
+
   void _init();
 
 protected:
   ZLink<ZVector>     mBalls;      // X{GS} L{A} RnrBits{0,0,0,0, 0,0,0,5}
   ZLink<ODECrawler>  mODECrawler; // X{GS} L{A}
 
-  Double_t           mTime;       // X{G}    7 Value(-range=>[1,10000, 1,100])
-  Double_t           mTimeFac;    // X{GS}   7 Value(-range=>[0,1000,  1,10])
+  Double_t           mTime;       // X{G}    7 Value(-range=>[0,1e9,  1,100])
+  Double_t           mTimeFac;    // X{GS}   7 Value(-range=>[0,1000, 1,10])
+
+  Int_t              mBallHistorySize; // X{GE} 7 Value(-range=>[0,4096, 1])
 
   ZColor             mColor;      // X{PRGS} 7 ColorButt()
   Double_t           mStarMass;   // X{GS}   7 Value()
@@ -46,23 +50,40 @@ protected:
 
   class Storage : public ODEStorageD
   {
-    GCondition    mCond;
+    Int_t mId;
+
   public:
 
     Storage(Int_t order, Int_t capacity=128) :
-      ODEStorageD(order, capacity)
+      ODEStorageD(order, capacity), mId(0)
     {}
 
-    void ResetFrom(const Storage& s)
-    { ResetOrder(s.Order(), 12*s.Size()/10); }
+    Storage(const Storage& s) : ODEStorageD(s.Order(), 12*s.Size()/10)
+    { mId = s.mId + 1; }
+
+    Int_t GetId() const { return mId; }
   };
 
-  GThread        *mIntegratorThread; //!
-  GMutex          mStorageLock;      //!
+  typedef map<Double_t, Storage*>           mTime2pStorage_t;
+  typedef map<Double_t, Storage*>::iterator mTime2pStorage_i;
 
-  map<Double_t, ODEStorageD*> mStorageMap; //!
+  mTime2pStorage_t   mStorageMap;     //!
+  mTime2pStorage_i   mCurrentStorage; //!
+
+  Double_t           mTimePerChunk;   // X{GS} 7 Value(-range=>[100,1e4, 1,100])
+  Double_t           mKeepPast;       // X{GS} 7 Value(-range=>[100,1e5, 1])
+  Double_t           mCalcFuture;     // X{GS} 7 Value(-range=>[100,1e5, 1])
 
   ODEStorageD* get_storage() { return (ODEStorageD*) mODECrawler->GetStorage(); }
+
+  GThread           *mIntegratorThread; //!
+  GCondition         mStorageCond;      //!
+
+  Storage* find_storage_from_time(Double_t t);
+  Double_t set_time(Double_t t, Bool_t from_timetick);
+
+  static void* tl_IntegratorThread(SolarSystem* ss);
+  void         IntegratorThread();
 
 public:
   SolarSystem(const Text_t* n="SolarSystem", const Text_t* t=0) :
@@ -70,6 +91,8 @@ public:
   virtual ~SolarSystem();
 
   virtual void AdEnlightenment();
+  // ?? Do i need this one?
+  // virtual void AdUnfoldment();    // called by Queen after comet unpacking
 
   // ODE virtuals
   virtual UInt_t ODEOrder();
@@ -80,11 +103,13 @@ public:
   virtual void TimeTick(Double_t t, Double_t dt);
 
   // Propagators. Not finished.
-  void StartStorageManager(); // X{ED}
-  void StopStorageManager();  // X{E}
+  void StartIntegratorThread(); // X{E} 7 MCWButt()
+  void StopIntegratorThread();  // X{E} 7 MCWButt()
 
   // Visualization
-  virtual void   SetTime(Double_t t); // X{E} 7 MCWButt()
+  void SetTime(Double_t t); // X{E} 7 MCWButt()
+
+  void SetBallHistorySize(Int_t history_size);
 
   // Builders
   virtual void   MakeStar();      // X{E} 7 MCWButt()
