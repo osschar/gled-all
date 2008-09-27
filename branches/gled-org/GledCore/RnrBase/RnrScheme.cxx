a7f1d12322c08da81d88a0c72d1dbd5a84836c5a
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2008-09-27 11:39:55 +0000

    Removing CVS related directories and tags.

--- branches/gled-org/GledCore/RnrBase/RnrScheme.cxx
+++
@@ -1,7 +0,0 @@
-// $Header$
-
-// Copyright (C) 1999-2003, Matevz Tadel. All rights reserved.
-// This file is part of GLED, released under GNU General Public License version 2.
-// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
-
-#include "RnrScheme.h"
--- branches/gled-org/TADemo/Rnr/GL/TA_TextureContainer.h
+++
@@ -1,5 +0,0 @@
-#ifndef TADemo_TA_TextureContainer_GLDefs_H
-#define TADemo_TA_TextureContainer_GLDefs_H
-
-
-#endif
