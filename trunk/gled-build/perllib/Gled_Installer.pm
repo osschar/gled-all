f170a3475658977b85d7dfa272b7dc44519cc7e8
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2008-12-23 14:02:18 +0000

    * bin/gled-config.in: 	New file - template for gled-config executable.
    	* libexec/gled_symlink.pl:
    	Removed - use 'gled_install.pl -s'.
    
    	* libexec/gled_install.pl:
    	* libexec/gled_uninstall.pl:
    	Removed - superseded by files/dirs variants.
    
    	* libexec/gled_install_dirs.pl:
    	* libexec/gled_uninstall_dirs.pl:
    	* libexec/gled_install_files.pl:
    	* libexec/gled_uninstall_files.pl:
    	New versions of install/unistall scripts.
    
    	* Makefile:
    	Add placeholders for top-level blob_install and install targets.
    
    	* configure:
    	Initialize sub-prefix vars if prefix is set and they are not.
    	Write out bin/gled-config.
    	Dump prefix/blob stuff to make/make_config.inc.
    	Dump prefix/blob stuff to top-level Makefile.config.
    
    	* configure.rc:
    	Change default value of all prefix related vars to <none>.
    	Add blobdir option / var.
    
    	* make/make_base.inc:
    	Properly define directories for various install types.
    
    	* make/make_defs.inc:
    	Define GLED_(UN)INSTALL_FILES/DIRS command variables.
    
    	* make/make_targets.inc:
    	Redefine all install targets.
    
    	* perllib/Gled_Installer.pm:
    	Complete rewrite - provide (un)install_files/dirs functions and
    	command-line argument parsing to support symlink-install,
    	recursive-install and dry-run (dump commands).

--- trunk/gled-build/perllib/Gled_Installer.pm
+++ trunk/gled-build/perllib/Gled_Installer.pm
@@ -8,104 +8,274 @@ package Gled_Installer;
 
 use Carp;
 
-sub filter_files {
-  # This one filters-out hidden files.
-  my @out = grep { $_ !~ m/^\./ } @_;
-  return @out;
+########################################################################
+
+$RECURSE = 0;
+$SYMLINK = 0;
+$DRYRUN  = 0;
+$HELP    = 0;
+
+########################################################################
+sub parse_opts
+########################################################################
+{
+  if (grep(/^--help$/, @ARGV))
+  {
+    $HELP = 1;
+    return;
+  }
+  while ($ARGV[0] =~ m/^-(\w+)$/)
+  {
+    $_ = $1;
+    $RECURSE = 1 if /r/;
+    $SYMLINK = 1 if /s/;
+    $DRYRUN  = 1 if /n/;
+    $HELP    = 1 if /h/;
+
+    shift @ARGV;
+  }
 }
 
-sub install_dirs {
+########################################################################
+sub option_string
+########################################################################
+{
+return <<"FNORD";
+Options:
+  -r    recurse [$RECURSE]
+  -s    symlink instead of rsync [$SYMLINK]
+  -n    dry-run - echo commands insted of executing them [$DRYRUN]
+  -h    help
+FNORD
+}
+
+########################################################################
+
+########################################################################
+sub filter_hidden
+########################################################################
+{
+  # This one filters-out hidden files.
+  return grep { $_ !~ m/^\./ } @_;
+}
+
+########################################################################
+sub find_files_and_dirs($\@\@)
+########################################################################
+{
+  my ($path, $files, $subdirs) = @_;
+
+  unless (opendir D, $path)
+  {
+    print "Gled_Installer::find_files_and_dirs $path not found ... skipping\n";
+    return 0;
+  }
+  my @dirlist = readdir D; closedir D;
+
+  @$files = grep (-f "$path/$_", @dirlist);
+  @$files = filter_hidden(@$files);
+
+  @$subdirs = grep (-d "$path/$_", @dirlist);
+  @$subdirs = filter_hidden(@$subdirs);
+
+  return $#$files >= 0 or $#$subdirs >= 0;
+}
+
+
+########################################################################
+# Low-level install functions
+########################################################################
+
+########################################################################
+sub install
+########################################################################
+{
+  my ($dest, $source, @files) = @_;
+
+  @files = map("$source/$_", @files);
+
+  unless (-d $dest)
+  {
+    system("mkdir -p $dest") and croak "mkdir of $dest failed";
+  }
+
+  if ($SYMLINK)
+  {
+    my @args = ("ln", "-sf", @files, $dest);
+    if ($DRYRUN)
+    {
+      print join(" ", @args), "\n";
+    }
+    else
+    {
+      system(@args) and croak "symlink to $dest failed";
+    }
+  }
+  else
+  {
+    my @args = ("rsync", "-a", @files, $dest);
+    if ($DRYRUN)
+    {
+      print join(" ", @args), "\n";
+    }
+    else
+    {
+      system(@args) and croak "rsync to $dest failed";
+    }
+  }
+}
+
+########################################################################
+sub uninstall
+########################################################################
+{
+  my ($dest, @files) = @_;
+
+  @files = map ("$dest/$_", @files);
+
+  my @args = ("rm", "-f", @files);
+  if ($DRYRUN)
+  {
+    print join(" ", @args), "\n";
+  }
+  else
+  {
+    system(@args) and croak "rm from $dest failed";
+  }
+}
+
+
+########################################################################
+# install of whole directories
+########################################################################
+
+########################################################################
+sub install_dirs
+########################################################################
+{
   my $dest_dir   = shift;
   my $source_dir = shift;
-  my @dirs = @_;
+  my @dirs       = @_;
 
   # For each $dir in @dirs, installs all files found in $source/$dir
-  # to $dest/$dir
+  # to $dest/$dir.
+  # If recurse is true, the directories found in source directories
+  # are installed recursively.
 
-  for $d (@dirs) {
-    unless( opendir D, "$source_dir/$d" ) {
-      # print "Gled_Installer::install_dirs $source_dir/$d not found ... skipping\n";
-      next;
-    }
-    my @files = readdir D;
-    closedir D;
-    @files = grep (-f "$source_dir/$d/$_", @files);
-    @files = filter_files(@files);
-    @files = map  ("$source_dir/$d/$_", @files);
+  for $d (@dirs)
+  {
+    my @files, @subdirs;
 
-    if($#files < 0) {
-      # print "Gled_Installer::install_dirs $source_dir/$d empty ... skipping\n";
-      next;
+    if (find_files_and_dirs("$source_dir/$d", @files, @subdirs))
+    {
+      if ($#files >= 0)
+      {
+        install("$dest_dir/$d", "$source_dir/$d", @files);
+      }
+      if ($RECURSE and $#subdirs >= 0)
+      {
+        install_dirs("$destdir/$d", "$source_dir/$d", @subdirs);
+      }
     }
-
-    unless(-d "$dest_dir/$d") {
-      system("mkdir $dest_dir/$d")
-	and croak "creation of $dest_dir/$d failed";
-    }
-    system("rsync", "-a", @files, "$dest_dir/$d")
-      and croak "copy to $dest_dir/$d failed";
   }
 }
 
-sub symlink_dirs {
+########################################################################
+sub uninstall_dirs
+########################################################################
+{
   my $dest_dir   = shift;
   my $source_dir = shift;
-  my @dirs = @_;
-
-  # For each $dir in @dirs, symlink all files found in $source/$dir
-  # to $dest/$dir
-
-  for $d (@dirs) {
-    unless( opendir D, "$source_dir/$d" ) {
-      # print "Gled_Installer::install_dirs $source_dir/$d not found ... skipping\n";
-      next;
-    }
-    my @files = readdir D;
-    closedir D;
-    @files = grep (-f "$source_dir/$d/$_", @files);
-    @files = filter_files(@files);
-    @files = map  ("$source_dir/$d/$_", @files);
-
-    if($#files < 0) {
-      # print "Gled_Installer::install_dirs $source_dir/$d empty ... skipping\n";
-      next;
-    }
-
-    unless(-d "$dest_dir/$d") {
-      system("mkdir $dest_dir/$d")
-	and croak "creation of $dest_dir/$d failed";
-    }
-    system("ln", "-sf", @files, "$dest_dir/$d")
-      and croak "copy to $dest_dir/$d failed";
-  }
-}
-
-sub uninstall_dirs {
-  my $dest_dir   = shift;
-  my $source_dir = shift;
-  my @dirs = @_;
+  my @dirs       = @_;
 
   # For each $dir in @dirs, uninstalls all files found in $source/$dir
-  # from $dest/$dir
+  # from $dest/$dir.
+  # If recurse is true, the directories found in source directories
+  # are uninstalled recursively.
+  # No check is done if the directories remain empty afterwards -
+  # they are always left there.
 
-  for $d (@dirs) {
-    unless( opendir D, "$source_dir/$d" ) {
-      # print "Gled_Installer::uninstall_dirs $source_dir/$d not found ... skipping\n";
-      next;
+  for $d (@dirs)
+  {
+    my @files, @subdirs;
+
+    if (find_files_and_dirs("$source_dir/$d", @files, @subdirs))
+    {
+      if ($#files >= 0)
+      {
+        uninstall("$dest_dir/$d", @files);
+      }
+      if ($RECURSE and $#subdirs >= 0)
+      {
+        uninstall_dirs("$destdir/$d", "$source_dir/$d", @subdirs);
+      }
     }
-    my @files = readdir D;
-    closedir D;
-    @files = grep (-f "$source_dir/$d/$_", @files);
-    @files = filter_files(@files);
-
-    if($#files < 0) {
-      # print "Gled_Installer::uninstall_dirs $source_dir/$d empty ... skipping\n";
-      next;
-    }
-
-    @files = map  ("$dest_dir/$d/$_", @files);
-    system("rm", "-f", @files);
   }
 }
+
+
+########################################################################
+# install of files
+########################################################################
+
+########################################################################
+sub install_files
+########################################################################
+{
+  my $dest_dir   = shift;
+  my $source_dir = shift;
+
+  # Installs all files in $source_dir to $dest/$dir.
+  # If recurse is true, the directories found in $source_dir
+  # are installed recursively.
+
+  my @files, @subdirs;
+
+  if (find_files_and_dirs("$source_dir", @files, @subdirs))
+  {
+    if ($#files >= 0)
+    {
+      install($dest_dir, $source_dir, @files);
+    }
+    if ($RECURSE and $#subdirs >= 0)
+    {
+      for $d (@subdirs)
+      {
+        install_files("$destdir/$d", "$source_dir/$d");
+      }
+    }
+  }
+}
+
+########################################################################
+sub uninstall_files
+########################################################################
+{
+  my $dest_dir   = shift;
+  my $source_dir = shift;
+
+  # Uninstalls all files in $source_dir to $dest/$dir.
+  # If recurse is true, the directories found in $source_dir
+  # are uninstalled recursively.
+
+  my @files, @subdirs;
+
+  if (find_files_and_dirs("$source_dir", @files, @subdirs))
+  {
+    if ($#files >= 0)
+    {
+      uninstall($dest_dir, @files);
+    }
+    if ($recurse and $#subdirs >= 0)
+    {
+      for $d (@subdirs)
+      {
+        uninstall_files("$destdir/$d", "$source_dir/$d");
+      }
+    }
+  }
+}
+
+########################################################################
 
 1;
