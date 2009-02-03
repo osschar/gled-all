28db0e59781de7670b1a74b04a08a7805a3299a6
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2009-02-03 12:44:37 +0000

    * testfltdbl.C: 	Modify the test to actually measure someting even when compiled as 	optimized code. 	Add running instructions.

--- trunk/demos/Var1/testfltdbl.C
+++ trunk/demos/Var1/testfltdbl.C
@@ -1,25 +1,27 @@
-#include <Gled/GTime.h>
-#include <math.h>
+// Compare float / double operation speed.
+// Run gled, then do:
+//  .L testfltdbl.C+  // loads this in compiled mode
+//  testfltdbl()
 
-int    NN = 1000000000;
-float  FX = 0.000001f;
-double DX = 0.000001;
+#include <../GledCore/Gled/GTime.h>
+#include <cmath>
+#include <cstdio>
+
+typedef ULong64_t Count_t;
+
+volatile Count_t NN = 100000000;
+volatile float   FX = 0.000001f;
+volatile double  DX = 0.000001;
 
 /**************************************************************************/
 
 double emptyloop()
 {
   GTime t(GTime::I_Now);
-  for (int i=0; i<NN; ++i);
-  return t.TimeUntilNow().ToDouble();
-}
-
-double emptyloop2()
-{
-  GTime t(GTime::I_Now);
-  int n = NN;
-  while(n--);
-  return t.TimeUntilNow().ToDouble();
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    ++j;
+  return t.TimeUntilNow().ToDouble() + (j - j);
 }
 
 /**************************************************************************/
@@ -27,19 +29,21 @@ double emptyloop2()
 double dbl_to_flt()
 {
   GTime t(GTime::I_Now);
-  float f = 0.0f;
-  for (int i=0; i<NN; ++i)
-    f = DX;
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f = DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f) + (j - j);
 }
 
 double flt_to_dbl()
 {
   GTime t(GTime::I_Now);
-  double f = 0.0;
-  for (int i=0; i<NN; ++i)
-    f = FX;
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.0;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f = FX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 /**************************************************************************/
@@ -47,37 +51,51 @@ double flt_to_dbl()
 double dbl_plus_dbl()
 {
   GTime t(GTime::I_Now);
-  double f = 0.0;
-  for (int i=0; i<NN; ++i)
-    f += DX;
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.0;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f += DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double dbl_plus_flt()
 {
   GTime t(GTime::I_Now);
-  double f = 0.0;
-  for (int i=0; i<NN; ++i)
-    f += FX;
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.0;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f += FX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double flt_plus_flt()
 {
   GTime t(GTime::I_Now);
-  float f = 0.0f;
-  for (int i=0; i<NN; ++i)
-    f += FX;
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f += FX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double flt_plus_dbl()
 {
   GTime t(GTime::I_Now);
-  float f = 0.0f;
-  for (int i=0; i<NN; ++i)
-    f += DX;
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f += DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
+}
+
+double flt_plus_dbl_2()
+{
+  GTime t(GTime::I_Now);
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f += (float) DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 /**************************************************************************/
@@ -85,37 +103,51 @@ double flt_plus_dbl()
 double dbl_times_dbl()
 {
   GTime t(GTime::I_Now);
-  double f = 0.0;
-  for (int i=0; i<NN; ++i)
-    f *= DX;
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.0;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f *= DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double dbl_times_flt()
 {
   GTime t(GTime::I_Now);
-  double f = 0.0;
-  for (int i=0; i<NN; ++i)
-    f *= FX;
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.0;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f *= FX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double flt_times_flt()
 {
   GTime t(GTime::I_Now);
-  float f = 0.0f;
-  for (int i=0; i<NN; ++i)
-    f *= FX;
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f *= FX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double flt_times_dbl()
 {
   GTime t(GTime::I_Now);
-  float f = 0.0f;
-  for (int i=0; i<NN; ++i)
-    f *= DX;
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f *= DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
+}
+
+double flt_times_dbl_2()
+{
+  GTime t(GTime::I_Now);
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f *= (float) DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 /**************************************************************************/
@@ -123,37 +155,51 @@ double flt_times_dbl()
 double dbl_divide_dbl()
 {
   GTime t(GTime::I_Now);
-  double f = 0.0;
-  for (int i=0; i<NN; ++i)
-    f /= DX;
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.0;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f /= DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double dbl_divide_flt()
 {
   GTime t(GTime::I_Now);
-  double f = 0.0;
-  for (int i=0; i<NN; ++i)
-    f /= FX;
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.0;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f /= FX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double flt_divide_flt()
 {
   GTime t(GTime::I_Now);
-  float f = 0.0f;
-  for (int i=0; i<NN; ++i)
-    f /= FX;
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f /= FX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double flt_divide_dbl()
 {
   GTime t(GTime::I_Now);
-  float f = 0.0f;
-  for (int i=0; i<NN; ++i)
-    f /= DX;
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f /= DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
+}
+
+double flt_divide_dbl_2()
+{
+  GTime t(GTime::I_Now);
+  volatile float f = 0.0f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f /= (float) DX, ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 /**************************************************************************/
@@ -161,21 +207,42 @@ double flt_divide_dbl()
 double sin_flt()
 {
   GTime t(GTime::I_Now);
-  float f = 0.14f;
-  for (int i=0; i<NN; ++i)
-    f = sin(f);
-  return t.TimeUntilNow().ToDouble();
+  volatile float f = 0.14f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f = sin(f), ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
 double sin_dbl()
 {
   GTime t(GTime::I_Now);
-  double f = 0.14;
-  for (int i=0; i<NN; ++i)
-    f = sin(f);
-  return t.TimeUntilNow().ToDouble();
+  volatile double f = 0.14;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f = sin(f), ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
 }
 
+double sinf_flt()
+{
+  GTime t(GTime::I_Now);
+  volatile float f = 0.14f;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f = sinf(f), ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
+}
+
+double sinf_dbl()
+{
+  GTime t(GTime::I_Now);
+  volatile double f = 0.14;
+  volatile Count_t j = 0;
+  for (volatile Count_t i=0; i<NN; ++i)
+    f = sinf(f), ++j;
+  return t.TimeUntilNow().ToDouble() + (f - f);
+}
 
 /**************************************************************************/
 
@@ -186,41 +253,46 @@ struct bar {
   foo          func;
 };
 
-bar tests[] = {
+bar tests[] =
+{
   { "emptyloop",	emptyloop	},
-  /*
-  { "emptyloop2",	emptyloop2	},
   { "dbl_to_flt",	dbl_to_flt,	},
   { "flt_to_dbl",	flt_to_dbl	},
+
   { "dbl_plus_dbl",	dbl_plus_dbl	},
   { "dbl_plus_flt",	dbl_plus_flt	},
   { "flt_plus_flt",	flt_plus_flt	},
   { "flt_plus_dbl",	flt_plus_dbl	},
+  { "flt_plus_dbl_2",	flt_plus_dbl_2	},
+
   { "dbl_times_dbl",	dbl_times_dbl	},
   { "dbl_times_flt",	dbl_times_flt	},
   { "flt_times_flt",	flt_times_flt	},
   { "flt_times_dbl",	flt_times_dbl	},
+  { "flt_times_dbl_2",	flt_times_dbl_2	},
+
   { "dbl_divide_dbl",	dbl_divide_dbl	},
   { "dbl_divide_flt",	dbl_divide_flt	},
   { "flt_divide_flt",	flt_divide_flt	},
   { "flt_divide_dbl",	flt_divide_dbl	},
-  */
+  { "flt_divide_dbl_2",	flt_divide_dbl_2},
+
   { "sin_flt",		sin_flt         },
   { "sin_dbl",	        sin_dbl 	},
+  { "sinf_flt",		sinf_flt        },
+  { "sinf_dbl",	        sinf_dbl 	},
 
   // { "",		},
   { 0, 0 }
 };
 
-int main()
+void testfltdbl()
 {
-  double empty = emptyloop();
-
   bar* tst = tests;
   while (tst->name)
   {
     double time = tst->func();
-    printf("%-16s %-10f %-10f\n", tst->name, time, time - empty);
+    printf("%-16s %-10f\n", tst->name, time);
     ++tst;
   }
 }
