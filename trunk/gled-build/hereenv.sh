bf0b0625bb95c654897e9661f9b29db26bb47500
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2009-03-01 19:47:37 +0000

    Clarify help, comment out osx settings.

--- trunk/gled-build/hereenv.sh
+++ trunk/gled-build/hereenv.sh
@@ -1,6 +1,7 @@
-# This is environment for fast gled-build set-up.
-# Use as: . hereenv.sh
-# Should be run from top Gled directory
+# This is environment for using exisitng external software and ROOT.
+# It expects external software in external/ and ROOT in root/.
+# Usage: . hereenv.sh
+# Should be run from the 'gled-build' directory.
 
 dir=`pwd`
 
@@ -8,24 +9,24 @@ export EXTD=$dir/external
 export ROOTSYS=$dir/root
 export GLEDSYS=$dir
 
-# ROOT
-PATH=$ROOTSYS/bin:$PATH
-LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
-
 # External
 PATH=$dir/external/bin:$PATH
 LD_LIBRARY_PATH=$dir/external/lib:$LD_LIBRARY_PATH
 MANPATH=$MANPATH:$dir/external/man
 
+# ROOT
+PATH=$ROOTSYS/bin:$PATH
+LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
+
 # Gled
 PATH=$GLEDSYS/bin:$PATH
 LD_LIBRARY_PATH=$GLEDSYS/lib:$LD_LIBRARY_PATH
 
+export LD_LIBRARY_PATH MANPATH
 
-# OSX specific
-export MACOSX_DEPLOYMENT_TARGET=10.4
-DYLD_LIBRARY_PATH=$ROOTSYS/lib:$DYLD_LIBRARY_PATH
-DYLD_LIBRARY_PATH=$dir/external/lib:$DYLD_LIBRARY_PATH
-DYLD_LIBRARY_PATH=$GLEDSYS/lib:$DYLD_LIBRARY_PATH
-
-export PATH LD_LIBRARY_PATH DYLD_LIBRARY_PATH MANPATH
+# OSX specific, probably obsolete
+#export MACOSX_DEPLOYMENT_TARGET=10.4
+#DYLD_LIBRARY_PATH=$ROOTSYS/lib:$DYLD_LIBRARY_PATH
+#DYLD_LIBRARY_PATH=$dir/external/lib:$DYLD_LIBRARY_PATH
+#DYLD_LIBRARY_PATH=$GLEDSYS/lib:$DYLD_LIBRARY_PATH
+#export DYLD_LIBRARY_PATH
