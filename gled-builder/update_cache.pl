536a4ac3a6dd11062e8b086d3d18e9c786681692
Matevž Tadel <osschar@users.noreply.github.com>
2009-03-30 19:14:15 +0000

    Improve management of external-package cache.
    	* GledBuildExternal.pm:
    	Add functions for cache handling and interaction.
    	Use them also when setting up and downloading a package.
    
    	* update_cache.pl:
    	New script - updates the cache state, downloads new versions and
    	optionally rebuilds the affected packages.

---
+++ gled-builder/update_cache.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use lib ".";
+use GledBuildExternal;
+
+if ($ARGV[0] eq '-h' or $ARGV[0] eq '--help')
+{
+  print <<"FNORD";
+Usage: $0 [--rebuild]
+Refreshes list of external packages available on the server and downloads
+the new versions of packages that were already in the cache.
+If --rebuild is specified, the affected packages are also rebuilt.
+FNORD
+  exit 0;
+}
+
+my @update_pkgs = update_cache();
+
+my $make_cmd = "make -C external ", join(" ", @update_pkgs);
+
+if (@update_pkgs)
+{
+  if ($ARGV[0] eq '--rebuild')
+  {
+    system_or_die($make_cmd);
+  }
+  else
+  {
+    print "Some packages have been updated. Run the following command to rebuild them:\n";
+    print "$make_cmd\n";
+  }
+}
+else
+{
+  print "No updates necessary.\n";
+}
