ea2779fe775dfccd13a2a8a19c69eea4bf3423eb
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2009-07-14 21:14:13 +0000

    Draw box so that shaders get correct vertex positions.
    	* Rnr/GL/Box_GL_Rnr.cxx:
    	Properly place corner vertices at coordinates as given by the mA,
    	mB and mC size variables (scaling was used before).

--- trunk/libsets/GledCore/Rnr/GL/Box_GL_Rnr.cxx
+++ trunk/libsets/GledCore/Rnr/GL/Box_GL_Rnr.cxx
@@ -10,37 +10,38 @@
 
 /**************************************************************************/
 
-namespace {
-  GLfloat a = 0.5;
-  GLfloat V_Cube[] = { a,a,a,	a,-a,a,		a,-a,-a,	a,a,-a,
-		       -a,a,a,	-a,-a,a,	-a,-a,-a,	-a,a,-a };
+namespace
+{
+  const GLubyte I_Cube[] = { 0,1,2,3,	3,2,6,7, 2,1,5,6,
+			     6,5,4,7, 7,4,0,3, 0,4,5,1 };
 
-  GLubyte I_Cube[] = { 0,1,2,3,	3,2,6,7,	2,1,5,6,	6,5,4,7,
-		       7,4,0,3,	0,4,5,1 };
-
-  GLfloat N_Cube[][3] = { {1,0,0}, {0,0,-1}, {0,-1,0},
-			  {-1,0,0}, {0,1,0}, {0,0,1} };
+  const GLfloat N_Cube[][3] = { { 1,0,0}, {0,0,-1}, {0,-1,0},
+				{-1,0,0}, {0,1, 0}, {0, 0,1} };
 }
 
 void Box_GL_Rnr::Draw(RnrDriver* rd)
 {
-  glPushAttrib(GL_CURRENT_BIT);
+  GLfloat a = 0.5f * mBox->mA;
+  GLfloat b = 0.5f * mBox->mB;
+  GLfloat c = 0.5f * mBox->mC;
+
+  GLfloat V_Cube[][3] = { { a,b,c}, { a,-b,c}, { a,-b,-c}, { a,b,-c},
+			  {-a,b,c}, {-a,-b,c}, {-a,-b,-c}, {-a,b,-c} };
+
   rd->GL()->Color(mBox->mColor);
-  glPushMatrix();
-  glScalef(mBox->mA, mBox->mB, mBox->mC);
 
   glEnableClientState(GL_VERTEX_ARRAY);
   glVertexPointer(3, GL_FLOAT, 0, V_Cube);
 
   glBegin(GL_QUADS);
   int idx = 0;
-  for(int i=0; i<6; ++i) {
+  for (int i=0; i<6; ++i)
+  {
     glNormal3fv(N_Cube[i]);
-    for(int j=0; j<4; ++j, ++idx) glArrayElement(I_Cube[idx]);
+    for (int j=0; j<4; ++j, ++idx)
+      glArrayElement(I_Cube[idx]);
   }
   glEnd();
-  glDisableClientState(GL_VERTEX_ARRAY);
 
-  glPopMatrix();
-  glPopAttrib();
+  glDisableClientState(GL_VERTEX_ARRAY);
 }
