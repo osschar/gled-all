3ef1eecd64b4e98794f126139130f2002560cee1
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2009-12-29 13:00:57 +0000

    Remove extra libset loading - this was solved by renaming libset GTS to GledGTS.

--- trunk/libsets/GledCore/macros/mac_hack.C
+++ trunk/libsets/GledCore/macros/mac_hack.C
@@ -1,12 +1,6 @@
 void mac_hack()
 {   
-   gSystem->Load("libGledCore_Pupils");
-   gROOT->ProcessLine(".L gled_view_globals.C");
-  	gROOT->ProcessLine(".L eye.C");
-   
-   Gled::theOne->LoadLibSet("Geom1");
-   Gled::theOne->LoadLibSet("GTS");
-   Gled::theOne->LoadLibSet("Audio1");
-   Gled::theOne->LoadLibSet("Numerica");
-   Gled::theOne->LoadLibSet("Var1");
+  gSystem->Load("libGledCore_Pupils");
+  gROOT->ProcessLine(".L gled_view_globals.C");
+  gROOT->ProcessLine(".L eye.C");
 }
