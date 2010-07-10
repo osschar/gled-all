6e649320ba39033f9dbe4f8964fc2db7c7a89e95
Matevž Tadel <osschar@users.noreply.github.com>
2010-07-10 21:23:12 +0000

    Add abstract interface GTSIsoMakerFunctor that allows iso-surfaces to be defined by a glass.
    	* Stones/GTSIsoMakerFunctor.cxx:
    	* Stones/GTSIsoMakerFunctor.h:
    	New stone - abstract interface for iso-surface maker functor.
    
    	* Glasses/GTSIsoMaker.cxx:
    	* Glasses/GTSIsoMaker.h:
    	Add support for function source being an external lens.
    	It must be sub-classed from GTSIsoMakerFunctor.
    	This is the default if the link Functor is set.

---
+++ libsets/GledGTS/Stones/GTSIsoMakerFunctor.cxx
@@ -0,0 +1,15 @@
+// $Id$
+
+// Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#include "GTSIsoMakerFunctor.h"
+
+// GTSIsoMakerFunctor
+
+//______________________________________________________________________________
+//
+//
+
+ClassImp(GTSIsoMakerFunctor);
---
+++ libsets/GledGTS/Stones/GTSIsoMakerFunctor.h
@@ -0,0 +1,25 @@
+// $Id$
+
+// Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef GledGTS_GTSIsoMakerFunctor_H
+#define GledGTS_GTSIsoMakerFunctor_H
+
+#include <Rtypes.h>
+
+class GTSIsoMakerFunctor
+{
+public:
+  virtual ~GTSIsoMakerFunctor() {}
+
+  virtual void     GTSIsoBegin(Double_t iso_value) {}
+  virtual Double_t GTSIsoFunc(Double_t x, Double_t y, Double_t z) = 0;
+  virtual void     GTSIsoEnd() {}
+
+#include "GTSIsoMakerFunctor.h7"
+  ClassDef(GTSIsoMakerFunctor, 0);
+}; // endclass GTSIsoMakerFunctor
+
+#endif
