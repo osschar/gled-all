806652613bef6d535fd2f9fbbccae344fec3b873
Matevž Tadel <osschar@users.noreply.github.com>
2010-07-18 21:31:51 +0000

    Several improvements in iso-surface generation.
    	* Glasses/TabletStrokeList.cxx:
    	* Glasses/TabletStrokeList.h:
    	Implement full set of parameters for controling generation of
    	iso-surface from a set of strokes.
    	Implement two new algorithms for calculating potential at given
    	point:
    	- StrongestPoint: trivial, takes the strongest point;
    	- LinearInterpolation: interpolates point parameters between two
    	  closest / strongest points and recalculates the potential for this
    	  point.
    	Properly calculate search radius passed into KD-tree.

--- libsets/Tmp1/Glasses/TabletStrokeList.cxx
+++ libsets/Tmp1/Glasses/TabletStrokeList.cxx
@@ -13,7 +13,11 @@
 
 //______________________________________________________________________________
 //
+// Potential is really (distance-from-point)^2
 //
+// mWidth         - radius where potential is one (for full-pressure)
+// mPotentialExp  - exponent applied to this potential, usually negative
+// mPressureAlpha - exponent of the pressure curve
 
 ClassImp(TabletStrokeList);
 
@@ -21,8 +25,11 @@ ClassImp(TabletStrokeList);
 
 void TabletStrokeList::_init()
 {
-  mExp   = -0.5;
-  mWidth =  0.02;
+  mAlgorithm      =  PA_TwoStrongestPoints;
+  mWidth          =  0.01;
+  mPotentialExp   = -0.5;
+  mPressureAlpha  =  2;
+  mMaxInterPoint  =  0.015;
 }
 
 TabletStrokeList::TabletStrokeList(const Text_t* n, const Text_t* t) :
@@ -102,7 +109,29 @@ void TabletStrokeList::GTSIsoBegin(Double_t iso_value)
   if (mKDTree == 0)
     MakeKDStuff();
 
-  mIsoValue = iso_value;
+  using namespace TMath;
+
+  // Find distance where two consequitive points just make up the iso surface.
+  // Standard maximum distance in tablet space is 0.01, default we take is 0.015.
+  {
+    const Double_t TR = mMaxInterPoint;
+    const Double_t TE = 2.0*mPotentialExp;
+    const Double_t C  = iso_value * Power(mWidth, TE);
+    Double_t R = mWidth;
+    Double_t V, D;
+    Int_t N = 0;
+    do
+    {
+      V = Power(R, TE) + Power(R + TR, TE) - C;
+      D = TE * (Power(R, TE - 1) + Power(R + TR, TE - 1));
+      // printf("Tangent method: R=%g, V=%g, D=%g\n", R, V, D);
+      R -= V / D;
+      ++N;
+    } while (Abs(V) > 1e-8 && N < 100);
+
+    mSearchRad = TR + R;
+  }
+  printf("TabletStrokeList::GTSIsoBegin iso=%f, search-radius=%f\n", iso_value, mSearchRad);
 }
 
 namespace
@@ -115,26 +144,71 @@ Double_t TabletStrokeList::GTSIsoFunc(Double_t x, Double_t y, Double_t z)
   Float_t       in_point[2] = { x, y };
   vector<Int_t> result;
 
-  mKDTree->FindInRange(in_point, 5*mWidth, result);
+  mKDTree->FindInRange(in_point, mSearchRad, result);
 
-  Double_t vm[2] = { 0, 0 };
+  if (result.empty()) return 0;
+
+  Int_t    id[2] = { -1, -1 };
+  Double_t vm[2] = {  0,  0 };
 
   for (vector<Int_t>::iterator pi = result.begin(); pi != result.end(); ++pi)
   {
     STabletPoint &p = * mPointRefs[*pi];
-    Double_t v = TMath::Power((sqr(x - p.x) + sqr(y - p.y) + sqr(z)) / sqr(mWidth*p.p), mExp);
+
+    const Double_t Dsqr = sqr(x - p.x) + sqr(y - p.y) + sqr(z);
+    const Double_t R    = mWidth * TMath::Power(p.p, mPressureAlpha);
+    const Double_t v    = TMath::Power(Dsqr / sqr(R), mPotentialExp);
+
     if (v > vm[0])
     {
-      vm[1] = vm[0];
-      vm[0] = v;
+      vm[1] = vm[0]; id[1] = id[0];
+      vm[0] = v;     id[0] = *pi;
     }
     else if (v > vm[1])
     {
-      vm[1] = v;
+      vm[1] = v;     id[1] = *pi;
     }
   }
 
-  return vm[0] + vm[1];
+  switch (mAlgorithm)
+  {
+    case PA_StrongestPoint:
+    {
+      return vm[0];
+    }
+    case PA_TwoStrongestPoints:
+    {
+      return vm[0] + vm[1];
+    }
+    case PA_LinearInterpolation:
+    {
+      if (id[1] == -1 || TMath::Abs(id[0] - id[1]) != 1) return vm[0];
+
+      STabletPoint &a = * mPointRefs[id[0]];
+      STabletPoint &b = * mPointRefs[id[1]];
+
+      STabletPoint atob = b - a;
+      HPointF      atop(x, y, z); atop -= a;
+      Float_t      dp = atob.Dot(atop);
+
+      // Check if we are off one or the other edge:
+      if (dp <= 0) return vm[0];
+      Float_t atob_mag  = atob.Mag();
+      dp /= atob_mag;
+      if (dp >= atob_mag) return vm[1];
+
+      Float_t f = dp / atob_mag;
+      STabletPoint p = (1.0f - f)*a + f*b;
+
+      const Double_t Dsqr = sqr(x - p.x) + sqr(y - p.y) + sqr(z);
+      const Double_t R    = mWidth * TMath::Power(p.p, mPressureAlpha);
+
+      return TMath::Power(Dsqr / sqr(R), mPotentialExp);
+    }
+  }
+
+  // gcc was complaining ...
+  return 0;
 }
 
 void TabletStrokeList::GTSIsoEnd()
