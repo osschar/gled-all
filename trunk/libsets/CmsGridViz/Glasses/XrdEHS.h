d42bd02fc0b6806a07e8b629b27b7bdeed7dc8fa
alja <alja@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2011-10-22 03:12:29 +0000

    * Glasses/XrdEHS.cxx: 	* Glasses/XrdEHS.h: 	First version of embedded http server to provide a web page with info 	about open files, its users, etc.
    	* glass.list:
    	New XrdEHS class.

---
+++ trunk/libsets/CmsGridViz/Glasses/XrdEHS.h
@@ -0,0 +1,37 @@
+// $Id$
+
+// Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef CmsGridViz_XrdEHS_H
+#define CmsGridViz_XrdEHS_H
+
+#include <Glasses/ZNameMap.h>
+
+
+class XrdMonSucker;
+
+class XrdEHS : public ZNameMap
+{
+private:
+   void _init();
+   Bool_t	  b_stop_server; //!
+   XrdMonSucker*  mXrdReporter;  //!
+
+protected:
+   Int_t	mPort;     // X{GS} 7 Value(-range=>[1,65535,1])
+   Bool_t	bServerUp; // X{GS} 7 BoolOut()
+
+public:
+   XrdEHS( XrdMonSucker* x = 0);
+   virtual ~XrdEHS();
+
+   void StartServer(); // X{Ed} 7 MButt(-join=>1)
+   void StopServer();  // X{E}  7 MButt()
+
+#include "XrdEHS.h7"
+   ClassDef(XrdEHS, 1);
+}; // endclass XrdEHS
+
+#endif
