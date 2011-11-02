998aeda9849939c5f1eb90c48bc4d5ef03ab6b1e
Matevž Tadel <osschar@users.noreply.github.com>
2011-11-02 01:46:09 +0000

    Standardize constructor & link usage.
    	* Glasses/XrdEHS.cxx:
    	* Glasses/XrdEHS.h:
    	Make XrdMonSucker a ZLink.
    	Use standard constructor arguments.
    	Put sub-class of EHS into an unnamed namespace.

--- libsets/CmsGridViz/Glasses/XrdEHS.cxx
+++ libsets/CmsGridViz/Glasses/XrdEHS.cxx
@@ -5,6 +5,7 @@
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
 
 #include "XrdEHS.h"
+#include "XrdMonSucker.h"
 #include "XrdEHS.c7"
 
 #include "TSystem.h"
@@ -14,7 +15,6 @@
 #include <ehs/ehstypes.h>
 
 
-#include "XrdMonSucker.h"
 #include "XrdFile.h"
 #include "XrdUser.h"
 #include "XrdServer.h"
@@ -29,50 +29,52 @@
 ClassImp(XrdEHS);
 
 //==============================================================================
+// EHS helper
 //==============================================================================
-//==============================================================================
 
-class  FormTester : public EHS
+namespace
 {
-public:
-   FormTester (XrdMonSucker* x ) : mRef( x ) { }
-   FormTester () : mRef( 0 ) { m_oParams.size(); }
+  class FormTester : public EHS
+  {
+  public:
+    FormTester (XrdMonSucker* x ) : mRef( x ) { }
+    FormTester () : mRef( 0 ) { m_oParams.size(); }
 
-   ResponseCode HandleRequest ( HttpRequest *, HttpResponse * );
+    ResponseCode HandleRequest ( HttpRequest *, HttpResponse * );
 
-   TimeStamp_t   mStamp;
-   list<XrdFile*> mList;
-   XrdMonSucker* mRef;
-};
+    TimeStamp_t    mStamp;
+    list<XrdFile*> mList;
+    XrdMonSucker*  mRef;
+  };
 
 
-// creates a page based on user input -- either displays data from
-// form or presents a form for users to submit data.
-ResponseCode FormTester::HandleRequest ( HttpRequest * request, HttpResponse * response )
-{
-   enum EMode { kAll, kFileNames, kHelp };
+  // creates a page based on user input -- either displays data from
+  // form or presents a form for users to submit data.
+  ResponseCode FormTester::HandleRequest ( HttpRequest * request, HttpResponse * response )
+  {
+    enum EMode { kAll, kFileNames, kHelp };
 
-   EMode mode = kAll;
+    EMode mode = kAll;
 
-   // std::cerr << request->Uri() << endl;
+    // std::cerr << request->Uri() << endl;
 
-   if (! request->FormValues().empty()) {
+    if (! request->FormValues().empty()) {
       mode = kHelp;
       if (request->FormValues("mode").m_sBody.length())
       {
-         if ( request->FormValues("mode").m_sBody == "all")
-            mode = kAll;
-         else if ( request->FormValues("mode").m_sBody == "files")
-            mode = kFileNames;
-         else 
-            mode = kHelp;
+        if ( request->FormValues("mode").m_sBody == "all")
+          mode = kAll;
+        else if ( request->FormValues("mode").m_sBody == "files")
+          mode = kFileNames;
+        else 
+          mode = kHelp;
       }
-   }
+    }
 
-   ostringstream oss;
+    ostringstream oss;
 
-   if ( mode == kHelp)
-   {
+    if ( mode == kHelp)
+    {
       std::string name = Form("http://%s:%s/?mode=", gSystem->HostName(),  m_oParams["port"].GetCharString());
 
 
@@ -81,94 +83,91 @@ ResponseCode FormTester::HandleRequest ( HttpRequest * request, HttpResponse * r
       oss << "<a href=\"" << name << "all\">" << name << "all </a>";
       response->SetBody( oss.str().c_str(), oss.str().length() );
       return HTTPRESPONSECODE_200_OK;
-   }
+    }
 
 
-   ZHashList* hl = mRef->GetOpenFiles();
+    ZHashList* hl = mRef->GetOpenFiles();
   
-   if (mStamp != hl->GetTimeStamp() )
-   {
+    if (mStamp != hl->GetTimeStamp() )
+    {
       mList.clear();
       hl->CopyListByGlass<XrdFile>(mList);
       mStamp = hl->GetTimeStamp();
-   }
+    }
 
-   oss << "<html><body><head><title>Xrd open files ["<< mList.size() << "]</title></head>" << endl;
+    oss << "<html><body><head><title>Xrd open files ["<< mList.size() << "]</title></head>" << endl;
 
-   oss << "<meta http-equiv=\"refresh\" content=\"180\" />" << endl;
+    oss << "<meta http-equiv=\"refresh\" content=\"180\" />" << endl;
 
-   oss << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/sorttable.js \"> </script>"<< endl;
-   oss << ""<< endl;
-   oss << "<style type=\"text/css\">"<< endl;
-   oss << "th, td {"<< endl;
-   oss << "  padding: 3px !important;"<< endl;
-   oss << "}"<< endl;
-   oss << ""<< endl;
-   oss << "table.sortable thead {"<< endl;
-   oss << "    background-color:#eee;"<< endl;
-   oss << "    color:#666666; "<< endl;
-   oss << "    font-weight: bold;"<< endl;
-   oss << "    cursor: default; "<< endl;
-   oss << "}"<< endl;
-   oss << "</style>"<< endl;
-   oss << "<br>"<< endl;
+    oss << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/sorttable.js \"> </script>"<< endl;
+    oss << ""<< endl;
+    oss << "<style type=\"text/css\">"<< endl;
+    oss << "th, td {"<< endl;
+    oss << "  padding: 3px !important;"<< endl;
+    oss << "}"<< endl;
+    oss << ""<< endl;
+    oss << "table.sortable thead {"<< endl;
+    oss << "    background-color:#eee;"<< endl;
+    oss << "    color:#666666; "<< endl;
+    oss << "    font-weight: bold;"<< endl;
+    oss << "    cursor: default; "<< endl;
+    oss << "}"<< endl;
+    oss << "</style>"<< endl;
+    oss << "<br>"<< endl;
 
-   oss << ""<< endl;
-   oss << "   <table class=\"sortable\">"<< endl;
-   oss << "  <tr>"<< endl;
-   oss << "    <th>File</th> " << endl;
-   if (mode == kAll) { 
+    oss << ""<< endl;
+    oss << "   <table class=\"sortable\">"<< endl;
+    oss << "  <tr>"<< endl;
+    oss << "    <th>File</th> " << endl;
+    if (mode == kAll) { 
       oss << " <th>OpenTime</th> <th>ServerDomain</th>  <th>ClientDomain</th>  <th>DN</th>  <th>Read [MB]</th>  <th>LastUpdateAgo</th>"<< endl;
-   }
-   oss << "  </tr>"<< endl;
+    }
+    oss << "  </tr>"<< endl;
 
 
-   for (list<XrdFile*>::iterator xfi = mList.begin(); xfi != mList.end(); ++xfi)
-   {
+    for (list<XrdFile*>::iterator xfi = mList.begin(); xfi != mList.end(); ++xfi)
+    {
       oss << "<tr>"<< endl;
       oss << Form("<td>%s</td>", (*xfi)->GetName()) << endl;
       if (mode == kAll)
       {
-         oss << Form("<td>%s</td>", (*xfi)->GetOpenTime().ToDateTimeLocal().Data()) << endl;
-         oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetServer()->GetDomain()) << endl;
-         oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetFromDomain()) << endl;
-         oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetDN()) << endl;
+        oss << Form("<td>%s</td>", (*xfi)->GetOpenTime().ToDateTimeLocal().Data()) << endl;
+        oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetServer()->GetDomain()) << endl;
+        oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetFromDomain()) << endl;
+        oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetDN()) << endl;
 
-         oss << Form("<td>%.3f</td>", (*xfi)->GetReadStats().GetSumX()) << endl;
-         GTime lastUp = (*xfi)->GetLastMsgTime();
-         if (lastUp.IsNever())
-         {
-            oss << "<td>Never</td>\n";
-         }
-         else
-         {
-            int sinceUp =  (int) ((GTime::Now() - lastUp).GetSec());
-            int hours = (sinceUp)/3600;
-            int min = (sinceUp - hours*3600)/60;
-            int sec = sinceUp - hours*3600 - min * 60;
-            oss << Form("<td>%02d:%02d:%02d</td>", hours, min, sec) << endl;
-         }
+        oss << Form("<td>%.3f</td>", (*xfi)->GetReadStats().GetSumX()) << endl;
+        GTime lastUp = (*xfi)->GetLastMsgTime();
+        if (lastUp.IsNever())
+        {
+          oss << "<td>Never</td>\n";
+        }
+        else
+        {
+          int sinceUp =  (int) ((GTime::Now() - lastUp).GetSec());
+          int hours = (sinceUp)/3600;
+          int min = (sinceUp - hours*3600)/60;
+          int sec = sinceUp - hours*3600 - min * 60;
+          oss << Form("<td>%02d:%02d:%02d</td>", hours, min, sec) << endl;
+        }
       }
       oss << "</tr>" << std::endl;
       
-   }    
-   oss << "</table>" ;
-   oss << "</body></html>";
+    }    
+    oss << "</table>" ;
+    oss << "</body></html>";
 
 
-   response->SetBody( oss.str().c_str(), oss.str().length() );
+    response->SetBody( oss.str().c_str(), oss.str().length() );
 
-
-   return HTTPRESPONSECODE_200_OK;
-
+    return HTTPRESPONSECODE_200_OK;
+  }
 }
 
 
-
 //==============================================================================
+// XrdEHS
 //==============================================================================
-//==============================================================================
-
 
 XrdEHS::~XrdEHS()
 {}
@@ -176,9 +175,8 @@ XrdEHS::~XrdEHS()
 void XrdEHS::_init()
 {}
 
-XrdEHS::XrdEHS(XrdMonSucker* x) :
-   ZNameMap("EHS ", "EHS test"),
-   mXrdReporter(x),
+XrdEHS::XrdEHS(const Text_t* n, const Text_t* t) :
+   ZNameMap(n, t),
    mPort(9999),
    bServerUp(false)
 {
@@ -186,16 +184,17 @@ XrdEHS::XrdEHS(XrdMonSucker* x) :
 }
 
 //==============================================================================
+
 void XrdEHS::StartServer()
 {
-   std::cerr << "call StartServer" << endl;
+  static const Exc_t _eh("XrdEHS::StartServer ");
 
-  const TString _eh("XrdEHS::StartServer ");
-  
-  if ( bServerUp)
+  assert_xrdreporter(_eh);
+
+  if (bServerUp)
     throw _eh + "server already running.";
-  
-  FormTester srv(mXrdReporter);
+
+  FormTester srv(*mXrdReporter);
   EHSServerParameters oSP;
 
   oSP["port"] = mPort; // argv [ 1 ];
@@ -211,7 +210,7 @@ void XrdEHS::StartServer()
   
   SetServerUp(true);
   b_stop_server = false;
-  while (b_stop_server == false )
+  while (b_stop_server == false)
   {
     srv.HandleData(1000); // waits for 1 second
   }
@@ -222,10 +221,10 @@ void XrdEHS::StartServer()
 
 void XrdEHS::StopServer()
 {
-  const TString _eh("XrdEHS::StopServer ");
+  static const Exc_t _eh("XrdEHS::StopServer ");
 
   if ( ! bServerUp)
     throw _eh + "server not running.";
 
-    b_stop_server = true; 
+  b_stop_server = true; 
 }
