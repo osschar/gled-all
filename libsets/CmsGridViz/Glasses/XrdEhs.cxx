8dfaaace3242312c962aad964668a79f3c3d3e31
Matevž Tadel <osschar@users.noreply.github.com>
2011-11-05 00:23:55 +0000

    EHS was locking up, use home-cooked "web server".
    	* Glasses/XrdEhs.cxx:
    	Use direct sockets and just dump the table.
    	The request is not even read.
    
    	* Makefile:
    	Remove dependency on EHS libset.

--- libsets/CmsGridViz/Glasses/XrdEhs.cxx
+++ libsets/CmsGridViz/Glasses/XrdEhs.cxx
@@ -8,16 +8,15 @@
 #include "XrdMonSucker.h"
 #include "XrdEhs.c7"
 
-#include "TSystem.h"
 #include <Glasses/ZHashList.h>
 #include <Gled/GTime.h>
-#include <ehs/ehs.h>
-#include <ehs/ehstypes.h>
-
 
 #include "XrdFile.h"
 #include "XrdUser.h"
 #include "XrdServer.h"
+
+#include "TSystem.h"
+#include "TServerSocket.h"
 
 #include <iostream>
 #include <sstream>
@@ -25,152 +24,21 @@
 
 #include <cstdlib>
 
+// XrdEhs
+
+//______________________________________________________________________________
+// Status reporting embedded http server.
+//
+// This used EHS once and supported ?mode=all|files|help but it locked itself
+// up regularly and so it got dumped.
+//
+// TODO:
+// - Needs proper thread cancellation.
+// - Read the request.
+// - Would be nice to return proper header, beyond 200 OK.
 
 ClassImp(XrdEhs);
 
-//==============================================================================
-// EHS helper
-//==============================================================================
-
-namespace
-{
-  class FormTester : public EHS
-  {
-  public:
-    FormTester (XrdMonSucker* x ) : mRef( x ) { }
-    FormTester () : mRef( 0 ) { m_oParams.size(); }
-
-    ResponseCode HandleRequest ( HttpRequest *, HttpResponse * );
-
-   const char* timeAgo(const GTime& iTime) const;
-
-    TimeStamp_t    mStamp;
-    list<XrdFile*> mList;
-    XrdMonSucker*  mRef;
-  };
-
-  ResponseCode FormTester::HandleRequest(HttpRequest* request, HttpResponse* response)
-  {
-    // Creates a page based on user input -- either displays data from
-    // form or presents a form for users to submit data.
-
-    enum EMode { kAll, kFileNames, kHelp };
-
-    EMode mode = kAll;
-
-    // std::cerr << request->Uri() << endl;
-
-    if (! request->FormValues().empty()) {
-      mode = kHelp;
-      if (request->FormValues("mode").m_sBody.length())
-      {
-        if ( request->FormValues("mode").m_sBody == "all")
-          mode = kAll;
-        else if ( request->FormValues("mode").m_sBody == "files")
-          mode = kFileNames;
-        else 
-          mode = kHelp;
-      }
-    }
-
-    ostringstream oss;
-
-    if ( mode == kHelp)
-    {
-      std::string name = Form("http://%s:%s/?mode=", gSystem->HostName(),  m_oParams["port"].GetCharString());
-
-
-      oss << "<b>HELP</b>, possible options are: <br><br> " << endl;
-      oss << "<a href=\"" << name << "files\">" << name << "files </a> <br>" << endl;
-      oss << "<a href=\"" << name << "all\">" << name << "all </a>";
-      response->SetBody( oss.str().c_str(), oss.str().length() );
-      return HTTPRESPONSECODE_200_OK;
-    }
-
-
-    ZHashList* hl = mRef->GetOpenFiles();
-  
-    if (mStamp != hl->GetTimeStamp() )
-    {
-      mList.clear();
-      hl->CopyListByGlass<XrdFile>(mList);
-      mStamp = hl->GetTimeStamp();
-    }
-
-    oss << "<html><body><head><title>Xrd open files ["<< mList.size() << "]</title></head>" << endl;
-
-    oss << "<meta http-equiv=\"refresh\" content=\"180\" />" << endl;
-
-    oss << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/sorttable.js \"> </script>"<< endl;
-    oss << ""<< endl;
-    oss << "<style type=\"text/css\">"<< endl;
-    oss << "th, td {"<< endl;
-    oss << "  padding: 3px !important;"<< endl;
-    oss << "}"<< endl;
-    oss << ""<< endl;
-    oss << "table.sortable thead {"<< endl;
-    oss << "    background-color:#eee;"<< endl;
-    oss << "    color:#666666; "<< endl;
-    oss << "    font-weight: bold;"<< endl;
-    oss << "    cursor: default; "<< endl;
-    oss << "}"<< endl;
-    oss << "</style>"<< endl;
-    oss << "<br>"<< endl;
-
-    oss << ""<< endl;
-    oss << "   <table class=\"sortable\">"<< endl;
-    oss << "  <tr>"<< endl;
-    oss << "    <th>File</th> " << endl;
-    if (mode == kAll) { 
-      oss << " <th>OpenAgo</th> <th>ServerDomain</th>  <th>ClientDomain</th>  <th>User</th>  <th>Read [MB]</th>  <th>UpdateAgo</th>"<< endl;
-    }
-    oss << "  </tr>"<< endl;
-
-
-    for (list<XrdFile*>::iterator xfi = mList.begin(); xfi != mList.end(); ++xfi)
-    {
-      oss << "<tr>"<< endl;
-      oss << Form("<td>%s</td>", (*xfi)->GetName()) << endl;
-      if (mode == kAll)
-      {
-        oss << timeAgo((*xfi)->RefOpenTime()) << endl;
-        oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetServer()->GetDomain()) << endl;
-        oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetFromDomain()) << endl;
-        oss << Form("<td>%s</td>", (*xfi)->GetUser()->GetRealName()) << endl;
-
-        oss << Form("<td>%.3f</td>", (*xfi)->GetReadStats().GetSumX()) << endl;
-        oss << timeAgo((*xfi)->RefLastMsgTime()) << endl;
-      }
-      oss << "</tr>" << std::endl;
-      
-    }    
-    oss << "</table>" ;
-    oss << "</body></html>";
-
-
-    response->SetBody( oss.str().c_str(), oss.str().length() );
-
-    return HTTPRESPONSECODE_200_OK;
-  }
-
-const char* FormTester::timeAgo(const GTime& iTime) const
-{
-   if (iTime.IsNever())
-        return "<td>Never</td>\n";
-
-   int sinceUp =  (int) ((GTime::Now() - iTime).GetSec());
-   int hours = (sinceUp)/3600;
-   int min = (sinceUp - hours*3600)/60;
-   int sec = sinceUp - hours*3600 - min * 60;
-   return Form("<td>%02d:%02d:%02d</td>",hours, min, sec);    
-}
-
-}
-
-
-//==============================================================================
-// XrdEhs
-//==============================================================================
 
 XrdEhs::~XrdEhs()
 {}
@@ -197,29 +65,101 @@ void XrdEhs::StartServer()
   if (bServerUp)
     throw _eh + "server already running.";
 
-  FormTester srv(*mXrdSucker);
-  EHSServerParameters oSP;
+  TServerSocket serv_sock(mPort);
+  GSelector     selector;
+  selector.fRead.Add(&serv_sock);
 
-  oSP["port"] = mPort; // argv [ 1 ];
-  oSP["mode"] = "singlethreaded"; 
-  // oSP["mode"] = "threadpool";
-  // oSP["threadcount"] = 1; // 1 is default anyway
+  // char request[8192];
+  // char erroret[] = "Error processing your request.\n";
 
-  // oSP["https"] = 1;
-  // oSP["certificate"] = argv [ 2 ];
-  // oSP["passphrase"] = argv [ 3 ];
-                
-  srv.StartServer(oSP);
-  
-  SetServerUp(true);
-  b_stop_server = false;
-  while (b_stop_server == false)
+  TimeStamp_t    stamp = 0;
+  list<XrdFile*> flist;
+  TString        data;
+
+  while (! b_stop_server)
   {
-    srv.HandleData(1000); // waits for 1 second
-  }
+    selector.Select();
+    TSocket *sock = serv_sock.Accept();
 
-  srv.StopServer();
-  SetServerUp(false);
+    if (sock == 0)
+    {
+      ISerr(_eh + "Accept failed, retrying ...");
+      continue;
+    }
+
+    // sock->SetOption(kNoBlock, 1);
+    // Int_t len = sock->RecvRaw(request, 8192);
+
+    ZHashList* hl = mXrdSucker->GetOpenFiles();
+    if (stamp != hl->GetTimeStamp())
+    {
+      flist.clear();
+      stamp = hl->CopyListByGlass<XrdFile>(flist);
+    }
+
+    GTime now(GTime::I_Now);
+
+    ostringstream oss;
+
+    oss << "HTTP/1.0 200 OK\n\n";
+
+    oss << "<html><body><head><title>Xrd open files ["<< flist.size() << "]</title></head>" << endl;
+
+    oss << "<meta http-equiv=\"refresh\" content=\"180\" />" << endl;
+
+    oss << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/sorttable.js \"> </script>"<< endl;
+    oss << ""<< endl;
+    oss << "<style type=\"text/css\">"<< endl;
+    oss << "th, td {"<< endl;
+    oss << "  padding: 3px !important;"<< endl;
+    oss << "}"<< endl;
+    oss << ""<< endl;
+    oss << "table.sortable thead {"<< endl;
+    oss << "    background-color:#eee;"<< endl;
+    oss << "    color:#666666; "<< endl;
+    oss << "    font-weight: bold;"<< endl;
+    oss << "    cursor: default; "<< endl;
+    oss << "}"<< endl;
+    oss << "</style>"<< endl;
+    oss << "<br>"<< endl;
+
+    oss << ""<< endl;
+    oss << "   <table class=\"sortable\">"<< endl;
+    oss << "  <tr>"<< endl;
+    oss << "    <th>File</th> " << endl;
+    // if (mode == kAll)
+    { 
+      oss << " <th>OpenAgo</th> <th>ServerDomain</th> <th>ClientDomain</th> <th>User</th> <th>Read [MB]</th> <th>UpdateAgo</th>"<< endl;
+    }
+    oss << "  </tr>"<< endl;
+
+    for (list<XrdFile*>::iterator xfi = flist.begin(); xfi != flist.end(); ++xfi)
+    {
+      XrdFile *file = *xfi;
+
+      oss << "<tr>"<< endl;
+      oss << Form("<td>%s</td>", file->GetName()) << endl;
+      // if (mode == kAll)
+      {
+        oss << "<td>" << (now - file->RefOpenTime()).ToHourMinSec() << "</td>" << endl;
+        oss << Form("<td>%s</td>", file->GetUser()->GetServer()->GetDomain()) << endl;
+        oss << Form("<td>%s</td>", file->GetUser()->GetFromDomain()) << endl;
+        oss << Form("<td>%s</td>", file->GetUser()->GetRealName()) << endl;
+
+        oss << Form("<td>%.3f</td>", file->GetReadStats().GetSumX()) << endl;
+        oss << "<td>" << (now - file->RefLastMsgTime()).ToHourMinSec() << "</td>" << endl;
+      }
+      oss << "</tr>" << std::endl;
+      
+    }    
+    oss << "</table>" ;
+    oss << "</body></html>";
+
+    sock->SendRaw(oss.str().c_str(), oss.str().length());
+
+    sock->Close();
+    delete sock;
+  }
 }
 
 void XrdEhs::StopServer()
@@ -229,5 +169,6 @@ void XrdEhs::StopServer()
   if ( ! bServerUp)
     throw _eh + "server not running.";
 
-  b_stop_server = true; 
+  b_stop_server = true;
+  // should know thread and kill it
 }
