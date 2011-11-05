d671301cd1904c90704eb05d9548f099d087ff64
Matevž Tadel <osschar@users.noreply.github.com>
2011-11-05 04:28:46 +0000

    Improve response header a bit.

--- libsets/CmsGridViz/Glasses/XrdEhs.cxx
+++ libsets/CmsGridViz/Glasses/XrdEhs.cxx
@@ -101,60 +101,67 @@ void XrdEhs::StartServer()
 
     ostringstream oss;
 
-    oss << "HTTP/1.0 200 OK\n\n";
-
     oss << "<html><body><head><title>Xrd open files ["<< flist.size() << "]</title></head>" << endl;
 
     oss << "<meta http-equiv=\"refresh\" content=\"180\" />" << endl;
 
-    oss << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/sorttable.js \"> </script>"<< endl;
-    oss << ""<< endl;
-    oss << "<style type=\"text/css\">"<< endl;
-    oss << "th, td {"<< endl;
-    oss << "  padding: 3px !important;"<< endl;
-    oss << "}"<< endl;
-    oss << ""<< endl;
-    oss << "table.sortable thead {"<< endl;
-    oss << "    background-color:#eee;"<< endl;
-    oss << "    color:#666666; "<< endl;
-    oss << "    font-weight: bold;"<< endl;
-    oss << "    cursor: default; "<< endl;
-    oss << "}"<< endl;
-    oss << "</style>"<< endl;
-    oss << "<br>"<< endl;
-
-    oss << ""<< endl;
-    oss << "   <table class=\"sortable\">"<< endl;
-    oss << "  <tr>"<< endl;
-    oss << "    <th>File</th> " << endl;
+    oss << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/sorttable.js \"> </script>" << endl;
+    oss << endl;
+    oss << "<style type=\"text/css\">" << endl;
+    oss << "th, td {" << endl;
+    oss << "  padding: 3px !important;" << endl;
+    oss << "}" << endl;
+    oss << endl;
+    oss << "table.sortable thead {" << endl;
+    oss << "    background-color:#eee;" << endl;
+    oss << "    color:#666666; " << endl;
+    oss << "    font-weight: bold;" << endl;
+    oss << "    cursor: default; " << endl;
+    oss << "}" << endl;
+    oss << "</style>" << endl;
+    oss << "<br>" << endl;
+    oss << endl;
+    oss << "<table class=\"sortable\">"<< endl;
+    oss << "<tr>"<< endl;
+    oss << "<th>File</th>";
     // if (mode == kAll)
     { 
-      oss << " <th>OpenAgo</th> <th>ServerDomain</th> <th>ClientDomain</th> <th>User</th> <th>Read [MB]</th> <th>UpdateAgo</th>"<< endl;
+      oss << " <th>OpenAgo</th> <th>ServerDomain</th> <th>ClientDomain</th> <th>User</th> <th>Read [MB]</th> <th>UpdateAgo</th>";
     }
-    oss << "  </tr>"<< endl;
+    oss << endl;
+    oss << "</tr>" << endl;
 
     for (list<XrdFile*>::iterator xfi = flist.begin(); xfi != flist.end(); ++xfi)
     {
       XrdFile *file = *xfi;
-
       oss << "<tr>"<< endl;
       oss << Form("<td>%s</td>", file->GetName()) << endl;
       // if (mode == kAll)
       {
         oss << "<td>" << (now - file->RefOpenTime()).ToHourMinSec() << "</td>" << endl;
-        oss << Form("<td>%s</td>", file->GetUser()->GetServer()->GetDomain()) << endl;
-        oss << Form("<td>%s</td>", file->GetUser()->GetFromDomain()) << endl;
-        oss << Form("<td>%s</td>", file->GetUser()->GetRealName()) << endl;
+        oss << "<td>" << file->GetUser()->GetServer()->GetDomain()  << "</td>" << endl;
+        oss << "<td>" << file->GetUser()->GetFromDomain()           << "</td>" << endl;
+        oss << "<td>" << file->GetUser()->GetRealName()             << "</td>" << endl;
 
-        oss << Form("<td>%.3f</td>", file->GetReadStats().GetSumX()) << endl;
+        oss << "<td>" << GForm("%.3f", file->GetReadStats().GetSumX()) << "</td>" << endl;
         oss << "<td>" << (now - file->RefLastMsgTime()).ToHourMinSec() << "</td>" << endl;
       }
-      oss << "</tr>" << std::endl;
-      
+      oss << "</tr>" << endl;
     }    
-    oss << "</table>" ;
-    oss << "</body></html>";
+    oss << "</table>" << endl;
+    oss << "</body>"  << endl;
+    oss << "</html>"  << endl;
 
+    ostringstream hdr;
+
+    hdr << "HTTP/1.1 200 OK" << endl;
+    hdr << "Date: " << now.ToAscUTC() << endl;
+    hdr << "Connection: close" << endl;
+    hdr << "Content-Type: text/html" << endl;
+    hdr << "Content-Length: " << oss.str().length() << endl;
+    hdr << endl;
+
+    sock->SendRaw(hdr.str().c_str(), hdr.str().length());
     sock->SendRaw(oss.str().c_str(), oss.str().length());
 
     sock->Close();
