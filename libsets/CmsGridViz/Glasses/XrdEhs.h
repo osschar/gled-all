82e8368376c2805b45cee910a7a85d8b7da7ce4b
Matevž Tadel <osschar@users.noreply.github.com>
2011-11-07 06:29:41 +0000

    Fix response header, serve response in a detached thread.
    	* Glasses/XrdEhs.cxx:
    	* Glasses/XrdEhs.h:
    	Serve a page in a detached thread (still doesn't read request).
    	Sleep for a second after sending (get ack would be better).
    	Use proper date format, usr crlf line ends in header.

--- libsets/CmsGridViz/Glasses/XrdEhs.h
+++ libsets/CmsGridViz/Glasses/XrdEhs.h
@@ -8,9 +8,13 @@
 #define CmsGridViz_XrdEhs_H
 
 #include <Glasses/ZNameMap.h>
-
+#include <Gled/GTime.h>
 
 class XrdMonSucker;
+class XrdFile;
+
+class TSocket;
+
 
 class XrdEhs : public ZNameMap
 {
@@ -24,12 +28,22 @@ protected:
    Int_t	        mPort;         // X{GS} 7 Value(-range=>[1,65535,1])
    Bool_t	        bServerUp;     // X{GS} 7 BoolOut()
 
+   list<XrdFile*>       mFileList;     //!
+   TimeStamp_t          mFileListTS;   //!
+   GMutex               mServeMutex;   //!
+   GTime                mServeTime;    //!
+   TString              mServeContent; //!
+
+   void fill_content(const GTime& req_time, TString& content);
+
 public:
    XrdEhs(const Text_t* n="XrdEhs", const Text_t* t=0);
    virtual ~XrdEhs();
 
    void StartServer(); // X{Ed} 7 MButt(-join=>1)
    void StopServer();  // X{E}  7 MButt()
+
+   void ServePage(TSocket* sock);
 
 #include "XrdEhs.h7"
    ClassDef(XrdEhs, 1);
