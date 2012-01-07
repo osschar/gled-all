7616e6a1281b6a009e8392685d686cd4a0d856f2
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2012-01-07 03:45:14 +0000

    Proto-verison of UDP packet processor and its client.
    	* Glasses/UdpPacketProcessor.cxx:
    	* Glasses/UdpPacketProcessor.h:
    	New glass - listen for UDP packets and forward them to connected
    	clients over TCP, streamed as SUdpPacket.
    
    	* Glasses/UdpPacketTcpClient.cxx:
    	* Glasses/UdpPacketTcpClient.h:
    	New glass - TCP client for UdpPacketProcessor. Can deliver packets
    	to several local consumers using GQueueSet.
    
    	* glass.list:
    	Register UdpPacketProcessor and UdpPacketTcpClient.

---
+++ trunk/libsets/CmsGridViz/Glasses/UdpPacketProcessor.h
@@ -0,0 +1,78 @@
+// $Id$
+
+// Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef CmsGridViz_UdpPacketProcessor_H
+#define CmsGridViz_UdpPacketProcessor_H
+
+#include "Glasses/ZList.h"
+
+#include "Gled/GQueue.h"
+#include "Gled/GSelector.h"
+#include "Gled/GTime.h"
+
+class SUdpPacket;
+
+class TServerSocket;
+class TFile; class TTree; class TBranch;
+
+
+class UdpPacketProcessor : public ZList
+{
+  MAC_RNR_FRIENDS(UdpPacketProcessor);
+
+private:
+  void _init();
+
+protected:
+  Int_t             mSuckPort;     // X{GS} 7 Value()
+  Int_t             mSocket;       //!
+  GThread          *mSuckerThread; //!
+
+  GQueue<SUdpPacket> mUdpQueue;
+
+  static void* tl_Suck(UdpPacketProcessor* s);
+  void Suck();
+
+  //--------------------------------
+
+  GSelector         mSelector;   //!
+  GThread          *mServThread; //!
+  TServerSocket    *mServSocket; //!
+  Int_t             mServPort;   // X{GS} 7 Value()
+  list<TSocket*>    mClients;
+
+  static void* tl_Serve(UdpPacketProcessor* s);
+  void Serve();
+
+  void AddClient(TSocket* cs);
+  void RemoveClient(TSocket* cs);
+  void RemoveClient(list<TSocket*>::iterator sli);
+
+  //--------------------------------
+
+  GThread          *mDeliThread;   //!
+
+  static void* tl_Deliver(UdpPacketProcessor* s);
+  void Deliver();
+
+  //--------------------------------
+
+  TFile            *mDFile;   //!
+  TTree            *mDTree;   //!
+  TBranch          *mDBranch; //!
+
+public:
+  UdpPacketProcessor(const Text_t* n="UdpPacketProcessor", const Text_t* t=0);
+  virtual ~UdpPacketProcessor();
+
+  void StartAllServices(); // X{Ed} 7 MButt()
+  void StopAllServices();  // X{Ed} 7 MButt()
+
+#include "UdpPacketProcessor.h7"
+  ClassDef(UdpPacketProcessor, 1);
+}; // endclass UdpPacketProcessor
+
+#endif
---
+++ trunk/libsets/CmsGridViz/Glasses/UdpPacketTcpClient.h
@@ -0,0 +1,55 @@
+// $Id$
+
+// Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
+// This file is part of GLED, released under GNU General Public License version 2.
+// For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#ifndef CmsGridViz_UdpPacketTcpClient_H
+#define CmsGridViz_UdpPacketTcpClient_H
+
+#include "Glasses/ZGlass.h"
+#include "Gled/GQueue.h"
+
+class GThread;
+class SUdpPacket;
+
+class TSocket;
+
+
+class UdpPacketTcpClient : public ZGlass
+{
+  MAC_RNR_FRIENDS(UdpPacketTcpClient);
+  friend class UdpPacketProcessor;
+
+private:
+  void _init();
+
+protected:
+  TString           mHost; // X{GS} 7 Textor()
+  UShort_t          mPort; // X{GS} 7 Value(-range=>[0,65535,1])
+
+  TSocket          *mSocket;         //!
+  GThread          *mListenerThread; //!
+
+  typedef GQueue<SUdpPacket>    Queue_t;
+  typedef GQueueSet<SUdpPacket> QueueSet_t;
+
+  QueueSet_t        mConsumerSet;
+
+  static void* tl_ListenLoop(UdpPacketTcpClient* c);
+  void ListenLoop();
+
+public:
+  UdpPacketTcpClient(const Text_t* n="UdpPacketTcpClient", const Text_t* t=0);
+  virtual ~UdpPacketTcpClient();
+
+  void ConnectAndListenLoop(); // X{Ed} 7 MButt()
+
+  void RegisterConsumer  (Queue_t* q);
+  void UnregisterConsumer(Queue_t* q);
+
+#include "UdpPacketTcpClient.h7"
+  ClassDef(UdpPacketTcpClient, 1);
+}; // endclass UdpPacketTcpClient
+
+#endif
