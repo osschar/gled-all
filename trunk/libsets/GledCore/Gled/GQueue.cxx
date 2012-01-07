4a381aec2e7e7133aaf90aac9c6de3a4fc871890
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2012-01-07 03:41:19 +0000

    Implement class GQueueSet.

--- trunk/libsets/GledCore/Gled/GQueue.cxx
+++ trunk/libsets/GledCore/Gled/GQueue.cxx
@@ -3,3 +3,5 @@
 // Copyright (C) 1999-2008, Matevz Tadel. All rights reserved.
 // This file is part of GLED, released under GNU General Public License version 2.
 // For the licensing terms see $GLEDSYS/LICENSE or http://www.gnu.org/.
+
+#include "GQueue.h"
