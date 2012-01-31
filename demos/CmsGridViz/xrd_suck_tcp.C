ac618593032105ba6223abefeae9863cba511aab
Matevž Tadel <osschar@users.noreply.github.com>
2012-01-31 07:18:23 +0000

    Update for changes in UDP processing. Some macros have still need to be converted.
    	* xrd_suck_tcp.C:
    	New demo - do almost everything as the full sucker but get data
    	from a udp -> tcp repeater.
    
    	* xrd_udp_forwarder.C:
    	Update to new class names etc.

---
+++ demos/CmsGridViz/xrd_suck_tcp.C
@@ -0,0 +1,90 @@
+// Connects to server serving xrdmon UDP traffic via TCP.
+// Processes this info with XrdMonSucker + runs file-close reporter
+// and embedded http server showing opened files.
+//
+// Could really have several TCP (or, also, UDP) sources, e.g. one
+// for each big site where we are running. Motivations:
+// 1. UDP packets are the first to be dropped by routers during congestion.
+//    I've seen this already, mostly from MIT and Wisonsin.
+// 2. To compactify and compress the streams, sending payload once per
+//    x seconds, or so.
+
+#include <glass_defines.h>
+
+class UdpPacketTcpClient;
+class XrdMonSucker;
+class XrdFileCloseReporter;
+class XrdEhs;
+
+ZLog                 *c_log  = 0;
+UdpPacketTcpClient   *c_upc  = 0;
+XrdMonSucker         *c_suck = 0;
+XrdFileCloseReporter *c_frep = 0;
+XrdEhs               *c_ehs  = 0;
+
+void xrd_suck_tcp(bool gui_p=false)
+{
+  Gled::AssertMacro("sun_demos.C");
+  Gled::theOne->AssertLibSet("CmsGridViz");
+
+  {
+    ZList* l = g_fire_queen;
+    l = l->AssertPath(NestInfo::sLayoutPath, "ZNameMap");
+    l = l->AssertPath("XrdStuff", "ZList");
+    l->Swallow(new ZGlass("XrdUser", "XrdUser(FromHost,FromDomain)"));
+  }
+
+  g_queen->SetName("XrdMonitorQueen");
+
+  ASSIGN_ADD_GLASS(c_log, ZLog, g_queen, "XrdMonSucker Log", 0);
+  // c_log->SetFileName("/var/log/xrootd/xrd-mon-sucker.log");
+  c_log->SetFileName("test_via_upp.log");
+  c_log->SetLevel(ZLog::L_Info);
+
+  ASSIGN_ADD_GLASS(c_upc, UdpPacketTcpClient, g_queen, "UdpPacketTcpClient", 0);
+  c_upc->SetLog(c_log);
+  c_upc->SetHost("desire.physics.ucsd.edu");
+  // default port is 9940
+
+  ASSIGN_ADD_GLASS(c_suck, XrdMonSucker, g_queen, "XrdMonSucker", 0);
+  c_suck->SetKeepSorted(true);
+  c_suck->SetLog(c_log);
+  c_suck->SetSource(c_upc);
+
+  ASSIGN_ADD_GLASS(c_frep, XrdFileCloseReporter, g_queen, "XrdFileCloseReporter", 0);
+  // c_frep->SetUdpHost("desire.physics.ucsd.edu");
+  // c_frep->SetUdpPort(7632);
+
+  c_suck->SetFCReporter(c_frep);
+
+  ASSIGN_ADD_GLASS(c_ehs, XrdEhs, g_queen, "XrdEhs", 0);
+  c_ehs->SetXrdSucker(c_suck);
+  c_ehs->SetPort(4242);
+
+
+  //============================================================================
+  // Spawn GUI
+
+  if (gui_p)
+  {
+    Gled::LoadMacro("eye.C");
+    eye(false);
+
+    g_nest->Add(g_queen);
+    g_nest->SetWName(50);
+  }
+
+  // Regexps for setting full-trace-print flag for new user sessions.
+  c_suck->SetTraceDN("Matevz Tadel");
+  c_suck->SetTraceHost("uaf-");
+  c_suck->SetTraceDomain("ucsd.edu");
+
+  c_log->StartLogging();
+
+  c_upc->ConnectAndListenLoop();
+
+  c_suck->StartSucker();
+  //c_frep->StartReporter();
+
+  g_saturn->ShootMIR( c_ehs->S_StartServer() );
+}
