2a6789eb4e6c1f0cc9437cd4568f977e2315b491
Matevž Tadel <osschar@users.noreply.github.com>
2012-02-24 01:12:24 +0000

    Canonicalize macros, scripts.
    	* xrd_suck_production.C:
    	Removed file - replaced by xrd_suck_udp.C.
    
    	* xrd_suck_udp.C:
    	New file - read directly from UDP (via UdpPacketListener).
    
    	* xrd_suck_tcp.C:
    	Set standard names, options.
    
    	* install-xrd-mon-sucker.sh:
    	Install .C files into /opt/gled, not /etc/xrootd/.
    
    	* xrd-mon-sucker.sh:
    	Use while true loop and send mail on failure.

--- demos/CmsGridViz/install-xrd-mon-sucker.sh
+++ demos/CmsGridViz/install-xrd-mon-sucker.sh
@@ -2,4 +2,4 @@
 
 cp xrd-mon-sucker.sh /opt/gled
 cp logrotate.d/xrd-mon-sucker /etc/logrotate.d
-cp xrd_suck_production.C /etc/xrootd
+cp xrd_*.C /opt/gled
--- demos/CmsGridViz/xrd_suck_udp.C
+++ demos/CmsGridViz/xrd_suck_udp.C
@@ -1,15 +1,24 @@
+// Listens directly for xrdmon UDP traffic.
+// Processes this info with XrdMonSucker + runs file-close reporter
+// and embedded http server showing opened files.
+//
+// Consider running UDP -> TCP server, maybe one at every big site.
+// See xrd_udp_forwarder.C and xrd_suck_tcp.C.
+
 #include <glass_defines.h>
 
+class UdpPacketListener;
 class XrdMonSucker;
 class XrdFileCloseReporter;
 class XrdEhs;
 
 ZLog                 *c_log  = 0;
+UdpPacketListener    *c_listener = 0;
 XrdMonSucker         *c_suck = 0;
 XrdFileCloseReporter *c_frep = 0;
 XrdEhs               *c_ehs  = 0;
 
-void xrd_suck_production()
+void xrd_suck_udp()
 {
   Gled::AssertMacro("sun_demos.C");
   Gled::theOne->AssertLibSet("CmsGridViz");
@@ -23,22 +32,29 @@ void xrd_suck_production()
 
   g_queen->SetName("XrdMonitorQueen");
 
+  ASSIGN_ADD_GLASS(c_log, ZLog, g_queen, "XrdMonSucker Log", 0);
+  c_log->SetFileName("/var/log/xrootd/xrd-mon-sucker.log");
+  // c_log->SetFileName("test_via_upp.log");
+  c_log->SetLevel(ZLog::L_Info);
+
+  ASSIGN_ADD_GLASS(c_listener, UdpPacketListener, g_queen, "UdpPacketListener", "");
+  c_listener->SetLog(c_log);
+  // c_listener->SetSuckPort(9930);
+
   ASSIGN_ADD_GLASS(c_suck, XrdMonSucker, g_queen, "XrdMonSucker", 0);
   c_suck->SetKeepSorted(true);
+  c_suck->SetLog(c_log);
+  c_suck->SetSource(c_listener);
 
   ASSIGN_ADD_GLASS(c_frep, XrdFileCloseReporter, g_queen, "XrdFileCloseReporter", 0);
-  c_frep->SetUdpHost("localhost");
-  c_frep->SetUdpPort(3334);
+  // c_frep->SetUdpHost("localhost");
+  // c_frep->SetUdpPort(4242);
+
   c_suck->SetFCReporter(c_frep);
 
   ASSIGN_ADD_GLASS(c_ehs, XrdEhs, g_queen, "XrdEhs", 0);
   c_ehs->SetXrdSucker(c_suck);
-  c_ehs->SetPort(4242);
-
-  ASSIGN_ADD_GLASS(c_log, ZLog, g_queen, "XrdMonSucker Log", 0);
-  c_log->SetFileName("/var/log/xrootd/xrd-mon-sucker.log");
-  c_log->SetLevel(ZLog::L_Info);
-  c_suck->SetLog(c_log);
+  // c_ehs->SetPort(4242);
 
 
   //============================================================================
@@ -50,7 +66,6 @@ void xrd_suck_production()
     eye(false);
 
     g_nest->Add(g_queen);
-    // g_nest->SetMaxChildExp(3); // This only works on creation.
     g_nest->SetWName(50);
   }
 
@@ -60,7 +75,10 @@ void xrd_suck_production()
   c_suck->SetTraceDomain("ucsd.edu");
 
   c_log->StartLogging();
+
   c_suck->StartSucker();
   c_frep->StartReporter();
+
+  g_saturn->ShootMIR( c_listener->S_StartAllServices() );
   g_saturn->ShootMIR( c_ehs->S_StartServer() );
 }
