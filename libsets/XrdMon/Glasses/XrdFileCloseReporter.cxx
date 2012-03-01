5cf2db599e9c669a6b75b374741cf661d2cb2fff
Matevž Tadel <osschar@users.noreply.github.com>
2012-03-01 05:37:41 +0000

    Add processing of appinfo messages.
    	* Glasses/XrdFileCloseReporter.cxx:
    	Report app_info at file close.
    
    	* Glasses/XrdMonSucker.cxx:
    	Process 'i' messages and stick the text into the right XrdUser.
    
    	* Glasses/XrdUser.cxx:
    	* Glasses/XrdUser.h:
    	Add member TString mAppInfo and method AppendAppInfo().

--- libsets/XrdMon/Glasses/XrdFileCloseReporter.cxx
+++ libsets/XrdMon/Glasses/XrdFileCloseReporter.cxx
@@ -159,9 +159,9 @@ void XrdFileCloseReporter::ReportLoop()
       GLensReadHolder _ulck(user);
       msg += TString::Format
         ("user_dn=%s\nuser_vo=%s\nuser_role=%s\nuser_fqan=%s\nclient_domain=%s\nclient_host=%s\n"
-         "server_username=%s\n",
+         "server_username=%s\napp_info=%s\n",
          user->GetDN(), user->GetVO(), user->GetRole(), user->GetGroup(),
-         user->GetFromDomain(), user->GetFromHost(), user->GetServerUsername());
+         user->GetFromDomain(), user->GetFromHost(), user->GetServerUsername(), user->GetAppInfo());
       server = user->GetServer();
     }
     {
