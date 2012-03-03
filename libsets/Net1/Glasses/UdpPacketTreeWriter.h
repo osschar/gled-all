8167bc18cff29c423c33e0e6620fce91111e28ae
Matevž Tadel <osschar@users.noreply.github.com>
2012-03-03 04:10:13 +0000

    First step towards activation, still not runnable.

--- libsets/Net1/Glasses/UdpPacketTreeWriter.h
+++ libsets/Net1/Glasses/UdpPacketTreeWriter.h
@@ -7,9 +7,17 @@
 #ifndef Net1_UdpPacketTreeWriter_H
 #define Net1_UdpPacketTreeWriter_H
 
-#include <Glasses/ZGlass.h>
+#include "Glasses/ZGlass.h"
+#include "Gled/GQueue.h"
 
-class TFile; class TTree; class TBranch;
+class ZLog;
+class UdpPacketSource;
+class SUdpPacket;
+
+class TFile;
+class TTree;
+class TBranch;
+
 
 class UdpPacketTreeWriter : public ZGlass
 {
@@ -19,13 +27,26 @@ private:
   void _init();
 
 protected:
-  TFile            *mDFile;   //!
-  TTree            *mDTree;   //!
-  TBranch          *mDBranch; //!
+  ZLink<ZLog>             mLog;       // X{GS} L{}
+  ZLink<UdpPacketSource>  mSource;    // X{GS} L{}
+
+  GQueue<SUdpPacket>      mUdpQueue;  //!
+
+  GThread                *mWLThread;  //!
+
+  TFile                  *mFile;      //!
+  TTree                  *mTree;      //!
+  TBranch                *mBranch;    //!
+
+  // static void* tl_WriteLoop(UdpPacketTreeWriter* w);
+  // void WriteLoop();
 
 public:
   UdpPacketTreeWriter(const Text_t* n="UdpPacketTreeWriter", const Text_t* t=0);
   virtual ~UdpPacketTreeWriter();
+
+  void Start(); // X{Ed} 7 MButt()
+  void Stop();  // X{Ed} 7 MButt()
 
 #include "UdpPacketTreeWriter.h7"
   ClassDef(UdpPacketTreeWriter, 1);
