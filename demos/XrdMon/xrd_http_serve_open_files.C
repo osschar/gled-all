13e54c70c0c0b9e79b5d9315aa8091fdfa0fab38
Matevž Tadel <osschar@users.noreply.github.com>
2012-03-06 01:37:26 +0000

    Improve configurability by splitting up startup macros.
    	* xrd_suck_tcp.C:
    	* xrd_suck_udp.C:
    	* xrd_udp_forwarder.C:
    	Separate XrdFileCloseReporter, XrdEhs and UdpPacketTreeWriter into
    	separate macros.
    	Add new variable for UdpPacketSource.
    
    	* xrd_file_close_reporter.C:
    	* xrd_http_serve_open_files.C:
    	* xrd_tree_writer.C:
    	New macros for individual functions.

---
+++ demos/XrdMon/xrd_http_serve_open_files.C
@@ -0,0 +1,14 @@
+// Add-on macro (expects xrd_suck_tcp/udp.C to be executed before).
+// Runs XrdEhs to serve a table of currently open files.
+
+class XrdEhs;
+XrdEhs *c_ehs  = 0;
+
+void xrd_http_serve_open_files()
+{
+  ASSIGN_ADD_GLASS(c_ehs, XrdEhs, g_queen, "XrdEhs", 0);
+  c_ehs->SetXrdSucker(c_suck);
+  // c_ehs->SetPort(4242);
+
+  g_saturn->ShootMIR( c_ehs->S_StartServer() );
+}
