5f971c65ce317ccaf4ee6a2c5a69d34989ff98df
Matevž Tadel <osschar@users.noreply.github.com>
2012-06-09 03:22:26 +0000

    Intermediate commit, needs some more work.

--- libsets/Net1/Glasses/UdpPacketTreeWriter.h
+++ libsets/Net1/Glasses/UdpPacketTreeWriter.h
@@ -10,9 +10,11 @@
 #include "Glasses/ZGlass.h"
 #include "Gled/GQueue.h"
 
+
 class ZLog;
 class UdpPacketSource;
 class SUdpPacket;
+class GSignal;
 
 class TFile;
 class TTree;
@@ -30,27 +32,37 @@ protected:
   ZLink<ZLog>             mLog;        // X{GS} L{}
   ZLink<UdpPacketSource>  mSource;     // X{GS} L{}
 
+  Int_t                   mAutoSaveEntries;  // X{GS} 7 Value(-range=>[0, 1000000, 1])
+  Int_t                   mRotateMinutes;    // X{GS} 7 Value(-range=>[0, 14400, 1])
+  GTime                   mLastFileOpen;     //!
+
   GQueue<SUdpPacket>      mUdpQueue;   //!
+
+  Bool_t                  bRunning;    //! X{G} 7 BoolOut()
 
   GThread                *mWLThread;   //!
 
-  TString                 mFilePrefix; // X{GS} Textor()  
+  TString                 mFilePrefix; // X{GS} 7 Textor()  
   TFile                  *mFile;       //!
   TTree                  *mTree;       //!
   TBranch                *mBranch;     //!
 
-  // static void* tl_WriteLoop(UdpPacketTreeWriter* w);
-  // void WriteLoop();
-
   void open_file_create_tree();
   void write_tree_close_file();
+
+  static void* tl_WriteLoop(UdpPacketTreeWriter* w);
+  static void  cu_WriteLoop(UdpPacketTreeWriter* w);
+  static void  sh_WriteLoop(GSignal* s);
+  void WriteLoop();
 
 public:
   UdpPacketTreeWriter(const Text_t* n="UdpPacketTreeWriter", const Text_t* t=0);
   virtual ~UdpPacketTreeWriter();
 
-  void Start(); // X{Ed} 7 MButt()
-  void Stop();  // X{Ed} 7 MButt()
+  void Start();      // X{Ed} 7 MButt()
+  void Stop();       // X{Ed} 7 MButt()
+
+  void RotateTree(); // X{E}  7 MButt()
 
 #include "UdpPacketTreeWriter.h7"
   ClassDef(UdpPacketTreeWriter, 1);
