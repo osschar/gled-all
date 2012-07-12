1e103277cd8f5128a38d695e9b3caf06deed65b9
Matevž Tadel <osschar@users.noreply.github.com>
2012-07-12 22:59:49 +0000

    Fix one variable and one method name.

--- demos/XrdMon/xrd_file_close_reporter_amq.C
+++ demos/XrdMon/xrd_file_close_reporter_amq.C
@@ -4,20 +4,20 @@
 class XrdFileCloseReporter;
 class XrdFileCloseReporterAmq;
 
-XrdFileCloseReporter       *c_frep     = 0;
-XrdFileCloseReporterGratia *c_frep_amq = 0;
+XrdFileCloseReporter    *c_frep     = 0;
+XrdFileCloseReporterAmq *c_frep_amq = 0;
 
 void xrd_file_close_reporter_amq()
 {
   // Sends JSON report via ActiveMQ
   ASSIGN_ADD_GLASS(c_frep_amq, XrdFileCloseReporterAmq, g_queen, "XrdFileCloseReporterAmq", 0);
-  c_frep_gratia->SetLog(c_log);
+  c_frep_amq->SetLog(c_log);
 
   // c_frep_amq->SetAmqHost("gridmsg007.cern.ch");
   // c_frep_amq->SetAmqPort(6163);
   // c_frep_amq->SetAmqUser("xrdpop");
-  c_frep_amq->SetAmqPswd("");
-  // c_frep_amq->SetQueue("/topic/xrdpop.uscms_test_popularity");
+  // c_frep_amq->SetAmqPswd("xyzz");
+  // c_frep_amq->SetAmqTopic("/topic/xrdpop.uscms_test_popularity");
 
   c_suck->AddFileCloseReporter(c_frep_amq);
 
