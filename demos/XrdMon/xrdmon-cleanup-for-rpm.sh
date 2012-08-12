33f9d5cee97707f78ee7b266b6e425ac7280499f
Matevž Tadel <osschar@users.noreply.github.com>
2012-08-12 01:28:59 +0000

    First complete version.

--- demos/XrdMon/xrdmon-cleanup-for-rpm.sh
+++ demos/XrdMon/xrdmon-cleanup-for-rpm.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
 
-### Make sure we are in the right directory !!!! ###
+# Clean gled-builder install tree for packaging into a binary RPM.
+#
+# Needs to be run with current directory equal to install directory, e.g.,
+# assuming $installdir:
+#   cd $installdir
+#   gled/demos/XrdMon/xrdmon-cleanup-for-rpm.sh
+#
+# Note that we need to leave a significant part of ROOT development files to
+# allow for dictionary / streamer generation via aclic.
+
+
+### Make sure we are in the right directory ###
+
+if [[ -x gled/bin/gled-config && -x root/bin/root-config ]]
+then
+    echo "Cleaning directory '" `pwd` "' for RPM packaging."
+else
+    dir=`dirname $0`
+    topdir=`readlink -e $dir/../../..`
+    echo "$0 directoy '`pwd`' is invalid."
+    echo "  Expect to be run in top install directory, probably '$topdir'."
+    exit 1
+fi
+
+
+### Erase redundant directories and files ###
 
 rm -rf include/
-rm -rf lib/*.a
-rm -rf lib/*.la
+rm -f  lib/*.a
+rm -f  lib/*.la
 
 cd root/
-
-rm -rf root/lib/libTMVA.*
-rm -rf root/lib/libEve.*
-rm -rf root/lib/libRGL.*
-rm -rf root/lib/libGeom.*
-rm -rf root/lib/libSmatrix.*
-rm -rf root/lib/libGeomBuilder.*
-rm -rf root/lib/libProof.*
-rm -rf root/lib/libProofPlayer.*
 
 rm -rf test/
 rm -rf tutorials/
 
+rm -f lib/libTMVA.*
+rm -f lib/libEve.*
+rm -f lib/libRGL.*
+rm -f lib/libGeom*
+rm -f lib/libSmatrix.*
+rm -f lib/libProof*
+
+rm -rf include/TMVA/
+rm -rf include/GL/
+
 cd cint/cint/lib
-find . -name \*.o  | xargs rm
-find . -name G__\* | xargs rm
+find . -name \*.o  | xargs rm -f
+find . -name G__\* | xargs rm -f
