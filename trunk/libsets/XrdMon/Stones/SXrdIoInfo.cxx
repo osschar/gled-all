0ad878f775aeff4ba7315f5f463ba677061b10ee
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2012-11-06 03:16:33 +0000

    Converging on final implementation for storage of iov details.

--- trunk/libsets/XrdMon/Stones/SXrdIoInfo.cxx
+++ trunk/libsets/XrdMon/Stones/SXrdIoInfo.cxx
@@ -12,28 +12,97 @@
 //
 //
 
-ClassImp(SXrdIoInfo);
+ClassImp(SXrdIoInfoFiller);
 
 //==============================================================================
 
-void SXrdIoInfo::RegisterRead(Int_t time, Long64_t offset, Int_t length)
-{
+SXrdIoInfoFiller::SXrdIoInfoFiller(SXrdIoInfo& ioi) :
+  mIoI(ioi),
+  mExpectedReadVSegs(0),
+  mLastVSeq(0),
+  bInReadV(false)
+{}
 
+//==============================================================================
+
+void SXrdIoInfoFiller::begin_readv(Int_t n_segments, Int_t total_length, Int_t time, UChar_t vseq)
+{
+  // create record,
 }
 
-void SXrdIoInfo::RegisterWrite(Int_t time, Long64_t offset, Int_t length)
+void SXrdIoInfoFiller::extend_readv(Int_t n_segments, Int_t total_length)
 {
-
+  // add totals
 }
 
-void SXrdIoInfo::RegisterReadV(Int_t time, Int_t n_segments, Int_t total_length)
+void SXrdIoInfoFiller::end_readv()
 {
-
-}
-
-void SXrdIoInfo::RegisterReadVSeg(Long64_t offset, Int_t length)
-{
-
+  // commit missing segs, if any and reset
 }
 
 //==============================================================================
+
+void SXrdIoInfoFiller::RegisterRead(Long64_t offset, Int_t length, Int_t time)
+{
+  // error if mExpectedReadVSegs > 0 ... when I'm getting them
+  if (bInReadV) end_readv();
+  // XXXX Hmmh ... wouldn't this be valid readv record??? Argh, etc. But yes ... it should.
+
+  mIoI.mReqs.push_back(SXrdReq(offset, length, time));
+}
+
+void SXrdIoInfoFiller::RegisterWrite(Long64_t offset, Int_t length, Int_t time)
+{
+  // error if mExpectedReadVSegs > 0 ... when I'm getting them
+  if (bInReadV)
+  {
+    if (mExpectedReadVSegs)
+      end_readv();
+  }
+
+  // Support both length notations.
+  if (length > 0) length = -length;
+  mIoI.mReqs.push_back(SXrdReq(offset, length, time));
+}
+
+//------------------------------------------------------------------------------
+
+void SXrdIoInfoFiller::RegisterReadV(Int_t n_segments, Int_t total_length, Int_t time, UChar_t vseq)
+{
+  // ReadV record, unpacked info is not following.
+  // Additional records with the same vseq are possible.
+}
+
+void SXrdIoInfoFiller::RegisterReadU(Int_t n_segments, Int_t total_length, Int_t time, UChar_t vseq)
+{
+  // ReadV record, unpacked info is about to follow.
+  // Additional records with the same vseq are possible but only after individual reads.
+
+  // Wait ... this guy could call RegsterReadV ... argh.
+
+  if (bInReadV)
+  {
+    if  (vseq == mLastVSeq)
+    {
+      // append to existing record
+    }
+    else
+    {
+      // error
+      end_readv();
+      // create new record
+      bInReadV = true;
+    }
+  }
+  else
+  {
+    mLastVSeq = vseq;
+  }
+
+  if (bInReadV) end_readv();
+}
+
+void SXrdIoInfoFiller::RegisterReadVSeg(Long64_t offset, Int_t length)
+{
+
+}
--- trunk/libsets/XrdMon/Stones/SXrdIoInfo.h
+++ trunk/libsets/XrdMon/Stones/SXrdIoInfo.h
@@ -8,6 +8,8 @@
 #define XrdMon_SXrdIoInfo_H
 
 #include "Rtypes.h"
+#include "TString.h"
+
 #include <vector>
 
 //==============================================================================
@@ -18,122 +20,100 @@ public:
   enum Req_e { R_Write, R_Read, R_VecRead };
 
 private:
-  Long64_t mOffset;
+  Long64_t mOffset;   // Overloaded for R_VecRead
   Int_t    mLength;
-  Int_t    mSeconds;
+  Int_t    mTime;
 
 public:
-  SXrdReq() : mOffset(0), mLength(0), mSeconds(0) {}
+  SXrdReq() : mOffset(0), mLength(0), mTime(0) {}
+  SXrdReq(Long64_t o, Int_t l, Int_t t) : mOffset(o), mLength(l), mTime(t) {}
+
+  // Filling functions
 
   void SetWrite()   {}
   void SetRead()    {}
   void SetVecRead() {}
   void AddVecRead() {}
 
+  // Access functions
+
   Req_e Type() const
   {
     if (mOffset < 0) return R_VecRead;
     if (mLength < 0) return R_Write; else return R_Read;
   }
-  Int_t  Length()  const { return mLength < 0 ? -mLength : mLength; }
-  Int_t  Seconds() const { return mSeconds; }
+  Int_t  Length() const { return mLength < 0 ? -mLength : mLength; }
+  Int_t  Time()   const { return mTime; }
 
   // For Read and Write
-  Long64_t Offset() const { return mOffset; }
+  Long64_t Offset() const { return mOffset < 0 ? -1 : mOffset; }
 
   // For VecRead
   Int_t    SubReqIndex() const { return mOffset & 0xffffffff; }
-  Short_t  SubReqLen()   const { return (mOffset > 32) & 0xffff; }
-  Short_t  SubReqsLost() const { return (mOffset > 48) & 0xffff; }
+  Short_t  SubReqCount() const { return (mOffset > 32) & 0xffff; }
+  Short_t  SubReqsLost() const { return (mOffset > 48) & 0x7fff; }
 
     ClassDefNV(SXrdReq, 1);
 }; // endclass SXrdReq
 
-//==============================================================================
+typedef std::vector<SXrdReq> vSXrdReq_t;
 
-class SXrdRwReq
-{
-public:
-  Long64_t          mOffset;
-  Int_t             mLength;
-  Int_t             mTime;
-
-  SXrdRwReq()  {}
-  ~SXrdRwReq() {}
-
-  ClassDefNV(SXrdRwReq, 1);
-}; // endclass SXrdRwReq
-
-typedef std::vector<SXrdRwReq> vSXrdRwReq_t;
-
-//==============================================================================
-
-class SXrdRvSubReq
-{
-public:
-  Long64_t          mOffset;
-  Int_t             mLength;
-
-  SXrdRvSubReq()  {}
-  ~SXrdRvSubReq() {}
-
-  ClassDefNV(SXrdRvSubReq, 1);
-}; // endclass SXrdRvSubReq
-
-typedef std::vector<SXrdRvSubReq> vSXrdRvSubReq_t;
-
-//==============================================================================
-
-class SXrdRvReq
-{
-public:
-  Int_t             mNSubReqs;
-  Int_t             mTotalLength;
-  Int_t             mTime;
-
-  vSXrdRvSubReq_t   mSubReqs;
-
-  SXrdRvReq()  {}
-  ~SXrdRvReq() {}
-
-  ClassDefNV(SXrdRvReq, 1);
-}; // endclass SXrdRvReq
-
-typedef std::vector<SXrdRvReq> vSXrdRvReq_t;
 
 //==============================================================================
 
 class SXrdIoInfo
 {
 public:
-  // Request info
-  std::vector<Int_t>    mTimeVec;
-  std::vector<Int_t>    mIndexVec; // Index into IO info
-  std::vector<Short_t>  mTypeVec;  // -2 write, -1 read, >= 0 N_segments of vector read
+  vSXrdReq_t            mReqs;
 
-  // IO info, there are several entries for vector reads.
-  std::vector<Long64_t> mOffsetVec; // First entry for readv is number of segments reported in initial record
+  // Details for unpacked vector reads.
+  std::vector<Long64_t> mOffsetVec;
   std::vector<Int_t>    mLengthVec;
 
-  UChar_t   mLastVSeq;          //! Should be in XrdFile?
-  Short_t   mExpectedReadVSegs; //! Hmmh ... how about this guy?
-
-  // Previous plan that couldn't replay time order
-  vSXrdRwReq_t      mWrites;
-  vSXrdRwReq_t      mReads;
-  vSXrdRvReq_t      mReadvs;
+  // ----------------------------------------------------------------
 
   SXrdIoInfo()  {}
   ~SXrdIoInfo() {}
 
-  void RegisterRead(Int_t time, Long64_t offset, Int_t length);
-  void RegisterWrite(Int_t time, Long64_t offset, Int_t length);
-
-  void RegisterReadV(Int_t time, Int_t n_segments, Int_t total_length);
-  void ExtendReadV  (Int_t n_segments, Int_t total_length); // Happens for multi-file ReadV
-  void RegisterReadVSeg(Long64_t offset, Int_t length);
 
   ClassDefNV(SXrdIoInfo, 1);
 }; // endclass SXrdIoInfo
+
+typedef std::vector<SXrdIoInfo> vSXrdIoInfo_t;
+
+
+//==============================================================================
+
+class SXrdIoInfoFiller
+{
+  SXrdIoInfo   &mIoI;
+
+  // State variables
+  Short_t       mExpectedReadVSegs;
+  UChar_t       mLastVSeq;
+  Bool_t        bInReadV;
+
+  // These two should go to IoInfo
+  TString       mErrors;
+  Int_t         mNErrors;
+
+  void begin_readv(Int_t n_segments, Int_t total_length, Int_t time, UChar_t vseq);
+  void extend_readv(Int_t n_segments, Int_t total_length);
+  void end_readv();
+
+public:
+  SXrdIoInfoFiller(SXrdIoInfo& ioi);
+
+  void RegisterRead(Long64_t offset, Int_t length, Int_t time);
+  void RegisterWrite(Long64_t offset, Int_t length, Int_t time);
+
+  void RegisterReadV(Int_t n_segments, Int_t total_length, Int_t time, UChar_t vseq);
+  void RegisterReadU(Int_t n_segments, Int_t total_length, Int_t time, UChar_t vseq);
+  void RegisterReadVSeg(Long64_t offset, Int_t length);
+
+  void FillingComplete();
+
+  ClassDefNV(SXrdIoInfoFiller, 0);
+};
 
 #endif
