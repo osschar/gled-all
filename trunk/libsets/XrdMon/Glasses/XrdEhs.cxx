30816036f24d57825b051c4474f36219e4f6d315
matevz <matevz@cc1feb4c-3097-4a1b-bcff-9df761f59f75>
2013-06-06 17:15:46 +0000

    Remove obsolete member mServeTime.

--- trunk/libsets/XrdMon/Glasses/XrdEhs.cxx
+++ trunk/libsets/XrdMon/Glasses/XrdEhs.cxx
@@ -65,205 +65,202 @@ void XrdEhs::fill_content(const GTime& req_time, TString& content, lStr_t& path,
 {
   GMutexHolder _lck(mServeMutex);
 
-  if ((req_time - mServeTime) > GTime(1, 0))
+  ZHashList* hl = mXrdSucker->GetOpenFiles();
+  if (mFileListTS != hl->GetTimeStamp())
   {
-    ZHashList* hl = mXrdSucker->GetOpenFiles();
-    if (mFileListTS != hl->GetTimeStamp())
-    {
-      mFileList.clear();
-      mFileListTS = hl->CopyListByGlass<XrdFile>(mFileList);
-    }
-
-    bool fqhn         = (args["fqhn"] == "1");
-    bool no_same_site = (args["no_same_site"] == "1");
-
-    int  file_len = 64;
-    if ( ! args["file_len"].IsNull()) file_len = TMath::Max(0, args["file_len"].Atoi());
-
-    TPMERegexp short_domain("[^\\.]+\\.[^\\.]+$", "o");
-
-    TPMERegexp srv_re, cli_re, usr_re, fil_re;
-    bool       f_srv,  f_cli,  f_usr,  f_fil;
-
-    try
-    {
-      f_srv = ( ! args["server_re"].IsNull());
-      if (f_srv) srv_re.Reset(args["server_re"], "o");
-
-      f_cli = ( ! args["client_re"].IsNull());
-      if (f_cli) cli_re.Reset(args["client_re"], "o");
-
-      f_usr = ( ! args["user_re"].IsNull());
-      if (f_usr) usr_re.Reset(args["user_re"], "o");
-
-      f_fil = ( ! args["file_re"].IsNull());
-      if (f_fil) fil_re.Reset(args["file_re"], "o");
-    }
-    catch (std::exception& exc)
-    {
-      content = GForm("Exception caught during parsing of regular expressions:<br>&nbsp;&nbsp;%s\n",
-                      exc.what());
-      return;
-    }
-
-    bool any_fil  = no_same_site || f_srv || f_cli || f_usr || f_fil;
-    int  pass_cnt = 0;
-
-    ostringstream oss;
-
-    TPMERegexp rePath("/");
-    bool odd = false;
-    for (list<XrdFile*>::iterator xfi = mFileList.begin(); xfi != mFileList.end(); ++xfi)
-    {
-      odd = !odd;
-      XrdFile *file = *xfi;
-      XrdUser *user = file->GetUser();
-
-      TString server_id = fqhn ? user->GetServer()->GetFqhn() : user->GetServer()->RefDomain();
-      TString client_id = fqhn ? user->GetFromFqhn()          : user->RefFromDomain();
-
-      if (no_same_site)
-      {
-        short_domain.Match(user->GetServer()->RefDomain());
-        TString srv = short_domain[0];
-        short_domain.Match(user->RefFromDomain());
-        TString clt = short_domain[0];
-        if (srv == clt)
-          continue;
-      }
-      if (f_srv && ! srv_re.Match(server_id))           continue;
-      if (f_cli && ! cli_re.Match(client_id))           continue;
-      if (f_usr && ! usr_re.Match(user->RefRealName())) continue;
-      if (f_fil && ! fil_re.Match(file->RefName()))     continue;
-
-      ++pass_cnt;
-
-      oss << Form("<tr class='row%d'>", odd ? 1 : 2)<< endl; 
-
-      if (! bParanoia)
-      {
-        TString fn(file->RefName());
-        if (file_len && fn.Length() > file_len) {
-          fn.Resize(file_len);
-          fn += "...";
-        }
-        oss << "<td>" << fn << "</td>" << endl;
-      }
-      else
-      {
-        if (file->RefName().BeginsWith("/store/user/"))
-          oss << "<td>/store/user</td>" << endl;
-        else if (rePath.Split(file->RefName()) > 3)
-          oss << Form("<td>/%s/%s/%s</td>", rePath[1].Data(), rePath[2].Data(), rePath[3].Data()) <<endl;
-        else
-          oss << Form("<td>%s</td>", file->GetName()) << endl;
-      }
-
-      if (bParanoia && ! user->RefRealName().IsNull())
-      {
-        oss << "<td>" << Form("%X",  user->RefRealName().Hash()) <<  "</td>" << endl;
-      }
-      else
-      {
-        oss << "<td>" << user->GetRealName() << "</td>" << endl;
-      }
-      oss << "<td>" << server_id << "</td>" << endl;
-      oss << "<td>" << client_id << "</td>" << endl;
-
-      GTime    open_t = req_time - file->RefOpenTime();
-      GTime    lmsg_t = req_time - file->RefLastMsgTime();
-
-      oss << "<td>" << open_t.ToHourMinSec(true) << "</td>" << endl;
-      oss << "<td>" << lmsg_t.ToHourMinSec(true) << "</td>" << endl;
-
-      Double_t siz_mb = file->GetSizeMB();
-      Double_t sum_mb = file->GetReadStats().GetSumX();
-      Double_t t_rate = (open_t - lmsg_t).ToDouble();  if (t_rate < 0.001) t_rate = 0.001;
-
-      Bool_t f_stream = (sum_mb == 0 && file->GetRTotalMB() > 0);
-      if (f_stream)
-      {
-        sum_mb = file->GetRTotalMB();
-      }
-      oss << "<td>" << GForm("%.3f", sum_mb) << "</td>" << endl;
-      oss << "<td>" << GForm("%.3f", siz_mb ? 100.0*sum_mb/siz_mb : 0.0) << "</td>" << endl;
-      oss << "<td>" << GForm("%.3f", sum_mb / t_rate) << "</td>" << endl;
-      if (f_stream)
-      {
-        oss << "<td>n/a</td>" << endl;
-      }
-      else
-      {
-        oss << "<td>" << GForm("%.3f", file->GetReadStats().GetAverage()) << "</td>" << endl;
-      }
-
-      oss << "</tr>" << endl;
-    }
-    oss << "</table>" << endl;
-    oss << "</body>"  << endl;
-    oss << "</html>"  << endl;
-
-
-    // Generate table header
-
-    ostringstream osh;
-    osh << "<html>" << std::endl;
-    osh << "<meta http-equiv=\"refresh\" content=\"180\" />" << endl;
-    osh << "<head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\"> " << std::endl;
-    osh << "<style type=\"text/css\">" << std::endl << std::endl;
-     
-    osh << "table.demo {" << std::endl;
-    osh << "   background-color: #C0C0FF;" << std::endl;
-    osh << "   padding: 2px 5px 2px 5px;" << std::endl;
-    osh << "}" << std::endl;
-    osh << "tr.row1 {" << std::endl;
-    osh << "   background-color: #FFFFFF;" << std::endl;
-    osh << "}" << std::endl;
-    osh << "tr.row2 {" << std::endl;
-    osh << "   background-color: #E0E0FF;" << std::endl;
-    osh << "}" << std::endl;
-    osh << "</style> "<< std::endl;
-    osh << "<title>Xrd open files [";
-    if (any_fil) osh << pass_cnt << "/";
-    osh << mFileList.size() << "]</title> " << std::endl;
-    osh << "</head>" << std::endl;
-    
-    osh << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/gs_sortable.js\"></script>" << std::endl;
-    osh << "<script type=\"text/javascript\">" << std::endl;
-    osh << "" << std::endl;
-   
-    osh << "TSort_Data = new Array ('table_xrd_cms_openfiles', 's',  's', 's', 's', 's', 's', 'f', 'f', 'f', 'f');" << std::endl;
-    osh << "TSort_Classes = new Array ('row2', 'row1');" << std::endl;
-    osh << "TSort_Initial = new Array ('0D');" << std::endl;
-    osh << "var TSort_Icons = new Array (' V', ' &#923;');" << std::endl;
-    osh << "var TSort_Cookie = 'table_xrd_cms_openfiles';" << std::endl;
-    osh << "tsRegister();" << std::endl;
-    osh << "</script>" << std::endl;
-
-
-    osh << "<TABLE id=\"table_xrd_cms_openfiles\" class=\"demo\" width=100%>" << std::endl;
-    osh << "<thead>"  << std::endl;
-
-    // header
-    osh << "<tr>"<< endl;
-    osh << "<th align=\"left\">File</th>";
-    osh << "<th align=\"left\">User" << (bParanoia ? " Hash" : "") << "</th>";
-    osh << "<th align=\"left\">Server" << (fqhn ? "" : " Domain") << "</th>";
-    osh << "<th align=\"left\">Client" << (fqhn ? "" : " Domain") << "</th>";
-    osh << "<th align=\"left\">Open Ago</th>";
-    osh << "<th align=\"left\">Update Ago</th>";
-    osh << "<th align=\"left\">Read [MB]</th>";
-    osh << "<th align=\"left\">Read [%]</th>";
-    osh << "<th align=\"left\">Rate [MB/s]</th>";
-    osh << "<th align=\"left\">Avg Read [MB]</th>";
-    osh << endl;
-    osh << "</tr>" << endl;
-
-    osh << "</thead>" << std::endl;
-
-    content  = osh.str();
-    content += oss.str();
+    mFileList.clear();
+    mFileListTS = hl->CopyListByGlass<XrdFile>(mFileList);
   }
+
+  bool fqhn         = (args["fqhn"] == "1");
+  bool no_same_site = (args["no_same_site"] == "1");
+
+  int  file_len = 64;
+  if ( ! args["file_len"].IsNull()) file_len = TMath::Max(0, args["file_len"].Atoi());
+
+  TPMERegexp short_domain("[^\\.]+\\.[^\\.]+$", "o");
+
+  TPMERegexp srv_re, cli_re, usr_re, fil_re;
+  bool       f_srv,  f_cli,  f_usr,  f_fil;
+
+  try
+  {
+    f_srv = ( ! args["server_re"].IsNull());
+    if (f_srv) srv_re.Reset(args["server_re"], "o");
+
+    f_cli = ( ! args["client_re"].IsNull());
+    if (f_cli) cli_re.Reset(args["client_re"], "o");
+
+    f_usr = ( ! args["user_re"].IsNull());
+    if (f_usr) usr_re.Reset(args["user_re"], "o");
+
+    f_fil = ( ! args["file_re"].IsNull());
+    if (f_fil) fil_re.Reset(args["file_re"], "o");
+  }
+  catch (std::exception& exc)
+  {
+    content = GForm("Exception caught during parsing of regular expressions:<br>&nbsp;&nbsp;%s\n",
+                    exc.what());
+    return;
+  }
+
+  bool any_fil  = no_same_site || f_srv || f_cli || f_usr || f_fil;
+  int  pass_cnt = 0;
+
+  ostringstream oss;
+
+  TPMERegexp rePath("/");
+  bool odd = false;
+  for (list<XrdFile*>::iterator xfi = mFileList.begin(); xfi != mFileList.end(); ++xfi)
+  {
+    odd = !odd;
+    XrdFile *file = *xfi;
+    XrdUser *user = file->GetUser();
+
+    TString server_id = fqhn ? user->GetServer()->GetFqhn() : user->GetServer()->RefDomain();
+    TString client_id = fqhn ? user->GetFromFqhn()          : user->RefFromDomain();
+
+    if (no_same_site)
+    {
+      short_domain.Match(user->GetServer()->RefDomain());
+      TString srv = short_domain[0];
+      short_domain.Match(user->RefFromDomain());
+      TString clt = short_domain[0];
+      if (srv == clt)
+        continue;
+    }
+    if (f_srv && ! srv_re.Match(server_id))           continue;
+    if (f_cli && ! cli_re.Match(client_id))           continue;
+    if (f_usr && ! usr_re.Match(user->RefRealName())) continue;
+    if (f_fil && ! fil_re.Match(file->RefName()))     continue;
+
+    ++pass_cnt;
+
+    oss << Form("<tr class='row%d'>", odd ? 1 : 2)<< endl; 
+
+    if (! bParanoia)
+    {
+      TString fn(file->RefName());
+      if (file_len && fn.Length() > file_len) {
+        fn.Resize(file_len);
+        fn += "...";
+      }
+      oss << "<td>" << fn << "</td>" << endl;
+    }
+    else
+    {
+      if (file->RefName().BeginsWith("/store/user/"))
+        oss << "<td>/store/user</td>" << endl;
+      else if (rePath.Split(file->RefName()) > 3)
+        oss << Form("<td>/%s/%s/%s</td>", rePath[1].Data(), rePath[2].Data(), rePath[3].Data()) <<endl;
+      else
+        oss << Form("<td>%s</td>", file->GetName()) << endl;
+    }
+
+    if (bParanoia && ! user->RefRealName().IsNull())
+    {
+      oss << "<td>" << Form("%X",  user->RefRealName().Hash()) <<  "</td>" << endl;
+    }
+    else
+    {
+      oss << "<td>" << user->GetRealName() << "</td>" << endl;
+    }
+    oss << "<td>" << server_id << "</td>" << endl;
+    oss << "<td>" << client_id << "</td>" << endl;
+
+    GTime    open_t = req_time - file->RefOpenTime();
+    GTime    lmsg_t = req_time - file->RefLastMsgTime();
+
+    oss << "<td>" << open_t.ToHourMinSec(true) << "</td>" << endl;
+    oss << "<td>" << lmsg_t.ToHourMinSec(true) << "</td>" << endl;
+
+    Double_t siz_mb = file->GetSizeMB();
+    Double_t sum_mb = file->GetReadStats().GetSumX();
+    Double_t t_rate = (open_t - lmsg_t).ToDouble();  if (t_rate < 0.001) t_rate = 0.001;
+
+    Bool_t f_stream = (sum_mb == 0 && file->GetRTotalMB() > 0);
+    if (f_stream)
+    {
+      sum_mb = file->GetRTotalMB();
+    }
+    oss << "<td>" << GForm("%.3f", sum_mb) << "</td>" << endl;
+    oss << "<td>" << GForm("%.3f", siz_mb ? 100.0*sum_mb/siz_mb : 0.0) << "</td>" << endl;
+    oss << "<td>" << GForm("%.3f", sum_mb / t_rate) << "</td>" << endl;
+    if (f_stream)
+    {
+      oss << "<td>n/a</td>" << endl;
+    }
+    else
+    {
+      oss << "<td>" << GForm("%.3f", file->GetReadStats().GetAverage()) << "</td>" << endl;
+    }
+
+    oss << "</tr>" << endl;
+  }
+  oss << "</table>" << endl;
+  oss << "</body>"  << endl;
+  oss << "</html>"  << endl;
+
+
+  // Generate table header
+
+  ostringstream osh;
+  osh << "<html>" << std::endl;
+  osh << "<meta http-equiv=\"refresh\" content=\"180\" />" << endl;
+  osh << "<head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\"> " << std::endl;
+  osh << "<style type=\"text/css\">" << std::endl << std::endl;
+     
+  osh << "table.demo {" << std::endl;
+  osh << "   background-color: #C0C0FF;" << std::endl;
+  osh << "   padding: 2px 5px 2px 5px;" << std::endl;
+  osh << "}" << std::endl;
+  osh << "tr.row1 {" << std::endl;
+  osh << "   background-color: #FFFFFF;" << std::endl;
+  osh << "}" << std::endl;
+  osh << "tr.row2 {" << std::endl;
+  osh << "   background-color: #E0E0FF;" << std::endl;
+  osh << "}" << std::endl;
+  osh << "</style> "<< std::endl;
+  osh << "<title>Xrd open files [";
+  if (any_fil) osh << pass_cnt << "/";
+  osh << mFileList.size() << "]</title> " << std::endl;
+  osh << "</head>" << std::endl;
+    
+  osh << "<script type=\"text/javascript\" src=\"http://uaf-2.t2.ucsd.edu/~alja/gs_sortable.js\"></script>" << std::endl;
+  osh << "<script type=\"text/javascript\">" << std::endl;
+  osh << "" << std::endl;
+   
+  osh << "TSort_Data = new Array ('table_xrd_cms_openfiles', 's',  's', 's', 's', 's', 's', 'f', 'f', 'f', 'f');" << std::endl;
+  osh << "TSort_Classes = new Array ('row2', 'row1');" << std::endl;
+  osh << "TSort_Initial = new Array ('0D');" << std::endl;
+  osh << "var TSort_Icons = new Array (' V', ' &#923;');" << std::endl;
+  osh << "var TSort_Cookie = 'table_xrd_cms_openfiles';" << std::endl;
+  osh << "tsRegister();" << std::endl;
+  osh << "</script>" << std::endl;
+
+
+  osh << "<TABLE id=\"table_xrd_cms_openfiles\" class=\"demo\" width=100%>" << std::endl;
+  osh << "<thead>"  << std::endl;
+
+  // header
+  osh << "<tr>"<< endl;
+  osh << "<th align=\"left\">File</th>";
+  osh << "<th align=\"left\">User" << (bParanoia ? " Hash" : "") << "</th>";
+  osh << "<th align=\"left\">Server" << (fqhn ? "" : " Domain") << "</th>";
+  osh << "<th align=\"left\">Client" << (fqhn ? "" : " Domain") << "</th>";
+  osh << "<th align=\"left\">Open Ago</th>";
+  osh << "<th align=\"left\">Update Ago</th>";
+  osh << "<th align=\"left\">Read [MB]</th>";
+  osh << "<th align=\"left\">Read [%]</th>";
+  osh << "<th align=\"left\">Rate [MB/s]</th>";
+  osh << "<th align=\"left\">Avg Read [MB]</th>";
+  osh << endl;
+  osh << "</tr>" << endl;
+
+  osh << "</thead>" << std::endl;
+
+  content  = osh.str();
+  content += oss.str();
 }
 
 //==============================================================================
@@ -410,7 +407,6 @@ void XrdEhs::StartServer()
     if (bServerUp)
       throw _eh + "server already running.";
 
-    mServeTime.SetZero();
     mFileListTS = 0;
 
     bServerUp = true;
