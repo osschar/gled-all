1fcbc863f2aa70a60c7de8d1ff8797aed5c10d29
Matevž Tadel <osschar@users.noreply.github.com>
2014-04-17 22:03:49 +0000

    Use github svn gateway to export root source. Bump to 5.34.18.

--- gled-builder/external/root/make-root-src-dist.sh
+++ gled-builder/external/root/make-root-src-dist.sh
@@ -5,14 +5,14 @@
 # Modified version of root/build/unix/makedistsrc.sh
 # Fix the following variables before running:
 
-svnurl="https://root.cern.ch/svn/root/branches/v5-34-00-patches"
-version="5.34.05"
+# svnurl="https://github.com/root-mirror/root/branches/v5-34-00-patches"
+# version="5.34.05"
 
-# svnurl="https://root.cern.ch/svn/root/branches/dev/matevz/5.34-patches-gled"
+# svnurl="https://github.com/root-mirror/root/branches/dev/matevz/5.34-patches-gled"
 # version=5.34.01
 
-# svnurl="https://root.cern.ch/svn/root/tags/v5-32-02"
-# version="5.32.02"
+svnurl="https://github.com/root-mirror/root/tags/v5-34-18"
+version="5.34.18"
 
 dir="root"
 tarfile="root-$version-gled.tar"
@@ -21,18 +21,19 @@ echo "Checking out ..."
 
 rm -rf $dir
 mkdir $dir
-svn co $svnurl $dir
+svn export $svnurl $dir
 cd $dir
 
 # echo "Merging gled-related changes ..."
 # For 5.26.01, FTGL stuff
 # svn merge -c 32740 $svntrunk
 
-echo "Generating etc/svninfo.txt"
-build/unix/svninfo.sh
+# echo "Generating etc/svninfo.txt"
+# build/unix/svninfo.sh
 
-echo "Remove .svn directories ..."
-find . -depth -type d -name .svn -exec rm -rf {} \;
+# echo "Remove .svn directories ..."
+# find . -depth -type d -name .svn -exec rm -rf {} \;
+
 echo "Removing docs ..."
 find . -depth -type d -name  doc -exec rm -rf {} \;
 rm -rf docbook
